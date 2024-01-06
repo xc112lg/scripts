d87568c7c1cce2038489cd5d7a6c309598ac7f75
xc112lg <152837944+xc112lg@users.noreply.github.com>
2024-01-06 13:13:43 +0800

    e

--- fixes.sh
+++ fixes.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-# Cam fix for LG G6
-cd frameworks/base/
-git fetch https://github.com/LG-G6/android_frameworks_base.git lineage-20.0
-git cherry-pick bd320b75a6c8d0212145406b5fd598181a199b1a
-cd ../../
+
 
 # Mixer: adjust input volume levels
 cd device/lge/g6-common
@@ -12,5 +8,10 @@ git fetch https://github.com/LG-G6/android_device_lge_g6-common.git dev/lineage-
 git cherry-pick b3edeba5ac6500c145fec7222ffc696c9b819af0
 cd ../../../
 
-# SELinux: let's keep it permissive
-repopick -f 375818
+# remove timekeep
+cd device/lge/msm8996-common
+git fetch https://github.com/xc112lg/android_device_lge_msm8996-common.git patch-1
+git cherry-pick e7fba872e802e245fb22f417f8db4868b60e6836
+cd ../../../
+
+
--- sync.sh
+++ sync.sh
@@ -11,5 +11,4 @@ rm -rf vendor/lineage/
 repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
 source build/envsetup.sh
 
-source scripts/repopick.sh
 source scripts/fixes.sh
