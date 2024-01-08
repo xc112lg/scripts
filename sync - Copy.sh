903d04fec81813c86fce10064d23ada190fea244
xc112lg <152837944+xc112lg@users.noreply.github.com>
2024-01-08 14:05:21 +0800

    e

--- sync - Copy.sh
+++ sync - Copy.sh
@@ -14,6 +14,13 @@ source build/envsetup.sh
 source scripts/fixes.sh
 lunch lineage_h870-userdebug
 m bacon
-
+lunch lineage_us997-userdebug
+m bacon
+lunch lineage_h872-userdebug
+m bacon
+find out/target/product/*/ -type f -name "*.zip" -execdir bash -c 'mv "$1" "${1%.zip}_newname.zip"' bash {} \;
+source scripts/fixes1.sh
+lunch lineage_h872-userdebug
+m bacon
 
 
