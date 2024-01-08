657dcfc817d8b9992a3f6b8fbc4586fbc00fdd07
xc112lg <152837944+xc112lg@users.noreply.github.com>
2024-01-08 10:49:23 +0800

    up

--- sync.sh
+++ sync.sh
@@ -7,7 +7,7 @@ rm -rf hardware/lge
 rm -rf kernel/lge/msm8996/
 rm -rf vendor/lge/
 rm -rf vendor/lineage/
-
+rm out/target/product/*/*.zip
 repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
 source build/envsetup.sh
 
--- sync1.sh
+++ sync1.sh
@@ -8,7 +8,7 @@ rm -rf kernel/lge/msm8996/
 rm -rf vendor/lge/
 rm -rf vendor/lineage/
 
-
+rm out/target/product/*/*.zip
 repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
 source build/envsetup.sh
 
