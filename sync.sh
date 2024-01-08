416fcd178753b9b47654abc3d7ef94d29245f31e
xc112lg <152837944+xc112lg@users.noreply.github.com>
2024-01-08 10:50:01 +0800

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -18,5 +18,9 @@ lunch lineage_us997-userdebug
 m bacon
 lunch lineage_h872-userdebug
 m bacon
+find out/target/product/*/ -type f -name "*.zip" -execdir bash -c 'mv "$1" "${1%.zip}_newname.zip"' bash {} \;
+source scripts/fixes1.sh
+lunch lineage_h872-userdebug
+m bacon
 
 
