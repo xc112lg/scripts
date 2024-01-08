2ad2b26f2ae268790a3efed31863e5ae15dab615
xc112lg <152837944+xc112lg@users.noreply.github.com>
2024-01-08 20:03:48 +0800

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -8,7 +8,7 @@ rm -rf kernel/lge/msm8996/
 rm -rf vendor/lge/
 rm -rf vendor/lineage/
 rm out/target/product/*/*.zip
-repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
+repo sync -c --force-sync --optimized-fetch --no-tags --no-clone-bundle --prune -j$(nproc --all)
 source build/envsetup.sh
 
 source scripts/fixes.sh
