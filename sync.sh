b411b045aa95508c008c1b6c5f1206d771025d8a
xc112lg <152837944+xc112lg@users.noreply.github.com>
2024-01-10 09:50:34 +0800

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -13,9 +13,9 @@ repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
 source build/envsetup.sh
 
 source scripts/fixes.sh
-lunch derp_h870-userdebug
-mka derp
-lunch derp_us997-userdebug
-mka derp
+#lunch derp_h870-userdebug
+#mka derp
+#lunch derp_us997-userdebug
+#mka derp
 lunch derp_h872-userdebug
 mka derp
