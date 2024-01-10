adcdcd9e7fc5370ae6658dd0ed15d1c19018f852
xc112lg <152837944+xc112lg@users.noreply.github.com>
2024-01-11 00:45:40 +0800

    Update sync3.sh

--- sync3.sh
+++ sync3.sh
@@ -5,15 +5,13 @@
 	#cd ../
 
 cd shrp
-rm -rf *
+rm -rf device/*
 repo init -u https://github.com/SHRP/manifest.git -b shrp-12.1
 repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
 rm -rf device/*
 git clone https://github.com/xc112lg/twrp_device_lge_h872 -b twrp ./device/lge/h872
-set +e
 source build/envsetup.sh
 export ALLOW_MISSING_DEPENDENCIES=true
-set -e
 lunch twrp_h872-eng && make clean && mka recoveryimage -j$(nproc --all)
 
 
