68499e36d77835ef39897e0f226fb25b0ad167be
xc112lg <152837944+xc112lg@users.noreply.github.com>
2024-01-10 15:32:47 +0800

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -2,13 +2,14 @@
 
 
 
-mkdir -p $HOME/OrangeFox
-cd $HOME/OrangeFox
-git clone https://gitlab.com/OrangeFox/sync.git -b master
-cd sync
-./orangefox_sync.sh --branch 12.1 --path $HOME/OrangeFox/fox_12.1
+#mkdir -p $HOME/OrangeFox
+#cd $HOME/OrangeFox
+#git clone https://gitlab.com/OrangeFox/sync.git -b master
+#cd sync
+#./orangefox_sync.sh --branch 12.1 --path $HOME/OrangeFox/fox_12.1
 cd $HOME/OrangeFox/fox_12.1
-git clone https://github.com/xc112lg/android_device_lge_h872-1 -b twrp ./device/lge/h872
+rm -rf /device/lge/h872
+git clone https://github.com/xc112lg/twrp_device_lge_h872 -b twrp ./device/lge/h872
 cd $HOME/OrangeFox/fox_12.1
 set +e
 source build/envsetup.sh
