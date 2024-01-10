364578588eddfaf8016751f2decbb666084352e5
xc112lg <152837944+xc112lg@users.noreply.github.com>
2024-01-11 02:03:58 +0800

    Create s4.sh

---
+++ s4.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+	git clone https://gitlab.com/dtiven13/scripts.git -b master script
+        cd script
+        sudo bash setup/android_build_env.sh
+	cd ../
+	cd shrp
+	repo init -u https://github.com/SHRP/manifest.git -b shrp-12.1
+	repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
+	rm -rf device/*
+	git clone https://github.com/xc112lg/twrp_device_lge_h872 -b twrp ./device/lge/h872
+	source build/envsetup.sh
+	export ALLOW_MISSING_DEPENDENCIES=true
+	lunch twrp_h872-eng && make clean && mka recoveryimage -j$(nproc --all)
+
+
+
