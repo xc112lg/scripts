036b60143956ddf210ebd855175db00e470c1efe
xc112lg <152837944+xc112lg@users.noreply.github.com>
2024-01-11 05:54:56 +0800

    Update sync2.sh

--- sync2.sh
+++ sync2.sh
@@ -2,14 +2,14 @@
 
 
 
-mkdir -p $HOME/OrangeFox
-cd $HOME/OrangeFox
+mkdir -p $HOME/shrp
+cd $HOME/shrp
 repo init -u https://github.com/SHRP/manifest.git -b shrp-12.1
 repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
-cd $HOME/OrangeFox
+cd $HOME/shrp
 rm -rf /device/lge/h872
 git clone https://github.com/xc112lg/twrp_device_lge_h872 -b android-7.1 ./device/lge/h872
-cd $HOME/OrangeFox
+cd $HOME/shrp
 set +e
 source build/envsetup.sh
 export ALLOW_MISSING_DEPENDENCIES=true
