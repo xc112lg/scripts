9f16ac5afaa054eb6145c2f8f1ad83baeeee5af2
xc112lg <152837944+xc112lg@users.noreply.github.com>
2024-01-11 02:12:42 +0800

    Update sync3.sh

--- sync3.sh
+++ sync3.sh
@@ -6,6 +6,8 @@
 	cd ../
 	cd shrp
 	rm -rf device/*
+	rm -rf out/.module_paths
+	rm -rf out/*
 	repo init -u https://github.com/SHRP/manifest.git -b shrp-12.1
 	repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
 	rm -rf device/*
