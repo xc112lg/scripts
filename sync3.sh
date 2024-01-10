5f39cd11e3813ff218cd958eb77d89a136129370
xc112lg <152837944+xc112lg@users.noreply.github.com>
2024-01-11 00:06:48 +0800

    Update sync3.sh

--- sync3.sh
+++ sync3.sh
@@ -8,7 +8,7 @@ cd shrp
 repo init -u https://github.com/SHRP/manifest.git -b shrp-12.1
 repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
 rm -rf device/*
-git clone https://github.com/xc112lg/twrp_device_lge_h872 -b test2 ./device/lge/h872
+git clone https://github.com/xc112lg/twrp_device_lge_h872 -b twrp ./device/lge/h872
 set +e
 source build/envsetup.sh
 export ALLOW_MISSING_DEPENDENCIES=true
