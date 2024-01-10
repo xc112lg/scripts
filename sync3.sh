103c986ea23449b6ed2ad7a46653dbdd96408a78
xc112lg <152837944+xc112lg@users.noreply.github.com>
2024-01-10 22:40:02 +0800

    Update sync3.sh

--- sync3.sh
+++ sync3.sh
@@ -2,7 +2,6 @@
 
 
 
-mkdir -p ./shrp
 cd shrp
 repo init -u https://github.com/SHRP/manifest.git -b shrp-12.1
 repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
@@ -12,7 +11,7 @@ set +e
 source build/envsetup.sh
 export ALLOW_MISSING_DEPENDENCIES=true
 set -e
-lunch twrp_h872-eng && make clean && mka recoveryimage -jX
+lunch twrp_h872-eng && make clean && mka recoveryimage -j$(nproc --all)
 
 
 
