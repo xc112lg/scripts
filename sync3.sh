d42ba8d322411ce609352882d34dfc6c4af413eb
xc112lg <152837944+xc112lg@users.noreply.github.com>
2024-01-10 23:33:14 +0800

    Update sync3.sh

--- sync3.sh
+++ sync3.sh
@@ -7,7 +7,7 @@
 cd shrp
 repo init -u https://github.com/SHRP/manifest.git -b shrp-12.1
 repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
-rm -rf /device/*
+rm -rf device/*
 git clone https://github.com/xc112lg/twrp_device_lge_h872 -b twrp ./device/lge/h872
 set +e
 source build/envsetup.sh
