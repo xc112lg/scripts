1049d1a9427e0209e11ebfe7725bc7d23db2e14a
xc112lg <152837944+xc112lg@users.noreply.github.com>
2024-01-10 13:50:49 +0800

    Update fixes.sh

--- fixes.sh
+++ fixes.sh
@@ -2,8 +2,8 @@
 
 # Cam fix for LG G6
 cd frameworks/base/
-git fetch https://github.com/xc112lg/android_frameworks_base-1.git patch-6
-git cherry-pick 270f82014a595cfcdf7cd131e8836e77497bf4d6
+git fetch https://github.com/xc112lg/android_frameworks_base-1.git cr9
+git cherry-pick 47d7418b2c1d11ce03f5ea99582ae77908be9ac7
 cd ../../
 
 # Mixer: adjust input volume levels
@@ -17,5 +17,24 @@ cd device/lge/msm8996-common
 git fetch https://github.com/xc112lg/android_device_lge_msm8996-common.git patch-1
 git cherry-pick 060a0a00e2954ac27b04e217390a78133c4484dc
 cd ../../../
+
+# change vendor
+cd device/lge/h870
+git fetch https://github.com/xc112lg/android_device_lge_h870.git cr9
+git cherry-pick fce1972cd6a4f40b216dbb65295e2d2975e29fdd
+cd ../../../
+
+# change vendor
+cd device/lge/h872
+git fetch https://github.com/xc112lg/android_device_lge_h872.git cr9
+git cherry-pick 43752795285591c333b54994b7b2fa60585c7454
+cd ../../../
+
+# change vendor
+cd device/lge/us997
+git fetch https://github.com/xc112lg/android_device_lge_us997.git cr9
+git cherry-pick abcf317c7c8a505c9ed14dbd59ff2dee9e5edd4a
+cd ../../../
+
 
 
