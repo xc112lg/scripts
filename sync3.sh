9d7965f3a28855916faf77335f72fa53d774c9b0
xc112lg <152837944+xc112lg@users.noreply.github.com>
2024-01-11 15:39:14 +0800

    Update sync3.sh

--- sync3.sh
+++ sync3.sh
@@ -6,16 +6,15 @@
 	#cd ../
 	cd shrp
 	rm -rf device/*
-	rm -rf *
 	rm -rf out/.module_paths
 	repo init -u https://github.com/SHRP/manifest.git -b shrp-12.1
-cd system/core/
-git fetch https://github.com/xc112lg/android_system_core.git patch-1
-git cherry-pick 76072abd225ba5d05006d8c45a50f42da202813e
-cd ../../
+        cd system/core/
+        git fetch https://github.com/xc112lg/android_system_core.git patch-1
+        git cherry-pick 76072abd225ba5d05006d8c45a50f42da202813e
+        cd ../../
 	repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
 	rm -rf device/*
-	git clone https://github.com/xc112lg/twrp_device_lge_h872 -b twrp ./device/lge/h872
+	git clone https://github.com/xc112lg/twrp_device_lge_h872 -b tr ./device/lge/h872
 	source build/envsetup.sh
 	export ALLOW_MISSING_DEPENDENCIES=true
 	lunch twrp_h872-eng && make clean && mka recoveryimage -j$(nproc --all)
