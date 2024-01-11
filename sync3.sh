527a087bf30ba1e6f082f3f20d4c9fd79d1491ed
xc112lg <152837944+xc112lg@users.noreply.github.com>
2024-01-11 14:07:22 +0800

    Update sync3.sh

--- sync3.sh
+++ sync3.sh
@@ -9,7 +9,10 @@
 	rm -rf *
 	rm -rf out/.module_paths
 	repo init -u https://github.com/SHRP/manifest.git -b shrp-12.1
-
+cd system/core/
+git fetch https://github.com/xc112lg/android_system_core.git patch-1
+git cherry-pick 76072abd225ba5d05006d8c45a50f42da202813e
+cd ../../
 	repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
 	rm -rf device/*
 	git clone https://github.com/xc112lg/twrp_device_lge_h872 -b twrp ./device/lge/h872
