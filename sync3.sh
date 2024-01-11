cfb4113839ce84e23134a82cf68d1b59a5f0993c
xc112lg <152837944+xc112lg@users.noreply.github.com>
2024-01-11 16:04:14 +0800

    Update sync3.sh

--- sync3.sh
+++ sync3.sh
@@ -15,7 +15,7 @@
         git fetch https://github.com/xc112lg/android_system_core.git patch-1
         git cherry-pick 76072abd225ba5d05006d8c45a50f42da202813e
         cd ../../
-	git clone https://github.com/xc112lg/twrp_device_lge_h872 -b tr ./device/lge/h872
+	git clone https://github.com/xc112lg/twrp_device_lge_h872 -b tr2 ./device/lge/h872
 	source build/envsetup.sh
 	export ALLOW_MISSING_DEPENDENCIES=true
 	lunch twrp_h872-eng && make clean && mka recoveryimage -j$(nproc --all)
