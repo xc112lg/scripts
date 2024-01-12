e90159c1cea0884e47fddc0350c02d8d0c39eb84
xc112lg <152837944+xc112lg@users.noreply.github.com>
2024-01-13 07:40:23 +0800

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -9,8 +9,4 @@ rm -rf vendor/lge/
 rm -rf vendor/lineage/
 
 repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
-source build/envsetup.sh
 
-source scripts/repopick.sh
-source scripts/fixes.sh
-source scripts/extras.sh
