8c6f9d2fa7f345e5e411d8d6ff620769fd2e67c7
WAR-DEPT <131593996+WAR-DEPT@users.noreply.github.com>
2024-01-16 18:45:44 +0530

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -2,10 +2,6 @@
 
 rm -rf build/make/
 rm -rf frameworks/base/
-rm -rf device/lge/
-rm -rf hardware/lge
-rm -rf kernel/lge/msm8996/
-rm -rf vendor/lge/
 rm -rf vendor/lineage/
 
 rm out/target/product/*/*.zip
@@ -13,9 +9,5 @@ repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
 source build/envsetup.sh
 
 source scripts/fixes.sh
-lunch derp_h870-userdebug
-mka derp
-lunch derp_us997-userdebug
-mka derp
-lunch derp_h872-userdebug
-mka derp
+lunch afterlifee_miatoll-userdebug
+m afterlife
