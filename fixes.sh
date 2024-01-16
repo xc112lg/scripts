26c85228ec787db92d793a2ceb716fdf68e1a229
WAR-DEPT <131593996+WAR-DEPT@users.noreply.github.com>
2024-01-16 18:43:21 +0530

    Update fixes.sh

--- fixes.sh
+++ fixes.sh
@@ -1,15 +1,1 @@
-#!/bin/bash
-
-# Cam fix for LG G6
-#cd frameworks/base/
-#git fetch https://github.com/LG-G6/android_frameworks_base.git lineage-20.0
-#git cherry-pick bd320b75a6c8d0212145406b5fd598181a199b1a
-#cd ../../
-
-# Mixer: adjust input volume levels
-cd device/lge/g6-common
-git fetch https://github.com/LG-G6/android_device_lge_g6-common.git dev/lineage-19.1
-git cherry-pick b3edeba5ac6500c145fec7222ffc696c9b819af0
-cd ../../../
-
 
