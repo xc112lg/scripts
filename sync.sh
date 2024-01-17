4aa9864788192e8e658c2c77409efe434ee0dc56
xc112lg <152837944+xc112lg@users.noreply.github.com>
2024-01-17 15:51:35 +0800

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -2,8 +2,8 @@
 rm -rf device/lge/msm8996-common
 git clone https://github.com/LineageOS/android_device_lge_msm8996-common -b lineage-21 device/lge/msm8996-common
 cd device/lge/msm8996-common
-git fetch https://github.com/xc112lg/android_device_lge_msm8996-common.git patch-9
-git cherry-pick 733ca3007ce466fb7ddde0cba8f5254d5c712e51
+git fetch https://github.com/xc112lg/android_device_lge_msm8996-common.git patch-10
+git cherry-pick eb3a34918658c0d14a3913b75dc503801b05a020
 cd ../../../
 source build/envsetup.sh
 rm out/target/product/*/*.zip
