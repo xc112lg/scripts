3ce922619fd0c33eb2cbfc6b634172cc60eabbb4
xc112lg <152837944+xc112lg@users.noreply.github.com>
2024-01-18 06:14:02 +0800

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-#rm -rf kernel/lge/msm8996
-#git clone https://github.com/LineageOS/lgkernel -b lineage-2 kernel/lge/msm8996
+rm -rf kernel/lge/msm8996
+git clone https://github.com/LG-G6/android_kernel_lge_msm8996 -b lineage-21 kernel/lge/msm8996
 rm -rf device/lge/msm8996-common
 git clone https://github.com/LineageOS/android_device_lge_msm8996-common -b lineage-21 device/lge/msm8996-common
 cd device/lge/msm8996-common
