b025cbb4e79d762ff55739fa7e73d2a2ba78734e
xc112lg <152837944+xc112lg@users.noreply.github.com>
2024-01-18 12:34:45 +0800

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -3,15 +3,15 @@
 #rm -rf kernel/lge/msm8996
 #git clone https://github.com/LG-G6/android_kernel_lge_msm8996 -b lineage-21 kernel/lge/msm8996
 rm -rf device/lge/msm8996-common
-git clone https://github.com/LineageOS/android_device_lge_msm8996-common -b lineage-21 device/lge/msm8996-common
-cd device/lge/msm8996-common
+git clone https://github.com/xc112lg/android_device_lge_msm8996-common -b patch-19 device/lge/msm8996-common
+#cd device/lge/msm8996-common
 #git fetch https://github.com/xc112lg/android_device_lge_msm8996-common.git patch-17
 #git cherry-pick de28f9e8147a0ae6e3691dc39a483c0201261d85
 #git fetch https://github.com/xc112lg/android_device_lge_msm8996-common.git patch-18
 #git cherry-pick dc936df700695e3ee1f36b0f89791f8b357f64c7
-git fetch https://github.com/xc112lg/android_device_lge_msm8996-common.git patch-19
-git cherry-pick 6a202ad64e9a8761570707a02edc279f4fdce253
-cd ../../../
+#git fetch https://github.com/xc112lg/android_device_lge_msm8996-common.git patch-19
+#git cherry-pick 6a202ad64e9a8761570707a02edc279f4fdce253
+#cd ../../../
 source build/envsetup.sh
 rm out/target/product/*/*.zip
 lunch lineage_h872-userdebug
