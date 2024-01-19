ece5b6bd2bb0b8d1b5e85bf9ea1abb5f9f3206b3
WAR-DEPT <131593996+WAR-DEPT@users.noreply.github.com>
2024-01-19 18:48:59 +0530

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -4,7 +4,6 @@ rm -rf hardware/xiaomi
 rm -rf vendor/lineage/
 rm -rf vendor/derp
 rm -rf build/make/
-rm -rf external/libcxx
 
 rm out/target/product/*/*.zip
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
