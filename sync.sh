071cb247a91a9e96a0aceb9ef9a70d892a07f295
xc112lg <152837944+xc112lg@users.noreply.github.com>
2024-01-20 01:32:56 +0800

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-rm -rf external/libcxx
+
 rm -rf hardware/xiaomi
 rm -rf vendor/lineage/
 rm -rf vendor/derp
@@ -8,6 +8,7 @@ rm -rf build/make/
 
 rm out/target/product/*/*.zip
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+rm -rf external/libcxx
 source  build/envsetup.sh
 
 lunch afterlife_miatoll-userdebug
