e7666476f3c2d6078e0901e42d46336dd97e4343
xc112lg <dtiven13@gmail.com>
2024-01-21 05:03:12 +0800

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -3,7 +3,10 @@
 
 
 rm -rf device/lge/
-
+rm -rf .repo/local_manifests
+repo init -u https://github.com/crdroidandroid/android.git -b 14.0 --git-lfs
+mkdir .repo/local_manifests
+cp scripts/roomservice.xml .repo/local_manifests
 repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
 #repo sync -c --no-clone-bundle --optimized-fetch --prune --force-sync -j$(nproc --all)
 source build/envsetup.sh
@@ -12,8 +15,8 @@ source build/envsetup.sh
 rm out/target/product/*/*.zip
 source scripts/fixes.sh
 lunch lineage_h872-userdebug
-m j$(nproc --all) bacon
+m -j$(nproc --all) bacon
 lunch lineage_us997-userdebug
-m j$(nproc --all) bacon
+m -j$(nproc --all) bacon
 lunch lineage_h870-userdebug
 m -j$(nproc --all) bacon
