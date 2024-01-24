620269ec4514f98557223e3a762b4ed374b68f1d
xc112lg <dtiven13@gmail.com>
2024-01-24 10:45:34 +0800

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -5,7 +5,7 @@ rm -rf .repo/local_manifests
 repo init -u https://github.com/DerpFest-AOSP/manifest.git -b 14 --git-lfs
 mkdir .repo/local_manifests
 cp scripts/roomservice.xml .repo/local_manifests
-#repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
+repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
 source build/envsetup.sh
 #mka clean
 #make clean
