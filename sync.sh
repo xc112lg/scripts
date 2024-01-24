526dc85744e50ffbd2b85190245e2483ca43044a
xc112lg <dtiven13@gmail.com>
2024-01-24 13:46:50 +0800

    df

--- sync.sh
+++ sync.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 rm -rf .repo/local_manifests
-#rm -rf .repo/manifests/snippets/lineage.xml
-#cp scripts/lineage.xml .repo/manifests/snippets
+rm -rf .repo/manifests/snippets/lineage.xml
+cp scripts/lineage.xml .repo/manifests/snippets
 
 rm -rf .repo/manifests/default.xml
 cp scripts/default.xml .repo/manifests
