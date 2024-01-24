9a2ccdcc8c8f0296bc3e5d14512c50338cc4a832
xc112lg <dtiven13@gmail.com>
2024-01-24 10:42:12 +0800

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -5,14 +5,14 @@ rm -rf .repo/local_manifests
 repo init -u https://github.com/DerpFest-AOSP/manifest.git -b 14 --git-lfs
 mkdir .repo/local_manifests
 cp scripts/roomservice.xml .repo/local_manifests
-repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
+#repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
 source build/envsetup.sh
 #mka clean
 #make clean
 rm out/target/product/*/*.zip
 #source scripts/fixes.sh
 lunch derp_h872-userdebug
-m -j$(nproc --all) derp
+mka -j$(nproc --all) derp
 #lunch lineage_us997-userdebug
 #m -j$(nproc --all) bacon
 #lunch lineage_h870-userdebug
