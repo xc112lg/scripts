989d71dda85e3ef74b01afc2896d4461093b5764
WAR-DEPT <131593996+WAR-DEPT@users.noreply.github.com>
2024-01-25 16:19:15 +0530

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -3,6 +3,6 @@
 repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
 source build/envsetup.sh
 
-source scripts/repopick.sh
-source scripts/fixes.sh
-source scripts/extras.sh
+
+lunch lineage_miatoll-userdebug
+m bacon
