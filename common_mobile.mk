52a7f079c6a0426364a04c344f31b86e5ae586c9
xc112lg <dtiven13@gmail.com>
2024-01-25 20:56:46 +0800

    Create common_mobile.mk

---
+++ common_mobile.mk
@@ -0,0 +1,41 @@
+# Inherit common mobile DerpFest stuff
+$(call inherit-product, vendor/derp/config/common.mk)
+
+# Default notification/alarm sounds
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.config.notification_sound=Argon.ogg \
+    ro.config.alarm_alert=Hassium.ogg
+
+PRODUCT_PACKAGES += \
+    DerpLauncherQuickStepGo
+
+PRODUCT_DEXPREOPT_SPEED_APPS += \
+    DerpLauncherQuickStepGo
+
+
+PRODUCT_PACKAGES += \
+    DerpLauncherOverlay
+
+# Charger
+PRODUCT_PACKAGES += \
+    charger_res_images \
+    product_charger_res_images \
+    product_charger_res_images_vendor
+
+# Customizations
+PRODUCT_PACKAGES += \
+    DisplayCutoutEmulationNarrowOverlay \
+    DisplayCutoutEmulationWideOverlay \
+    KeyboardNoNavigationBarOverlay \
+    Launcher3NoHintOverlay \
+    NoCutoutOverlay \
+    NavigationBarMode2ButtonOverlay \
+    NavigationBarNoHintOverlay
+
+# Media
+PRODUCT_SYSTEM_PROPERTIES += \
+    media.recorder.show_manufacturer_and_model=true
+
+# SystemUI plugins
+PRODUCT_PACKAGES += \
+    QuickAccessWallet
