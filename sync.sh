a90e24d217519d8721be462f85f567932ce00ace
xc112lg <dtiven13@gmail.com>
2024-01-26 22:30:24 +0800

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -8,7 +8,7 @@ mkdir .repo/local_manifests
 cp scripts/roomservice.xml .repo/local_manifests
 #repo sync -c -j16 --force-sync --no-clone-bundle --no-tags
 repo sync -c --no-clone-bundle --optimized-fetch --prune --force-sync -j$(nproc --all)
-git clone https://github.com/LG-G6/android_kernel_lge_msm8996 -b lineage-21 kernel/lge/msm8996
+git clone https://github.com/xc112lg/msm8996_lge_kernel -b patch-1 kernel/lge/msm8996
 source build/envsetup.sh
 #mka clean
 #make clean
