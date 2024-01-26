bf30ea1f67a41ffe4833baa321fb8a942a7eeba7
xc112lg <dtiven13@gmail.com>
2024-01-26 15:07:37 +0800

    f

--- sync.sh
+++ sync.sh
@@ -4,11 +4,10 @@
 #rm -rf frameworks/base/
 rm -rf kernel/lge/msm8996
 rm -rf .repo/local_manifests
-repo init -u https://github.com/crdroidandroid/android.git -b 14.0 --git-lfs
 mkdir .repo/local_manifests
 cp scripts/roomservice.xml .repo/local_manifests
-repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
-#repo sync -c --no-clone-bundle --optimized-fetch --prune --force-sync -j$(nproc --all)
+#repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
+repo sync -c --no-clone-bundle --optimized-fetch --prune --force-sync -j$(nproc --all)
 source build/envsetup.sh
 #mka clean
 #make clean
