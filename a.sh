158fa0d0dc1f8410d519798e6a106211368b2838
xc112lg <dtiven13@gmail.com>
2024-01-28 11:35:17 +0800

    Update a.sh

--- a.sh
+++ a.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 rm -rf h870/* h872/* us997/* 
-rm cr10.1/*.zip
+rm crdroid10.1/*.zip
 crave pull cd9/out/target/product/*/*.zip
 crave pull cd9/out/target/product/*/recovery.img
 mv h870/recovery.img h870/recoveryh870.img
 mv h872/recovery.img h872/recoveryh872.img
 mv us997/recovery.img us997/recoveryus997.img
-mv h870/* h872/* us997/* ./cr10.1/ 
-cd cr10.1
+mv h870/* h872/* us997/* ./crdroid10.1/ 
+cd crdroid10.1
 ./multi_upload.sh
