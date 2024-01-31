ee9fd301c6cd284b1dffef9476b37fc765b86a1e
xc112lg <dtiven13@gmail.com>
2024-01-31 14:07:06 +0800

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -11,8 +11,8 @@ repo sync -c -j16 --force-sync --no-clone-bundle --no-tags
 #git clone https://github.com/xc112lg/msm8996_lge_kernel -b patch-1 kernel/lge/msm8996
 source build/envsetup.sh
 #mka clean
-make clean
-rm out/target/product/*/*.zip
+#make clean
+#rm out/target/product/*/*.zip
 source scripts/fixes.sh
 #lunch lineage_us997-userdebug
 #m -j16 bacon
