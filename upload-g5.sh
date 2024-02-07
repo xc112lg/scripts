2a395ff40a4eda27fdc5942c9656f133ff784ed3
WAR-DEPT <131593996+WAR-DEPT@users.noreply.github.com>
2024-02-07 13:25:32 +0530

    Delete upload-g5.sh

--- upload-g5.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cd ~/android/builds/banana/miatoll/ && rm *.*
-cd ~/android/cr/out/target/product/miatoll/ && mv *.zip *.md5sum ~/android/builds/cr/miatoll/
-cd ~/android/builds/cr/miatoll/ && rm banana_miatoll-ota*.zip *.md5sum
-
-sftp itubuild@frs.sourceforge.net:/home/pfs/project/itu/build/miatoll
