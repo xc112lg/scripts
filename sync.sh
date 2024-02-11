b46c979c76e7679634aee3e55e9816ac0c6474a8
xc112lg <dtiven13@gmail.com>
2024-02-11 09:05:42 +0800

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -21,8 +21,6 @@ rm -rf .repo/local_manifests
 mkdir .repo/local_manifests
 cp scripts/roomservice.xml .repo/local_manifests
 repo sync -c -j16 --force-sync --no-clone-bundle --no-tags
-#repo sync -c --no-clone-bundle --optimized-fetch --prune --force-sync -j$(nproc --all)
-#git clone https://github.com/xc112lg/msm8996_lge_kernel -b patch-1 kernel/lge/msm8996
 source build/envsetup.sh
 #mka clean
 #make clean
