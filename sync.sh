faed98ba9f3943681b2109178acbc709f69140c5
xc112lg <dtiven13@gmail.com>
2024-02-11 09:55:54 +0800

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Set the device and command
+DEVICE="all"  # Change to "h872" or another device if needed
+COMMAND="clean"  # Change to "build" or another command if needed
+
 # Function to wait for 1 second
 wait_one_second() {
     sleep 1
@@ -14,8 +18,8 @@ wait_one_second && sudo apt-get install -y ccache
 wait_one_second && export USE_CCACHE=1
 wait_one_second && ccache -M 100G
 wait_one_second && export CCACHE_DIR=/tmp/src/android/cc
-ls
 echo $CCACHE_DIR
+
 rm -rf frameworks/base/
 rm -rf device/lge
 rm -rf .repo/local_manifests
@@ -23,13 +27,36 @@ mkdir .repo/local_manifests
 cp scripts/roomservice.xml .repo/local_manifests
 repo sync -c -j16 --force-sync --no-clone-bundle --no-tags
 source build/envsetup.sh
-m clean
-#make clean
-#rm out/target/product/*/*.zip
 source scripts/fixes.sh
-lunch lineage_us997-userdebug
-m -j15 bacon
-lunch lineage_h870-userdebug
-m -j15 bacon
-lunch lineage_h872-userdebug
-m -j15 bacon
+
+# Check if command is "clean"
+if [ "$COMMAND" == "clean" ]; then
+    m clean
+fi
+
+# Check if device is set to "all"
+if [ "$DEVICE" == "all" ]; then
+    lunch lineage_us997-userdebug
+    m -j15 bacon
+    lunch lineage_h870-userdebug
+    m -j15 bacon
+    lunch lineage_h872-userdebug
+    m -j15 bacon
+elif [ "$DEVICE" == "h872" ]; then
+    lunch lineage_h872-userdebug
+
+    # Build if the command is not "clean"
+    if [ "$COMMAND" != "clean" ]; then
+        m -j15 bacon
+    fi
+else
+    # Build for the specified device
+    lunch "$DEVICE"
+
+    # Build if the command is not "clean"
+    if [ "$COMMAND" != "clean" ]; then
+        m -j15 bacon
+    fi
+fi
+
+# Additional build commands if needed
