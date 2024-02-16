596800221605502329bfbfaa61ed82cf6fbac727
xc112lg <dtiven13@gmail.com>
2024-02-16 19:08:24 +0800

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -43,8 +43,6 @@ fi
 # Check if device is set to "all"
 if [ "$DEVICE" == "all" ]; then
     echo "Building for all devices..."
-wait_one_second && rm -rf out/target/product/*/*.zip  device/lge/msm8996-common
-git clone https://github.com/xc112lg/android_device_lge_msm8996-common -b cd2 device/lge/msm8996-common
     lunch lineage_us997-userdebug
     m -j15 bacon
     lunch lineage_h870-userdebug
