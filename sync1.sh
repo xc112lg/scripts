9fe5e505edb29b03be328422948a295c33a47d1d
xc112lg <dtiven13@gmail.com>
2024-02-18 03:05:47 +0800

    Update sync1.sh

--- sync1.sh
+++ sync1.sh
@@ -3,7 +3,6 @@ rm -rf frameworks/base/
 rm -rf .repo/local_manifests
 # Clone local_manifests repository
 git clone https://github.com/krishnaspeace/local_manifests --depth 1 -b main .repo/local_manifests
-repo init --depth 1 -u https://github.com/crdroidandroid/android.git -b 14.0 --git-lfs
 repo sync -c -j16 --force-sync --no-clone-bundle --no-tags
 # removing non working fingerprint
 rm -rf vendor/fingerprint/opensource/interfaces
