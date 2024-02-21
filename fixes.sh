e352c67a98eba01cf0702e76d6f9c9e474fa8395
xc112lg <dtiven13@gmail.com>
2024-02-22 05:59:48 +0800

    Update fixes.sh

--- fixes.sh
+++ fixes.sh
@@ -1,8 +1,8 @@
 ## Cam fix for LG G6 and delete some line cause im stupid.
-cd frameworks/base/
-git fetch https://github.com/xc112lg/android_frameworks_base-1.git patch-17
-git cherry-pick a245af744209ccb9cb6ad6981f181fa8a9ba65c5
-cd ../../
+#cd frameworks/base/
+#git fetch https://github.com/xc112lg/android_frameworks_base-1.git patch-17
+#git cherry-pick a245af744209ccb9cb6ad6981f181fa8a9ba65c5
+#cd ../../
 
 # Mixer: adjust input volume levels
 cd device/lge/g6-common
