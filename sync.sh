47a133c5e7d1bb22382d339bb91708d98bfe6074
xc112lg <dtiven13@gmail.com>
2024-02-23 16:15:05 +0800

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -4,7 +4,7 @@
 repo forall -c 'git reset --hard ; git clean -fdx'
 rm -rf .repo/local_manifests
 rm -rf device/lge/
-
+mkdir -p .repo/local_manifests
 cp scripts/roomservice.xml .repo/local_manifests
 repo sync -c -j16 --force-sync --no-clone-bundle --no-tags
 
