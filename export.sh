75d5bb10bcab77919b35c7345495c45a85404eb0
xc112lg <152837944+xc112lg@users.noreply.github.com>
2024-03-04 05:35:51 +0800

    test

---
+++ export.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+export MAINTAINER="xc112lg"
+export OEM="LG"
+export DEVICE="LG G6"
+export BUILDTYPE="userdebug" # Specify the build type
+export FORUM="https://xdaforums.com/t/unofficial-build-crdroid10-1-android-14-for-the-lg-g6.4648250/"
+export GAPPS="https://sourceforge.net/projects/crdroid/files/other/"
+export BOOTLOADER="https://github.com/xc112lg/crdroid10.1/releases"
+export RECOVERY="https://github.com/xc112lg/crdroid10.1/releases"
+export PAYPAL="https://www.paypal.com/paypalme/xc112xda"
+export DT="https://github.com/LineageOS/android_device_lge_msm8996-common"
+export COMMON_DT="https://github.com/LineageOS/android_device_lge_g6-common"
+export KERNEL="https://github.com/lineageos/android_kernel_lge_msm8996"
