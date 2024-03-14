3b8618edf77fcd9ec83299be20f1f14f4603b624
xc112lg <152837944+xc112lg@users.noreply.github.com>
2024-03-14 09:48:34 +0800

    Update clean.sh

--- clean.sh
+++ clean.sh
@@ -3,29 +3,7 @@ cd frameworks/base/
 git reset --hard
 cd ../../
 
-# Mixer: adjust input volume levelsa
-cd device/lge/g6-common
-git reset --hard
-cd ../../../
-
-cd kernel/lge/msm8996
-# Fix LTO
-git reset --hard
-cd ../../../
-
 #some fixes will be push to source fter testingg
 cd device/lge/msm8996-common
 git reset --hard
 cd ../../../
-
-cd packages/apps/Updater
-git reset --hard
-cd ../../../
-
-cd vendor/lineage/
-git reset --hard @{u}
-cd ../../
-
-
-
-
