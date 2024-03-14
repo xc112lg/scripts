ff3d43b01849d5e1c8434ceb276ba30aee8de034
xc112lg <152837944+xc112lg@users.noreply.github.com>
2024-03-14 09:38:10 +0800

    Update clean.sh

--- clean.sh
+++ clean.sh
@@ -1,25 +1,25 @@
 ## Cam fix for LG G6 and delete some line cause im stupid.
 cd frameworks/base/
-git reset --hard @{u}
+git reset --hard
 cd ../../
 
 # Mixer: adjust input volume levelsa
 cd device/lge/g6-common
-git reset --hard @{u}
+git reset --hard
 cd ../../../
 
 cd kernel/lge/msm8996
 # Fix LTO
-git reset --hard @{u}
+git reset --hard
 cd ../../../
 
 #some fixes will be push to source fter testingg
 cd device/lge/msm8996-common
-git reset --hard @{u}
+git reset --hard
 cd ../../../
 
 cd packages/apps/Updater
-git reset --hard @{u}
+git reset --hard
 cd ../../../
 
 cd vendor/lineage/
