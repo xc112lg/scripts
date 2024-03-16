c91a8b027cf030c620e32f9e332ff2d534c011b9
xc112lg <152837944+xc112lg@users.noreply.github.com>
2024-03-16 18:45:05 +0800

    Update fixes.sh

--- fixes.sh
+++ fixes.sh
@@ -16,26 +16,3 @@ sleep 1 && git fetch https://github.com/xc112lg/msm8996_lge_kernel.git patch-1
 sleep 1 && git cherry-pick 581d1240a1ac99ebbf172ec95b8d7f4f40ca4d21
 cd ../../../
 
-#some fixes will be push to source fter testingg
-cd device/lge/msm8996-common
-sleep 1 &&git fetch https://github.com/xc112lg/android_device_lge_msm8996-common.git cd10
-sleep 1 &&git cherry-pick aef6632c220ec671b69ed3564d37f74cca295ce2 
-sleep 1 &&git cherry-pick 7ac9890a15cbf0be818fa00c8374620fb5c737c1
-sleep 1 &&git cherry-pick c6c8c172e3f92b628465b5888c13b6a29fbc4383
-cd ../../../
-
-cd packages/apps/Updater
-sleep 1 &&git fetch https://github.com/xc112lg/android_packages_apps_Updater.git patch-1
-sleep 1 &&git cherry-pick af70aa09ee94e7db085cd5777229b82f3abca313
-cd ../../../
-
-cd vendor/lineage/
-sleep 1 &&git fetch https://github.com/xc112lg/android_vendor_crdroid.git patch-1
-sleep 1 &&git cherry-pick be2f71d81e21608f82376fd5266ce95d3266a411
-cd ../../
-
-
-#added crdroid setting 
-wget -N -P device/lge/msm8996-common/overlay/frameworks/base/core/res/res/values/ https://github.com/crdroidandroid/android_frameworks_base/raw/14.0/core/res/res/values/cr_config.xml
-wget -N -P device/lge/msm8996-common/overlay/frameworks/base/packages/SystemUI/res/values/ https://github.com/crdroidandroid/android_frameworks_base/raw/14.0/packages/SystemUI/res/values/cr_config.xml
-
