72566eb96ef5ce243b21d5c87be067ba5316faff
xc112lg <152837944+xc112lg@users.noreply.github.com>
2024-03-18 04:36:08 +0800

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -5,47 +5,29 @@ COMMAND="${2:-build}"  # If no value is provided, default to "build"
 DELZIP="${3}"
 echo $PWD
 echo $PWD
-mkdir -p cc
-mkdir -p c
-cp -f cc/ccache.conf c/ccache.conf 
-time ls -1 cc | xargs -I {} -P 5 -n 1 rsync -au cc/{} c/
-ccache -s
-# Update and install ccache
-sudo apt-get update -y
-sudo apt-get install -y apt-utils
-sudo apt-get install -y ccache
-export USE_CCACHE=1
-ccache -M 100G
-export CCACHE_DIR=${PWD}/cc
-ccache -s
-ccache -o compression=false
-ccache --show-config | grep compression
-echo $CCACHE_DIR
-echo $CCACHE_EXEC
-time ls -1 c | xargs -I {} -P 5 -n 1 rsync -au c/{} cc/
-cp -f c/ccache.conf cc/ccache.conf 
-ccache -o compression=false
-ccache --show-config | grep compression
+repo init -u https://github.com/LineageOS/android.git -b lineage-18.1 --git-lfs
 
-ccache -s
-## Remove existing build artifactsa
-if [ "$DELZIP" == "delzip" ]; then
-    rm -rf out/target/product/*/*.zip
+# Define the log file path
+log_file="deleted_repos.log"
+
+# Sync repositories and capture the output
+output=$(repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags 2>&1)
+
+# Check if there are any failing repositories
+if echo "$output" | grep -q "error:"; then
+    echo "Deleting failing repositories..."
+    # Extract failing repositories from the error message and log the deletion
+    while IFS= read -r line; do
+        repo_name=$(echo "$line" | awk '{print $NF}')
+        echo "Deleted repository: $repo_name" >> "$log_file"
+        rm -rf "$repo_name"
+    done <<< "$(echo "$output" | grep "error:")"
+    
+    # Re-sync all repositories after deletion
+    echo "Re-syncing all repositories..."
+    repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
+else
+    echo "All repositories synchronized successfully."
 fi
 
-#git clean -fdX
-#rm -rf frameworks/base/
-rm -rf .repo/local_manifests
-#rm -rf device/lge/
-#rm -rf kernel/lge/msm8996
-mkdir -p .repo/local_manifests
-cp scripts/roomservice.xml .repo/local_manifests
-source scripts/clean.sh
-repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
-
-#wget -O a.py https://raw.githubusercontent.com/xc112lg/crdroid10.1/main/a.py
-#chmod +x a.py
-chmod +x scripts/export.sh
-#python3 a.py
-source scripts/fixes.sh
 source build/envsetup.sh
