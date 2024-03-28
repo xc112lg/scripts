e9eded5edbc36b7b54c205a2529952d693d4752c
xc112lg <152837944+xc112lg@users.noreply.github.com>
2024-03-29 04:44:58 +0800

    Update fixes.sh

--- fixes.sh
+++ fixes.sh
@@ -16,8 +16,8 @@ sleep 1 && git fetch https://github.com/xc112lg/msm8996_lge_kernel.git patch-1
 sleep 1 && git cherry-pick 581d1240a1ac99ebbf172ec95b8d7f4f40ca4d21
 cd ../../../
 
-cd vendor/lge/msm8996-common/
+#cd vendor/lge/msm8996-common/
 
-sleep 1 && git fetch https://github.com/xc112lg/proprietary_vendor_lge_msm8996-common.git patch-1
-sleep 1 && git cherry-pick bb7e1512b25af2aa66773fd9138b63b560af254c
-cd ../../../
+#sleep 1 && git fetch https://github.com/xc112lg/proprietary_vendor_lge_msm8996-common.git patch-1
+#sleep 1 && git cherry-pick bb7e1512b25af2aa66773fd9138b63b560af254c
+#cd ../../../
