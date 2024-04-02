1789f1ffc83fdd3faaf1ff7577e3aeac2992fa1a
xc112lg <152837944+xc112lg@users.noreply.github.com>
2024-04-02 19:17:20 +0800

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -131,7 +131,7 @@ elif [ "$DEVICE" == "h872" ]; then
     echo "Building for h872..."
 export BUILD_DEVICE="h872"
 	echo "${MAKEFILE}_h872${RELEASETYPE1}-userdebug"
-
+    m clean
     lunch ${MAKEFILE}_h872${RELEASETYPE1}-eng
     m installclean
     ${COM1} -j$(nproc --all) ${COM2}
