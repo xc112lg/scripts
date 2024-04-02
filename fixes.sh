e65788e80f314394b8bc91c59c9aa683bd1edc20
xc112lg <152837944+xc112lg@users.noreply.github.com>
2024-04-03 02:38:37 +0800

    Update fixes.sh

--- fixes.sh
+++ fixes.sh
@@ -28,3 +28,10 @@ sleep 1 &&git fetch https://github.com/xc112lg/android_device_lge_msm8996-common
 sleep 1 &&git cherry-pick 7ef8ee92f398052a9d6351e4d7157e8474401f5b
 
 cd ../../../
+
+cd vendor/lge/msm8996-common/
+
+sleep 1 && git fetch https://github.com/xc112lg/proprietary_vendor_lge_msm8996-common.git patch-2
+sleep 1 && git cherry-pick b7ae264df1d799c5d635bada6afbc3714df75cdb 
+sleep 1 && git cherry-pick 1efc7fd60e02b78c0ce03b184b1c0f485100cd18
+cd ../../../
