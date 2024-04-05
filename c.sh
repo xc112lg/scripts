dcc5f5a50d3242d19c91ba33b7f508b4a19d4658
xc112lg <152837944+xc112lg@users.noreply.github.com>
2024-04-05 18:18:41 +0800

    Create c.sh

---
+++ c.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get install python python3-pip wget git 
+pip3 install payload_dumper
+sudo apt-get install python3-protobuf
+
+mkdir ~/android
+mkdir ~/android/system_dump/
+cd ~/android/system_dump/
+wget -O archive.zip https://bn.d.miui.com/V14.0.5.0.TMAMIXM/miui_ISHTARGlobal_V14.0.5.0.TMAMIXM_6f43e09971_13.0.zip
+unzip -j archive.zip  -d .
+cd ..
+cd ..
+git clone https://github.com/LineageOS/android_prebuilts_extract-tools android/prebuilts/extract-tools
+git clone https://github.com/LineageOS/android_tools_extract-utils android/tools/extract-utils
+git clone https://github.com/LineageOS/android_system_update_engine android/system/update_engine
+./android/prebuilts/extract-tools/linux-x86/bin/ota_extractor --payload android/system_dump/payload.bin
