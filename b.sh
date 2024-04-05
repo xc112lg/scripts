aca7c797faba089328a21fc0e8d23a4d0c7aa145
xc112lg <152837944+xc112lg@users.noreply.github.com>
2024-04-05 19:56:24 +0800

    Update b.sh

--- b.sh
+++ b.sh
@@ -7,18 +7,18 @@
   && sudo apt update \
   && sudo apt install gh -y
 
-mkdir workspace && cd workspace
+mkdi -p workspace && cd workspace
   git clone --depth=1 https://github.com/Jiovanni-dump/xiaomi_ishtar_dump -b missi_phone_global-user-13-TKQ1.221114.001-V14.0.1.0.TMAMIXM-release-keys ./firmware-dump
- cd /workspace
+
   git clone --depth=1 https://github.com/jacksproject/device_xiaomi_ishtar -b 13.0 ./android/device/ishtar/xiaomi_ultra_13
 
- cd /workspace
+
   git clone --depth=1 https://github.com/LineageOS/android_tools_extract-utils -b lineage-20.0 ./android/tools/extract-utils
   echo "Done cloning extract-utils."
   git clone --depth=1  https://github.com/LineageOS/android_prebuilts_extract-tools -b lineage-20.0 ./android/prebuilts/extract-tools
   echo "Done cloning extract-tools."
 
-cd /workspace
+
   chmod a+x android/device/ishtar/xiaomi_ultra_13/setup-makefiles.sh
   cd android/device/ishtar/xiaomi_ultra_13/
   bash extract-files.sh ${GITHUB_WORKSPACE}/workspace/firmware-dump/
