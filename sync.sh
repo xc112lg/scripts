a43708490b9b5f76283feb74b7e8cbbaf54a41bf
xc112lg <152837944+xc112lg@users.noreply.github.com>
2024-04-24 21:45:15 +0800

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -5,15 +5,15 @@ cp scripts/roomservice.xml .repo/local_manifests/
 cp scripts/local_manifests.xml .repo/local_manifests/
 
 
-#git clone https://github.com/krishnaspeace/local_manifests.git --depth 1 -b main .repo/local_manifests
+git clone https://github.com/krishnaspeace/local_manifests.git --depth 1 -b main .repo/local_manifests
 rm -rf vendor/fingerprint/opensource/interfaces
 git clone https://github.com/xiaomi-msm8953-devs/android_vendor_fingerprint_opensource_interfaces vendor/fingerprint/opensource/interfaces
 
 
-# /opt/crave/resync.sh
+/opt/crave/resync.sh
 source build/envsetup.sh
 lunch
-# lunch lineage_ysl-ap1a-userdebug
-# m bacon
-# lunch lineage_h872-ap1a-userdebug
-# m bacon
+lunch lineage_ysl-ap1a-userdebug
+m bacon
+lunch lineage_h872-ap1a-userdebug
+m bacon
