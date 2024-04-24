351abba45feb73ae6da0b89e399e2b61c3466756
xc112lg <152837944+xc112lg@users.noreply.github.com>
2024-04-24 09:52:46 +0800

    test

--- sync.sh
+++ sync.sh
@@ -1,15 +1,9 @@
 #!/bin/bash
 
-rm -rf build/make/
-rm -rf frameworks/base/
-rm -rf device/lge/
-rm -rf hardware/lge
-rm -rf kernel/lge/msm8996/
-rm -rf vendor/lge/
-rm -rf vendor/lineage/
 
-repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
+
+/opt/crave/resync.sh
 source build/envsetup.sh
 
-source scripts/fixes.sh
-source scripts/extras.sh
+lunch lineage_h872-ap1a-userdebug
+m bacon
