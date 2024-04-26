236c052785561f69a883d7f76343b18cb98486a6
xc112lg <152837944+xc112lg@users.noreply.github.com>
2024-04-26 18:39:05 +0800

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 rm -rf .repo/local_manifests
+cp scripts/X01BD.xml .repo/local_manifests/
 mkdir .repo/local_manifests
 cp scripts/roomservice.xml .repo/local_manifests/
 #cp scripts/local_manifests.xml .repo/local_manifests/
 cp scripts/eureka_deps.xml .repo/local_manifests/
 cp scripts/x.xml .repo/local_manifests/
-#cp scripts/X01BD.xml .repo/local_manifests/
+
 
 
 
@@ -65,5 +66,38 @@ lunch lineage_a10-ap1a-userdebug
 m bacon
 lunch lineage_X00TD-ap1a-userdebug
 m bacon
-# lunch lineage_X01BD-ap1a-userdebug
-# m bacon
+
+rm -rf .repo/local_manifests
+cp scripts/X01BD.xml .repo/local_manifests/
+main() {
+    # Run repo sync command and capture the output
+    repo sync -c -j64 --force-sync --no-clone-bundle --no-tags 2>&1 | tee /tmp/output.txt
+
+    # Check if there are any failing repositories
+    if grep -q "Failing repos:" /tmp/output.txt ; then
+        echo "Deleting failing repositories..."
+        # Extract failing repositories from the error message and echo the deletion path
+        while IFS= read -r line; do
+            # Extract repository name and path from the error message
+            repo_info=$(echo "$line" | awk -F': ' '{print $NF}')
+            repo_path=$(dirname "$repo_info")
+            repo_name=$(basename "$repo_info")
+            # Echo the deletion path
+            echo "Deleted repository: $repo_info"
+            # Save the deletion path to a text file
+            echo "Deleted repository: $repo_info" > deleted_repositories.txt
+            # Delete the repository
+            rm -rf "$repo_path/$repo_name"
+        done <<< "$(cat /tmp/output.txt | awk '/Failing repos:/ {flag=1; next} /Try/ {flag=0} flag')"
+
+        # Re-sync all repositories after deletion
+        echo "Re-syncing all repositories..."
+        repo sync -c -j${CORE} --force-sync --no-clone-bundle --no-tags
+    else
+        echo "All repositories synchronized successfully."
+    fi
+}
+
+main $*
+lunch lineage_X01BD-ap1a-userdebug
+m bacon
