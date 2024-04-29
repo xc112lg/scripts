942ee33a2a51c5dc23ceffe3e081cc4562a4081c
xc112lg <152837944+xc112lg@users.noreply.github.com>
2024-04-29 19:18:16 +0800

    g

--- cleanmanifest.sh
+++ cleanmanifest.sh
@@ -2,9 +2,9 @@
 if [ -d .repo/local_manifests ]; then
 
 # Extract and echo the paths
-echo "$(xmlstarlet sel -t -m "//project" -v "@path" -n .repo/local_manifest/*.xml)"
+echo "$(xmlstarlet sel -t -m "//project" -v "@path" -n .repo/local_manifests/*.xml)"
 
-paths=$(xmlstarlet sel -t -m "//project" -v "@path" -n .repo/local_manifest/*.xml)
+paths=$(xmlstarlet sel -t -m "//project" -v "@path" -n .repo/local_manifests/*.xml)
 
 echo "Paths to be deleted:"
 echo "$paths"
