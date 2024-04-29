19525cc241c79ad6269b585f2c590d51a10201e5
xc112lg <152837944+xc112lg@users.noreply.github.com>
2024-04-29 21:27:37 +0800

    Update resync.sh

--- resync.sh
+++ resync.sh
@@ -1,13 +1,11 @@
+#!/bin/bash
+
 main() {
-
-
-
-
     # Run repo sync command and capture the output
-    repo sync -c -j64 --force-sync --no-clone-bundle --no-tags 2>&1 | tee /tmp/output.txt
+    repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags 2>&1 | tee /tmp/output.txt
 
     # Check if there are any failing repositories
-    if grep -q -e "Failing repos:" -e "error:" /tmp/output.txt ; then
+    if grep -q "Failing repos:" /tmp/output.txt || grep -q "error:" /tmp/output.txt ; then
         echo "Deleting failing repositories..."
         # Extract failing repositories from the error message and echo the deletion path
         while IFS= read -r line; do
@@ -18,15 +16,14 @@ main() {
             # Echo the deletion path
             echo "Deleted repository: $repo_info"
             # Save the deletion path to a text file
-            echo "Deleted repository: $repo_info" > deleted_repositories.txt
+            echo "Deleted repository: $repo_info" >> deleted_repositories.txt
             # Delete the repository
             rm -rf "$repo_path/$repo_name"
-            done <<< "$(cat /tmp/output.txt | awk '/Failing repos:/ || /error: / {flag=1; next} /Try/ {flag=0} flag')"
-
-#        done <<< "$(cat /tmp/output.txt | awk '/Failing repos:/ {flag=1; next} /Try/ {flag=0} flag')"
+        done <<< "$(cat /tmp/output.txt | awk '/Failing repos:/ {flag=1; next} /Try/ {flag=0} flag')"
+        
         # Re-sync all repositories after deletion
         echo "Re-syncing all repositories..."
-        repo sync -c -j64 --force-sync --no-clone-bundle --no-tags
+        repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
     else
         echo "All repositories synchronized successfully."
     fi
