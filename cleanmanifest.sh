d3827022994566fe36fdfd3d2e795e81ae575277
xc112lg <152837944+xc112lg@users.noreply.github.com>
2024-04-30 07:19:41 +0800

    Update cleanmanifest.sh

--- cleanmanifest.sh
+++ cleanmanifest.sh
@@ -1,16 +1,30 @@
 # Check if the directory exists
 if [ -d .repo/local_manifests ]; then
     # Extract and echo the paths
-    paths=$(xmlstarlet sel -t -m "//project/@path" -v .repo/local_manifests/*.xml)
-    echo "Paths to be deleted:"
-    echo "$paths"
+    # paths=$(xmlstarlet sel -t -m "//project/@path" -v .repo/local_manifests/*.xml)
+    # echo "Paths to be deleted:"
+    # echo "$paths"
 
-    # Remove each file
-    for path in $paths; do
-        rm -rf "$path"
-        #sudo find "$path" -delete
-        echo "Deleted: $path"
-    done
+    # # Remove each file
+    # for path in $paths; do
+    #     rm -rf "$path"
+    #     #sudo find "$path" -delete
+    #     echo "Deleted: $path"
+    # done
+
+
+
+    # Extract and echo the paths
+paths=$(xmlstarlet sel -t -v "//project/@path" .repo/local_manifests/*.xml)
+echo "Paths to be deleted:"
+echo "$paths"
+
+# Remove each file
+for path in $paths; do
+    rm -rf "$path"
+    echo "Deleted: $path"
+done
+
 else
     echo "Skipping command. .repo/local_manifests directory does not exist."
 fi
