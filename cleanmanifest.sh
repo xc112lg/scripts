03e91b3818935ccad3ca79a41e9775ac41355534
xc112lg <152837944+xc112lg@users.noreply.github.com>
2024-04-30 04:58:56 +0800

    Update cleanmanifest.sh

--- cleanmanifest.sh
+++ cleanmanifest.sh
@@ -7,16 +7,9 @@ if [ -d .repo/local_manifests ]; then
 
     echo "Paths to be deleted:"
     echo "$paths"
-
-    # Remove each file except for paths starting with "kernel", "vendor", or "hardware"
-  #  for path in $paths; do
-    #    if [[ ! "$path" =~ ^(vendor|hardware) ]]; then
             rm -rf "$path"
             echo "Deleted: $path"
-        else
-            echo "Skipping deletion for path: $path"
-        fi
-    done
+
 else
     echo "Skipping command. .repo/local_manifests directory does not exist."
 fi
