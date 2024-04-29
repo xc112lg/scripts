eeca1495cab8b539140914783fe163f748b98b02
xc112lg <152837944+xc112lg@users.noreply.github.com>
2024-04-29 18:32:09 +0800

    Update a.sh

--- a.sh
+++ a.sh
@@ -3,23 +3,14 @@
 echo "$(xmlstarlet sel -t -m "//project[starts-with(@path, 'device')]" -v "@path" -n .repo/local_manifests/*.xml)"
 
 
-# Check if the directory exists
-if [ -d .repo/local_manifests ]; then
-    # Extract and echo the paths
-    paths=$(xmlstarlet sel -t -m "//project/@path" -v .repo/local_manifest/*.xml)
-    echo "Paths to be deleted:"
-    echo "$paths"
+# Extract and echo the paths
+paths=$(xmlstarlet sel -t -m "//project[starts-with(@path, 'device' , 'prebuilt')]" -v "@path" -n .repo/local_manifests/*.xml)
+echo "Paths to be deleted:"
+echo "$paths"
 
-    # Remove each file except those containing "kernel" or "vendor"
-    for path in $paths; do
-        if [[ ! "$path" =~ "kernel" && ! "$path" =~ "vendor" ]]; then
-            rm -rf "$path"
-            echo "Deleted: $path"
-        fi
-    done
-else
-    echo "Skipping command. .repo/local_manifests directory does not exist."
-fi
-
-
+# Remove each file
+for path in $paths; do
+    rm -rf "$path"
+    echo "Deleted: $path"
+done
 
