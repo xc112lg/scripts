7034643559308edd6970cf710166553f837beab1
xc112lg <152837944+xc112lg@users.noreply.github.com>
2024-04-29 17:34:30 +0800

    Update resync.sh

--- resync.sh
+++ resync.sh
@@ -1,4 +1,22 @@
 main() {
+
+# Check if the directory exists
+if [ -d .repo/local_manifests ]; then
+    # Extract and echo the paths
+    paths=$(xmlstarlet sel -t -m "//project[starts-with(@path, 'device')]" -v "@path" -n .repo/local_manifest/*.xml)
+    echo "Paths to be deleted:"
+    echo "$paths"
+
+    # Remove each file
+    for path in $paths; do
+        rm -rf "$path"
+        echo "Deleted: $path"
+    done
+else
+    echo "Skipping command. .repo/local_manifests directory does not exist."
+fi
+
+
     # Run repo sync command and capture the output
     repo sync -c -j64 --force-sync --no-clone-bundle --no-tags 2>&1 | tee /tmp/output.txt
 
