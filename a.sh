c1949f6740fd03edd1935a25b890c962fc90234a
xc112lg <152837944+xc112lg@users.noreply.github.com>
2024-04-29 17:29:14 +0800

    Update a.sh

--- a.sh
+++ a.sh
@@ -1,2 +1,5 @@
 echo "$(xmlstarlet sel -t -m "//project" -v "@path" -n .repo/local_manifests/*.xml)"
 
+echo "$(xmlstarlet sel -t -m "//project[starts-with(@path, 'device')]" -v "@path" -n .repo/local_manifests/*.xml)"
+
+
