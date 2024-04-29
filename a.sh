2029e87bd280e0fcca8af8ba42af5dfd25964d85
xc112lg <152837944+xc112lg@users.noreply.github.com>
2024-04-29 17:23:07 +0800

    Update a.sh

--- a.sh
+++ a.sh
@@ -1,2 +1,7 @@
 cd scripts
 echo "$(xmlstarlet sel -t -m "//project" -v "@path" -n roomservice.xml)"
+echo "$(xmlstarlet sel -t -m "//project" -v "@path" -n X01BD.xml)"
+echo "$(xmlstarlet sel -t -m "//project" -v "@path" -n x.xml)"
+echo "$(xmlstarlet sel -t -m "//project" -v "@path" -n lge_sdm845.xml)"
+echo "$(xmlstarlet sel -t -m "//project" -v "@path" -n eureka_deps.xml)"
+
