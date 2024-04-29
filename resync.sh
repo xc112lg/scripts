29b8a51fa7fcc556578ab72bd51d2703719274cb
xc112lg <152837944+xc112lg@users.noreply.github.com>
2024-04-29 19:04:06 +0800

    Update resync.sh

--- resync.sh
+++ resync.sh
@@ -1,22 +1,6 @@
 main() {
 
-# Check if the directory exists
-if [ -d .repo/local_manifests ]; then
 
-# Extract and echo the paths
-paths=$(xmlstarlet sel -t -m "//project[starts-with(@path, 'device') or starts-with(@path, 'prebuilt')]" -v "@path" -n .repo/local_manifests/*.xml)
-
-echo "Paths to be deleted:"
-echo "$paths"
-
-# Remove each file
-for path in $paths; do
-    rm -rf "$path"
-    echo "Deleted: $path"
-done
-else
-    echo "Skipping command. .repo/local_manifests directory does not exist."
-fi
 
 
     # Run repo sync command and capture the output
