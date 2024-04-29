ad21359a677cf7d6a46f9c14872edc937796116b
xc112lg <152837944+xc112lg@users.noreply.github.com>
2024-04-29 08:07:36 +0800

    Update upload-g5.sh

--- upload-g5.sh
+++ upload-g5.sh
@@ -1,20 +1,105 @@
-#!/bin/bash
+rm -rf .repo/local_manifests 
+#cp scripts/local_manifests.xml .repo/local_manifests/
 
-cd ~/android/builds/lineage/rs988/ && rm *.*
-cd ~/android/los21/out/target/product/rs988/ && mv *.zip *.md5sum ~/android/builds/lineage/rs988/
-cd ~/android/builds/lineage/rs988/ && rm lineage_rs988-ota*.zip *.md5sum
-sftp bernardobas@frs.sourceforge.net:/home/pfs/project/lg-g5/LineageOS\ 21.0/RS988/ <<< $'put *.*'
+source scripts/resync.sh
 
-cd ~/android/builds/lineage/h850/ && rm *.*
-cd ~/android/los21/out/target/product/h850/ && mv *.zip *.md5sum ~/android/builds/lineage/h850/
-cd ~/android/builds/lineage/h850/ && rm lineage_h850-ota*.zip *.md5sum
-sftp bernardobas@frs.sourceforge.net:/home/pfs/project/lg-g5/LineageOS\ 21.0/H850/ <<< $'put *.*'
+DEVICE="${1:-all}"  
 
-cd ~/android/builds/lineage/h830/ && rm *.*
-cd ~/android/los21/out/target/product/h830/ && mv *.zip *.md5sum ~/android/builds/lineage/h830/
-cd ~/android/builds/lineage/h830/ && rm lineage_h830-ota*.zip *.md5sum
-sftp bernardobas@frs.sourceforge.net:/home/pfs/project/lg-g5/LineageOS\ 21.0/H830/ <<< $'put *.*'
+source build/envsetup.sh
+#make installclean
+ls  out/target/product
+#lunch lineage_ysl-ap1a-userdebug
+#m bacon
+breakfast gsi_arm64 userdebug
 
-echo -e "\nLG G5 builds completed and uploaded\n";
+time mka
+if [ "$DEVICE" == "h872" ]; then
+rm -rf .repo/local_manifests
+mkdir .repo/local_manifests
+cp scripts/roomservice.xml .repo/local_manifests/
+sudo find device -delete
+source scripts/resync.sh
 
-cd ~/android/los21/
+
+cd device/lge/msm8996-common
+sleep 1 &&git fetch https://github.com/xc112lg/android_device_lge_msm8996-common.git patch-1
+sleep 1 &&git cherry-pick 7ef8ee92f398052a9d6351e4d7157e8474401f5b
+
+cd ../../../
+
+cd vendor/lge/msm8996-common/
+
+sleep 1 && git fetch https://github.com/xc112lg/proprietary_vendor_lge_msm8996-common.git patch-2
+sleep 1 && git cherry-pick b7ae264df1d799c5d635bada6afbc3714df75cdb 
+sleep 1 && git cherry-pick 1efc7fd60e02b78c0ce03b184b1c0f485100cd18
+cd ../../../
+
+
+
+
+source build/envsetup.sh
+lunch lineage_h872-ap1a-userdebug
+make installclean
+time m bacon
+fi
+
+if [ "$DEVICE" == "a10" ]; then
+rm -rf .repo/local_manifests
+mkdir .repo/local_manifests
+cp scripts/eureka_deps.xml .repo/local_manifests/
+sudo find device -delete
+source scripts/resync.sh
+source build/envsetup.sh
+lunch lineage_a10-ap1a-userdebug
+make installclean
+time m bacon
+fi
+
+if [ "$DEVICE" == "X00TD" ]; then
+rm -rf .repo/local_manifests
+mkdir .repo/local_manifests
+
+rm -rf .repo/local_manifests
+mkdir .repo/local_manifests
+cp scripts/x.xml .repo/local_manifests/
+sudo find device -delete
+source scripts/resync.sh
+
+
+source build/envsetup.sh
+lunch lineage_X00TD-ap1a-userdebug
+make installclean
+time m bacon
+fi
+
+
+if [ "$DEVICE" == "X01BD" ]; then
+rm -rf .repo/local_manifests
+mkdir .repo/local_manifests
+cp scripts/X01BD.xml .repo/local_manifests/
+sudo find device -delete
+source scripts/resync.sh
+source build/envsetup.sh
+lunch lineage_X01BD-ap1a-userdebug
+make installclean
+time m bacon
+
+fi
+
+if [ "$DEVICE" == "judypn" ]; then
+rm -rf .repo/local_manifests
+mkdir .repo/local_manifests
+
+cp scripts/lge_sdm845.xml .repo/local_manifests/
+cp scripts/setup.sh  .repo/local_manifests/
+cp scripts/rom.sh .repo/local_manifests/
+sudo find device -delete
+
+
+
+bash .repo/local_manifests/setup.sh && source scripts/resync.sh && bash .repo/local_manifests/rom.sh
+
+source build/envsetup.sh
+# Build the ROM
+lunch lineage_judypn-ap1a-eng && make installclean &&time  mka bacon
+fi
