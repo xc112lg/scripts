12915a3ffccb18a583987697029023f00aec72d5
xc112lg <152837944+xc112lg@users.noreply.github.com>
2024-04-29 17:32:08 +0800

    Update a.sh

--- a.sh
+++ a.sh
@@ -1,5 +1,18 @@
-echo "$(xmlstarlet sel -t -m "//project" -v "@path" -n .repo/local_manifests/*.xml)"
+
 
 echo "$(xmlstarlet sel -t -m "//project[starts-with(@path, 'device')]" -v "@path" -n .repo/local_manifests/*.xml)"
+
+
+# Extract and echo the paths
+paths=$(xmlstarlet sel -t -m "//project[starts-with(@path, 'device')]" -v "@path" -n .repo/local_manifests/*.xml)
+echo "Paths to be deleted:"
+echo "$paths"
+
+# Remove each file
+for path in $paths; do
+    rm -rf "$path"
+    echo "Deleted: $path"
+done
+
 
 
