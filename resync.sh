a155dfb6c4343f4499eee21074e68685d87a0338
xc112lg <152837944+xc112lg@users.noreply.github.com>
2024-05-04 09:48:44 +0800

    Update resync.sh

--- resync.sh
+++ resync.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
+# Copyright (c) 2016-2024 Crave.io Inc. All rights reserved
 
 main() {
     # Run repo sync command and capture the output
-    repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags 2>&1 | tee /tmp/output.txt
+    find .repo -name '*.lock' -delete
+    repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags --prune 2>&1 | tee /tmp/output.txt
 
     # Check if there are any failing repositories
-    if grep -q "Failing repos:" /tmp/output.txt || grep -q "error:" /tmp/output.txt ; then
+    if grep -q "Failing repos:" /tmp/output.txt ; then
         echo "Deleting failing repositories..."
         # Extract failing repositories from the error message and echo the deletion path
         while IFS= read -r line; do
@@ -16,14 +18,15 @@ main() {
             # Echo the deletion path
             echo "Deleted repository: $repo_info"
             # Save the deletion path to a text file
-            echo "Deleted repository: $repo_info" >> deleted_repositories.txt
+            echo "Deleted repository: $repo_info" > deleted_repositories.txt
             # Delete the repository
             rm -rf "$repo_path/$repo_name"
         done <<< "$(cat /tmp/output.txt | awk '/Failing repos:/ {flag=1; next} /Try/ {flag=0} flag')"
-        
+
         # Re-sync all repositories after deletion
         echo "Re-syncing all repositories..."
-        repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
+        find .repo -name '*.lock' -delete
+        repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags --prune
     else
         echo "All repositories synchronized successfully."
     fi
