18aa45d036ba0c107af5134fcf62253381a7a75a
xc112lg <152837944+xc112lg@users.noreply.github.com>
2024-05-06 09:02:20 +0800

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -1,4 +1,4 @@
-
+#can cause issue with other rom if they change it
 #cp scripts/local_manifests.xml .repo/local_manifests/
 
 #source scripts/resync.sh
@@ -13,71 +13,71 @@
 #breakfast gsi_arm64 userdebug
 
 #time mka
-source scripts/cleanmanifest.sh
-rm -rf .repo/local_manifests
-mkdir .repo/local_manifests
-cp scripts/roomservice.xml .repo/local_manifests/
+# source scripts/cleanmanifest.sh
+# rm -rf .repo/local_manifests
+# mkdir .repo/local_manifests
+# cp scripts/roomservice.xml .repo/local_manifests/
 
-source scripts/resync.sh
-
-
-cd device/lge/msm8996-common
-sleep 1 &&git fetch https://github.com/xc112lg/android_device_lge_msm8996-common.git patch-1
-sleep 1 &&git cherry-pick 7ef8ee92f398052a9d6351e4d7157e8474401f5b
-
-cd ../../../
-
-cd vendor/lge/msm8996-common/
-
-sleep 1 && git fetch https://github.com/xc112lg/proprietary_vendor_lge_msm8996-common.git patch-2
-sleep 1 && git cherry-pick b7ae264df1d799c5d635bada6afbc3714df75cdb 
-sleep 1 && git cherry-pick 1efc7fd60e02b78c0ce03b184b1c0f485100cd18
-cd ../../../
+# source scripts/resync.sh
 
 
+# cd device/lge/msm8996-common
+# sleep 1 &&git fetch https://github.com/xc112lg/android_device_lge_msm8996-common.git patch-1
+# sleep 1 &&git cherry-pick 7ef8ee92f398052a9d6351e4d7157e8474401f5b
 
+# cd ../../../
 
-source build/envsetup.sh
-lunch lineage_h872-ap1a-userdebug
-make installclean
-time m bacon
-source scripts/cleanmanifest.sh
-rm -rf .repo/local_manifests
-mkdir .repo/local_manifests
-cp scripts/eureka_deps.xml .repo/local_manifests/
+# cd vendor/lge/msm8996-common/
 
-source scripts/resync.sh
-source build/envsetup.sh
-lunch lineage_a10-ap1a-userdebug
-make installclean
-time m bacon
-
-
-source scripts/cleanmanifest.sh
-rm -rf .repo/local_manifests
-mkdir .repo/local_manifests
-cp scripts/x.xml .repo/local_manifests/
-
-source scripts/resync.sh
-
-
-source build/envsetup.sh
-lunch lineage_X00TD-ap1a-userdebug
-make installclean
-time m bacon
+# sleep 1 && git fetch https://github.com/xc112lg/proprietary_vendor_lge_msm8996-common.git patch-2
+# sleep 1 && git cherry-pick b7ae264df1d799c5d635bada6afbc3714df75cdb 
+# sleep 1 && git cherry-pick 1efc7fd60e02b78c0ce03b184b1c0f485100cd18
+# cd ../../../
 
 
 
-source scripts/cleanmanifest.sh
-rm -rf .repo/local_manifests
-mkdir .repo/local_manifests
-cp scripts/X01BD.xml .repo/local_manifests/
 
-source scripts/resync.sh
-source build/envsetup.sh
-lunch lineage_X01BD-ap1a-userdebug
-make installclean
-time m bacon
+# source build/envsetup.sh
+# lunch lineage_h872-ap1a-userdebug
+# make installclean
+# time m bacon
+# source scripts/cleanmanifest.sh
+# rm -rf .repo/local_manifests
+# mkdir .repo/local_manifests
+# cp scripts/eureka_deps.xml .repo/local_manifests/
 
-source scripts/cleanmanifest.sh
-source scripts/resync.sh 
+# source scripts/resync.sh
+# source build/envsetup.sh
+# lunch lineage_a10-ap1a-userdebug
+# make installclean
+# time m bacon
+
+
+# source scripts/cleanmanifest.sh
+# rm -rf .repo/local_manifests
+# mkdir .repo/local_manifests
+# cp scripts/x.xml .repo/local_manifests/
+
+# source scripts/resync.sh
+
+
+# source build/envsetup.sh
+# lunch lineage_X00TD-ap1a-userdebug
+# make installclean
+# time m bacon
+
+
+
+# source scripts/cleanmanifest.sh
+# rm -rf .repo/local_manifests
+# mkdir .repo/local_manifests
+# cp scripts/X01BD.xml .repo/local_manifests/
+
+# source scripts/resync.sh
+# source build/envsetup.sh
+# lunch lineage_X01BD-ap1a-userdebug
+# make installclean
+# time m bacon
+
+# source scripts/cleanmanifest.sh
+# source scripts/resync.sh 
