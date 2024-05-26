3b4757d29454abf4fb748d693b15ed0075ded199
xc112lg <152837944+xc112lg@users.noreply.github.com>
2024-05-26 18:21:59 +0800

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -1,22 +1,28 @@
 #!/bin/bash
 
-rm -rf ~/.android-certs/
+if [ -d ~/.android-certs/ ]; then
+echo "key already exist"
 
-ls ~/.android-certs/
-
-
-
-subject='/C=PH/ST=Philippines/L=Manila/O=Rex H/OU=Rex H/CN=Rex H/emailAddress=dtiven13@gmail.com'
+else
+    subject='/C=PH/ST=Philippines/L=Manila/O=Rex H/OU=Rex H/CN=Rex H/emailAddress=dtiven13@gmail.com'
 mkdir ~/.android-certs
 
 for x in releasekey platform shared media networkstack testkey cyngn-priv-app bluetooth sdk_sandbox verifiedboot; do 
     yes "" | ./development/tools/make_key ~/.android-certs/$x "$subject"
 done
 
-cd ~/.android-certs/
-ls
+mkdir vendor/extra
+mkdir vendor/lineage-priv/keys
+
+mv ~/.android-certs vendor/extra/keys
+#For Lineage 21 and newer use the command below if not then use above 
+#mv ~/.android-certs vendor/lineage-priv/keys
+echo "PRODUCT_DEFAULT_DEV_CERTIFICATE := vendor/extra/keys/releasekey" > vendor/extra/product.mk
+#For Lineage 21 and newer use the command below if not then use above
+#echo "PRODUCT_DEFAULT_DEV_CERTIFICATE := vendor/lineage-priv/keys/releasekey" > vendor/lineage-priv/keys/keys.mk
 
 
 
+fi
 
 
