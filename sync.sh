1991e395c730c1cefd9f332bb8827f31310483d6
xc112lg <152837944+xc112lg@users.noreply.github.com>
2024-05-26 08:43:44 +0800

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 
-subject='/C=US/ST=California/L=Mountain View/O=Android/OU=Android/CN=Android/emailAddress=android@android.com'
+
 subject='/C=DE/ST=Germany/L=Berlin/O=Max Mustermann/OU=Max Mustermann/CN=Max Mustermann/emailAddress=max@mustermann.de'
 mkdir ~/.android-certs
 
-for x in releasekey platform shared media networkstack testkey cyngn-priv-app bluetooth sdk_sandbox verifiedboot; do \
-    ./development/tools/make_key ~/.android-certs/$x "$subject"; \
+for x in releasekey platform shared media networkstack testkey cyngn-priv-app bluetooth sdk_sandbox verifiedboot; do 
+    yes "" | ./development/tools/make_key ~/.android-certs/$x "$subject"
 done
 
 cd ~/.android-certs/
