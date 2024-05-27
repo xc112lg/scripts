d13aba825cd321ce63d31a1b310c801dc7d3804f
xc112lg <152837944+xc112lg@users.noreply.github.com>
2024-05-27 22:09:48 +0800

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+
+cd external/chromium-webview/prebuilt/arm64
+git rev-parse --git-dir
+git config --global --add safe.directory external/chromium-webview/prebuilt/arm64/
+git lfs pull
+
+
 # repo init -u https://github.com/xc112lg/android.git -b 14.0 --git-lfs
 # /opt/crave/resync.sh
-source scripts/clean.sh
+#source scripts/clean.sh
