ffb98b89035e58e0d12737a950c56d021cb5cad2
xc112lg <152837944+xc112lg@users.noreply.github.com>
2024-05-28 06:52:56 +0800

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -1,36 +1,10 @@
 #!/bin/bash
 
 
-git lfs
-#!/bin/bash
-
-# Scan all folders under external/chromium-webview/prebuilt/*
-echo "Scanning folders in external/chromium-webview/prebuilt/*"
-find external/chromium-webview/prebuilt/* -type d
-
-# Install Git LFS
-echo "Installing Git LFS"
-git lfs install
-
-# Find the Git directory
-echo "Finding the Git directory"
-GIT_DIR=$(git rev-parse --git-dir)
-echo "Git directory is: $GIT_DIR"
-
-# Get the first subfolder name under external/chromium-webview/prebuilt/
-TARGET_FOLDER=$(find external/chromium-webview/prebuilt/* -type d | head -n 1)
-echo "Target folder is: $TARGET_FOLDER"
-
-# Add the target folder to the list of safe directories
-echo "Adding $TARGET_FOLDER to the list of safe directories"
-git config --global --add safe.directory "$TARGET_FOLDER"
-
-# Pull Git LFS objects
-echo "Pulling Git LFS objects"
-git lfs pull
+source scripts/extras.sh
 
 
 repo init --depth 1 -u https://github.com/PixelOS-AOSP/manifest.git -b fourteen --git-lfs
 #repo init -u https://github.com/xc112lg/android.git -b 14.0 --git-lfs
-/opt/crave/resync.sh
+#/opt/crave/resync.sh
 #source scripts/clean.sh
