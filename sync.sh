0efba656f913e230849464043c8216df3a18d9e3
xc112lg <152837944+xc112lg@users.noreply.github.com>
2024-05-28 06:18:50 +0800

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -12,6 +12,6 @@ git lfs install --system
 # git lfs pull
 
 
-repo init -u https://github.com/xc112lg/android.git -b 14.0 --git-lfs
-/opt/crave/resync.sh
+# repo init -u https://github.com/xc112lg/android.git -b 14.0 --git-lfs
+# /opt/crave/resync.sh
 #source scripts/clean.sh
