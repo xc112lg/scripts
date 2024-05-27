24bb12d97700d7729c4aa81a1290cc49305fbcf4
xc112lg <152837944+xc112lg@users.noreply.github.com>
2024-05-28 06:24:19 +0800

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -11,7 +11,7 @@ git lfs install --system
 # git config --global --add safe.directory external/chromium-webview/prebuilt/arm64/
 # git lfs pull
 
-
-repo init -u https://github.com/xc112lg/android.git -b 14.0 --git-lfs
+repo init --depth 1 -u https://github.com/PixelOS-AOSP/manifest.git -b fourteen --git-lfs
+#repo init -u https://github.com/xc112lg/android.git -b 14.0 --git-lfs
 /opt/crave/resync.sh
 #source scripts/clean.sh
