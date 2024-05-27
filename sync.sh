afcb738f86af91723d3720867e49b2b67acb0f17
xc112lg <152837944+xc112lg@users.noreply.github.com>
2024-05-27 09:26:04 +0800

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -1,10 +1,62 @@
 #!/bin/bash
 
 repo init -u https://github.com/LineageOS/android.git -b lineage-21.0 --git-lfs
-/opt/crave/resync.sh
+
+main() {
+    # Run repo sync command and capture the output
+    find .repo -name '*.lock' -delete
+    repo sync -c -j64 --force-sync --no-clone-bundle --no-tags --prune 2>&1 | tee /tmp/output.txt
+
+    if ! grep -qe "Failing repos:\|error" /tmp/output.txt ; then
+         echo "All repositories synchronized successfully."
+         exit 0
+    else
+        rm -f deleted_repositories.txt
+    fi
+
+    # Check if there are any failing repositories
+    if grep -q "Failing repos:" /tmp/output.txt ; then
+        echo "Deleting failing repositories..."
+        # Extract failing repositories from the error message and echo the deletion path
+        while IFS= read -r line; do
+            # Extract repository name and path from the error message
+            repo_info=$(echo "$line" | awk -F': ' '{print $NF}')
+            repo_path=$(dirname "$repo_info")
+            repo_name=$(basename "$repo_info")
+            # Save the deletion path to a text file
+            echo "Deleted repository: $repo_info" | tee -a deleted_repositories.txt
+            # Delete the repository
+            rm -rf "$repo_path/$repo_name"
+        done <<< "$(cat /tmp/output.txt | awk '/Failing repos:/ {flag=1; next} /Try/ {flag=0} flag')"
+    fi
+
+    # Check if there are any failing repositories due to uncommitted changes
+    if grep -q "uncommitted changes are present" /tmp/output.txt ; then
+        echo "Deleting repositories with uncommitted changes..."
+
+        # Extract failing repositories from the error message and echo the deletion path
+        while IFS= read -r line; do
+            # Extract repository name and path from the error message
+            repo_info=$(echo "$line" | awk -F': ' '{print $2}')
+            repo_path=$(dirname "$repo_info")
+            repo_name=$(basename "$repo_info")
+            # Save the deletion path to a text file
+            echo "Deleted repository: $repo_info" | tee -a deleted_repositories.txt
+            # Delete the repository
+            rm -rf "$repo_path/$repo_name"
+        done <<< "$(cat /tmp/output.txt | grep 'uncommitted changes are present')"
+    fi
+
+    # Re-sync all repositories after deletion
+    echo "Re-syncing all repositories..."
+    find .repo -name '*.lock' -delete
+    repo sync -c -j64 --force-sync --no-clone-bundle --no-tags --prune
+}
+
+main $*
 cd frameworks/base
 git log --pretty=format:"%H %ad %s" --date=iso --after="2024-01-31" --before="2024-03-01"
-
+cd ../..
 
 # Function to find the commits in February and echo their hashes
 echo_commits_in_february() {
