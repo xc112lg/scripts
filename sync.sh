e7dd61bf6aab8fe40166824fef8d53031620f1ac
xc112lg <152837944+xc112lg@users.noreply.github.com>
2024-05-28 07:47:37 +0800

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 
-source scripts/extras.sh
+#source scripts/extras.sh
 
 
-repo init --depth 1 -u https://github.com/PixelOS-AOSP/manifest.git -b fourteen --git-lfs
+repo init -u https://github.com/LineageOS/android.git -b lineage-21.0 --git-lfs
 #repo init -u https://github.com/xc112lg/android.git -b 14.0 --git-lfs
-#/opt/crave/resync.sh
+/opt/crave/resync.sh
 #source scripts/clean.sh
