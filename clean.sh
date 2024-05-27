069bdaf02bc3302428544576dc30bf1701e165f2
xc112lg <152837944+xc112lg@users.noreply.github.com>
2024-05-27 19:45:02 +0800

    f

--- clean.sh
+++ clean.sh
@@ -1,3 +1,3 @@
 paths=$(xmlstarlet sel -t -v "//project/@path" .repo/manifests/*.xml)
 echo "Paths to be deleted:"
-echo "$paths" | sed 's/$/\/"/' | sed 's/^/"/; s/ /" "/g'
+echo "$paths" | sed 's/$/\/"/' | sed 's/^/"/; s/ /" "/g' | tr -d '\n'
