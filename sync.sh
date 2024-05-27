f3ae1887a911187f5f33ee84eed01b487ac56077
xc112lg <152837944+xc112lg@users.noreply.github.com>
2024-05-27 19:25:01 +0800

    f

--- sync.sh
+++ sync.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-repo init -u https://github.com/xc112lg/android.git -b 14.0 --git-lfs
-/opt/crave/resync.sh
+# repo init -u https://github.com/xc112lg/android.git -b 14.0 --git-lfs
+# /opt/crave/resync.sh
 source scripts/clean.sh
