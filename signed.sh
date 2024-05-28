d1f7e2681304ac79571177ddb94f672071aaf35e
xc112lg <152837944+xc112lg@users.noreply.github.com>
2024-05-28 16:14:28 +0800

    d

--- signed.sh
+++ signed.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-rm -rf vendor/lineage-priv
+rm -rf vendor/lineage-priv/keys
 
 if [ -d "vendor/lineage-priv/keys" ]; then
   echo "Directory vendor/extra/keys already exists, skipping."
