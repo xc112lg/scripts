b356be4e740b15ea9537e41cc0b195bc9caca18b
xc112lg <152837944+xc112lg@users.noreply.github.com>
2024-05-28 16:54:28 +0800

    v

--- fixes.sh
+++ fixes.sh
@@ -10,11 +10,11 @@ git fetch https://github.com/LG-G6/android_device_lge_g6-common.git dev/lineage-
 git cherry-pick b3edeba5ac6500c145fec7222ffc696c9b819af0
 cd ../../../
 
-cd kernel/lge/msm8996
-# Fix LTO
-sleep 1 && git fetch https://github.com/xc112lg/msm8996_lge_kernel.git patch-1
-sleep 1 && git cherry-pick 581d1240a1ac99ebbf172ec95b8d7f4f40ca4d21
-cd ../../../
+# cd kernel/lge/msm8996
+# # Fix LTO
+# sleep 1 && git fetch https://github.com/xc112lg/msm8996_lge_kernel.git patch-1
+# sleep 1 && git cherry-pick 581d1240a1ac99ebbf172ec95b8d7f4f40ca4d21
+# cd ../../../
 
 #some fixes will be push to source fter testingg
 cd device/lge/msm8996-common
@@ -33,15 +33,15 @@ sleep 1 &&git cherry-pick 3732dcb95ded45faca1f37792fd3ed47a5cae39b
 
 cd ../../../
 
-cd packages/apps/Updater
-sleep 1 &&git fetch https://github.com/xc112lg/android_packages_apps_Updater.git patch-1
-sleep 1 &&git cherry-pick af70aa09ee94e7db085cd5777229b82f3abca313
-cd ../../../
+# cd packages/apps/Updater
+# sleep 1 &&git fetch https://github.com/xc112lg/android_packages_apps_Updater.git patch-1
+# sleep 1 &&git cherry-pick af70aa09ee94e7db085cd5777229b82f3abca313
+# cd ../../../
 
-cd vendor/lineage/
-sleep 1 &&git fetch https://github.com/xc112lg/android_vendor_crdroid.git patch-1
-sleep 1 &&git cherry-pick be2f71d81e21608f82376fd5266ce95d3266a411
-cd ../../
+# cd vendor/lineage/
+# sleep 1 &&git fetch https://github.com/xc112lg/android_vendor_crdroid.git patch-1
+# sleep 1 &&git cherry-pick be2f71d81e21608f82376fd5266ce95d3266a411
+# cd ../../
 
 
 cd vendor/lge/msm8996-common/
