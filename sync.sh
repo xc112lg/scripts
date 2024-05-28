8e789bce3f8640e72c8cda1366dc77638ae6c3e9
xc112lg <152837944+xc112lg@users.noreply.github.com>
2024-05-28 16:34:13 +0800

    d

--- sync.sh
+++ sync.sh
@@ -63,40 +63,40 @@ rm -rf .repo/local_manifests
 mkdir -p .repo/local_manifests
 cp scripts/roomservice.xml .repo/local_manifests
 
-# source scripts/clean.sh
+source scripts/clean.sh
 
-# #!/bin/bash
+#!/bin/bash
 
-# main() {
-#     # Run repo sync command and capture the output
-#     repo sync -c -j${CORE} --force-sync --no-clone-bundle --no-tags 2>&1 | tee /tmp/output.txt
+main() {
+    # Run repo sync command and capture the output
+    repo sync -c -j${CORE} --force-sync --no-clone-bundle --no-tags 2>&1 | tee /tmp/output.txt
 
-#     # Check if there are any failing repositories
-#     if grep -q "Failing repos:" /tmp/output.txt ; then
-#         echo "Deleting failing repositories..."
-#         # Extract failing repositories from the error message and echo the deletion path
-#         while IFS= read -r line; do
-#             # Extract repository name and path from the error message
-#             repo_info=$(echo "$line" | awk -F': ' '{print $NF}')
-#             repo_path=$(dirname "$repo_info")
-#             repo_name=$(basename "$repo_info")
-#             # Echo the deletion path
-#             echo "Deleted repository: $repo_info"
-#             # Save the deletion path to a text file
-#             echo "Deleted repository: $repo_info" > deleted_repositories.txt
-#             # Delete the repository
-#             rm -rf "$repo_path/$repo_name"
-#         done <<< "$(cat /tmp/output.txt | awk '/Failing repos:/ {flag=1; next} /Try/ {flag=0} flag')"
+    # Check if there are any failing repositories
+    if grep -q "Failing repos:" /tmp/output.txt ; then
+        echo "Deleting failing repositories..."
+        # Extract failing repositories from the error message and echo the deletion path
+        while IFS= read -r line; do
+            # Extract repository name and path from the error message
+            repo_info=$(echo "$line" | awk -F': ' '{print $NF}')
+            repo_path=$(dirname "$repo_info")
+            repo_name=$(basename "$repo_info")
+            # Echo the deletion path
+            echo "Deleted repository: $repo_info"
+            # Save the deletion path to a text file
+            echo "Deleted repository: $repo_info" > deleted_repositories.txt
+            # Delete the repository
+            rm -rf "$repo_path/$repo_name"
+        done <<< "$(cat /tmp/output.txt | awk '/Failing repos:/ {flag=1; next} /Try/ {flag=0} flag')"
 
-#         # Re-sync all repositories after deletion
-#         echo "Re-syncing all repositories..."
-#         repo sync -c -j${CORE} --force-sync --no-clone-bundle --no-tags
-#     else
-#         echo "All repositories synchronized successfully."
-#     fi
-# }
+        # Re-sync all repositories after deletion
+        echo "Re-syncing all repositories..."
+        repo sync -c -j${CORE} --force-sync --no-clone-bundle --no-tags
+    else
+        echo "All repositories synchronized successfully."
+    fi
+}
 
-# main $*
+main $*
 
 
 
@@ -108,51 +108,51 @@ else
 fi
 
 source scripts/signed.sh
-#source scripts/extras.sh
-# source scripts/fixes.sh
+source scripts/extras.sh
+source scripts/fixes.sh
 export USE_CCACHE=1
 source build/envsetup.sh
 
 
-# # Check if command is "clean"
-# if [ "$COMMAND" == "clean" ]; then
-#     echo "Cleaning..."
-#     m clean
-# fi
+# Check if command is "clean"
+if [ "$COMMAND" == "clean" ]; then
+    echo "Cleaning..."
+    m clean
+fi
 
-# # Check if device is set to "all"
-# if [ "$DEVICE" == "all" ]; then
-#     echo "Building for all devices..."
+# Check if device is set to "all"
+if [ "$DEVICE" == "all" ]; then
+    echo "Building for all devices..."
 
-#     lunch ${MAKEFILE}_us997${RELEASETYPE1}-userdebug
-#     m installclean
-#     ${COM1} -j$(nproc --all) ${COM2}
-#     lunch ${MAKEFILE}_h870${RELEASETYPE1}-userdebug
-#     m installclean
-#     ${COM1} -j$(nproc --all) ${COM2}
-#     lunch ${MAKEFILE}_h872${RELEASETYPE1}-userdebug
-#     m installclean
-#     ${COM1} -j$(nproc --all) ${COM2}
+    lunch ${MAKEFILE}_us997${RELEASETYPE1}-userdebug
+    m installclean
+    ${COM1} -j$(nproc --all) ${COM2}
+    lunch ${MAKEFILE}_h870${RELEASETYPE1}-userdebug
+    m installclean
+    ${COM1} -j$(nproc --all) ${COM2}
+    lunch ${MAKEFILE}_h872${RELEASETYPE1}-userdebug
+    m installclean
+    ${COM1} -j$(nproc --all) ${COM2}
  
-# elif [ "$DEVICE" == "h872" ]; then
-#     echo "Building for h872..."
-# export BUILD_DEVICE="h872"
-# 	echo "${MAKEFILE}_h872${RELEASETYPE1}-userdebug"
+elif [ "$DEVICE" == "h872" ]; then
+    echo "Building for h872..."
+export BUILD_DEVICE="h872"
+	echo "${MAKEFILE}_h872${RELEASETYPE1}-userdebug"
 
-#     lunch ${MAKEFILE}_h872${RELEASETYPE1}-userdebug
-# # Check if command is "clean"
-# if [ "$COMMAND" == "clean" ]; then
-#     echo "Cleaning..."
-#     m clean
-# fi
-#     m installclean
-#     ${COM1} -j$(nproc --all) ${COM2}
-# else
-#     echo "Building for the specified device: $DEVICE..."
-#     # Build for the specified device
-#     lunch "$DEVICE"
-#     ${COM1} -j16 ${COM2}
-# fi
+    lunch ${MAKEFILE}_h872${RELEASETYPE1}-userdebug
+# Check if command is "clean"
+if [ "$COMMAND" == "clean" ]; then
+    echo "Cleaning..."
+    m clean
+fi
+    m installclean
+    ${COM1} -j$(nproc --all) ${COM2}
+else
+    echo "Building for the specified device: $DEVICE..."
+    # Build for the specified device
+    lunch "$DEVICE"
+    ${COM1} -j16 ${COM2}
+fi
 
 
 
