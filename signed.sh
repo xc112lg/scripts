d47efc4a275de67f1e92a37b42c2115fc94a12f3
xc112lg <152837944+xc112lg@users.noreply.github.com>
2024-05-28 16:33:38 +0800

    d

--- signed.sh
+++ signed.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-rm -rf vendor/lineage-priv android-certs
+
 mkdir -p vendor/lineage-priv
 if [ -d "vendor/lineage-priv/keys" ]; then
   echo "Directory vendor/extra/keys already exists, skipping."
