d91b7b91611d9ba1a535df190a2f34e0583fb735
xc112lg <152837944+xc112lg@users.noreply.github.com>
2024-05-28 11:01:57 +0800

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-
-sudo apt-get install -y git python-is-python3 bc bison build-essential ccache curl flex g++-multilib gcc-multilib git git-lfs gnupg gperf imagemagick lib32ncurses5-dev lib32readline-dev lib32z1-dev libelf-dev liblz4-tool libncurses5 libncurses5-dev libsdl1.2-dev libssl-dev libxml2 libxml2-utils lzop pngcrush rsync schedtool squashfs-tools xsltproc zip zlib1g-dev
+sudo apt update
+sudo apt install -y git python-is-python3 bc bison build-essential ccache curl flex g++-multilib gcc-multilib git git-lfs gnupg gperf imagemagick lib32ncurses5-dev lib32readline-dev lib32z1-dev libelf-dev liblz4-tool libncurses5 libncurses5-dev libsdl1.2-dev libssl-dev libxml2 libxml2-utils lzop pngcrush rsync schedtool squashfs-tools xsltproc zip zlib1g-dev
 #source scripts/extras.sh
 
 
