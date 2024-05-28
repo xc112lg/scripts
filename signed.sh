d9256dc76af804e6b97e632ac7d9efbd4ebfc84c
xc112lg <152837944+xc112lg@users.noreply.github.com>
2024-05-28 11:45:54 +0800

    Update signed.sh

--- signed.sh
+++ signed.sh
@@ -2,13 +2,13 @@
 
 
 
-rm -rf vendor/extra
 
     subject='/C=PH/ST=Philippines/L=Manila/O=Rex H/OU=Rex H/CN=Rex H/emailAddress=dtiven13@gmail.com'
-mkdir ~/.android-certs
+mkdir ./android-certs
 
 for x in releasekey platform shared media networkstack testkey cyngn-priv-app bluetooth sdk_sandbox verifiedboot; do 
-    yes "" | ./development/tools/make_key ~/.android-certs/$x "$subject"
+    yes "" | ./development/tools/make_key ./android-certs/$x "$subject"; \
+done
 
 
 mkdir vendor/extra
