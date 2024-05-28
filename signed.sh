5020ba1b88638174205fc8e5c6da3bef39ee281a
xc112lg <152837944+xc112lg@users.noreply.github.com>
2024-05-28 16:25:27 +0800

    d

--- signed.sh
+++ signed.sh
@@ -17,7 +17,7 @@ mkdir vendor/lineage-priv
 
 # cp -r ./android-certs vendor/extra/keys
 #For Lineage 21 and newer use the command below if not then use above 
-cp -r ./android-certs vendor/lineage-priv/keys
+mv ./android-certs vendor/lineage-priv/keys
 # echo "PRODUCT_DEFAULT_DEV_CERTIFICATE := vendor/extra/keys/releasekey" > vendor/extra/product.mk
 #For Lineage 21 and newer use the command below if not then use above
 echo "PRODUCT_DEFAULT_DEV_CERTIFICATE := vendor/lineage-priv/keys/releasekey" > vendor/lineage-priv/keys/keys.mk
