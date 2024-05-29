563d58c0fb79c3fc59b57d9ed36f994839c695d1
xc112lg <152837944+xc112lg@users.noreply.github.com>
2024-05-29 19:54:27 +0800

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -8,7 +8,7 @@ cp scripts/roomservice.xml .repo/local_manifests
 
 #repo init -u https://github.com/LineageOS/android.git -b lineage-21.0 --git-lfs
 repo init -u https://github.com/xc112lg/android.git -b 14.0 --git-lfs
-# /opt/crave/resync.sh
+/opt/crave/resync.sh
 
 
 
