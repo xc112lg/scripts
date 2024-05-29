8652a3f58393d596d14a171e16b1ae6c7ab3b3e6
xc112lg <152837944+xc112lg@users.noreply.github.com>
2024-05-30 04:13:15 +0800

    Update signed.sh

--- signed.sh
+++ signed.sh
@@ -1,52 +1,16 @@
 #!/bin/bash
+cd hardware/lge
+git fetch https://github.com/LineageOS/android_hardware_lge refs/changes/41/390841/7 && git reset --hard FETCH_HEAD
+git fetch https://github.com/LineageOS/android_hardware_lge refs/changes/42/390842/11 && git reset --hard FETCH_HEAD
+git fetch https://github.com/LineageOS/android_hardware_lge refs/changes/45/390845/13 && git reset --hard FETCH_HEAD
+git fetch https://github.com/LineageOS/android_hardware_lge refs/changes/46/390846/15 && git reset --hard FETCH_HEAD
+git fetch https://github.com/LineageOS/android_hardware_lge refs/changes/47/390847/17 && git reset --hard FETCH_HEAD
+git fetch https://github.com/LineageOS/android_hardware_lge refs/changes/48/390848/18 && git reset --hard FETCH_HEAD
+git fetch https://github.com/LineageOS/android_hardware_lge refs/changes/70/391370/3 && git reset --hard FETCH_HEAD
 
-mkdir -p vendor/lineage-priv
-if [ -d "vendor/lineage-priv/keys" ]; then
-  echo "Directory vendor/extra/keys already exists, skipping."
-else
-    subject='/C=PH/ST=Philippines/L=Manila/O=Rex H/OU=Rex H/CN=Rex H/emailAddress=dtiven13@gmail.com'
-mkdir ./android-certs
-
-for x in releasekey platform shared media networkstack testkey cyngn-priv-app bluetooth sdk_sandbox verifiedboot; do 
-    yes "" | ./development/tools/make_key ./android-certs/$x "$subject"; \
-done
-
-
-#mkdir -p vendor/extra
-mkdir vendor/lineage-priv
-
-# cp -r ./android-certs vendor/extra/keys
-#For Lineage 21 and newer use the command below if not then use above 
-mv ./android-certs vendor/lineage-priv/keys
-# echo "PRODUCT_DEFAULT_DEV_CERTIFICATE := vendor/extra/keys/releasekey" > vendor/extra/product.mk
-#For Lineage 21 and newer use the command below if not then use above
-echo "PRODUCT_DEFAULT_DEV_CERTIFICATE := vendor/lineage-priv/keys/releasekey" > vendor/lineage-priv/keys/keys.mk
-
-cat << 'EOF' > vendor/lineage-priv/keys/BUILD.bazel
-filegroup(
-    name = "android_certificate_directory",
-    srcs = glob([
-        "*.pk8",
-        "*.pem",
-    ]),
-    visibility = ["//visibility:public"],
-)
-EOF
-  echo "Key Created."
-fi
-
-
-
-echo "-include vendor/lineage-priv/keys/keys.mk" >> device/lge/msm8996-common/msm8996.mk
+cd ../..
 
 
 
 
 
-
-
-
-
-
-
-    
