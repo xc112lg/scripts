3973a7f68689177f284bea710d326a972b6214eb
xc112lg <152837944+xc112lg@users.noreply.github.com>
2024-05-29 21:17:52 +0800

    Update clean.sh

--- clean.sh
+++ clean.sh
@@ -1,52 +1,35 @@
 xml_dir=".repo/manifests"
-paths=$(xmlstarlet sel -t -v "//project/@path" "$xml_dir"/*.xml | sed 's/$/\//')
-
-
-
-# Remove each file
-for path in $paths; do
-    cd "$path"
-    # Count the number of slashes in the path
-    num_slashes=$(tr -dc '/' <<< "$path" | awk '{ print length; }')
-
-
-
-
+paths=$(xmlstarlet sel -t -v "//project/@path" "$xml_dir"/*.xml)
 
 # Define the target date
 target_date="2024-03-12"
 
+for path in $paths; do
+    # Append a slash to the end of the path
+    path="${path}/"
 
+    # Navigate to the directory
+    cd "$path" || { echo "Directory $path not found"; continue; }
 
-# Navigate to the frameworks/base directory
-cd "$path" || { echo "Directory $path not found"; exit 1; }
+    # Find the latest commit hash
+    latest_commit_hash=$(git rev-parse HEAD)
 
-# Find the latest commit hash
-latest_commit_hash=$(git rev-parse HEAD)
+    # Find the commit hash before the target date
+    commit_hash=$(git rev-list -n 1 --before="$target_date" HEAD)
 
-# Find the commit hash before the target date
-commit_hash=$(git rev-list -n 1 --before="$target_date" HEAD)
-
-if [ -z "$commit_hash" ]; then
-    echo "No commits found before $target_date in $path"
-else
-    # Check if the latest commit is after the target date
-    if [[ "$(git log -n 1 --format=%ci $latest_commit_hash)" > "$target_date" ]]; then
-        # Reset the branch to the commit before the target date
-        git reset --hard "$commit_hash"
-        echo "Reverted to commit $commit_hash before $target_date in $path"
+    if [ -z "$commit_hash" ]; then
+        echo "No commits found before $target_date in $path"
     else
-        echo "No need to revert in $path, latest commit is before $target_date"
+        # Check if the latest commit is after the target date
+        if [[ "$(git log -n 1 --format=%ci $latest_commit_hash)" > "$target_date" ]]; then
+            # Reset the branch to the commit before the target date
+            git reset --hard "$commit_hash"
+            echo "Reverted to commit $commit_hash before $target_date in $path"
+        else
+            echo "No need to revert in $path, latest commit is before $target_date"
+        fi
     fi
-fi
 
-
-
-  # Move back to the original directory
-    for ((i=0; i<num_slashes; i++)); do
-        cd ..
-    done
-
+    # Move back to the original directory
+    cd -
 done
-
-
