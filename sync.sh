d261116cf6d93af521006a60f412f7509d41ec64
xc112lg <152837944+xc112lg@users.noreply.github.com>
2024-05-29 15:45:35 +0800

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-sudo apt update
-sudo apt install -y git python-is-python3 bc bison build-essential ccache curl flex g++-multilib gcc-multilib git git-lfs gnupg gperf imagemagick lib32ncurses5-dev lib32readline-dev lib32z1-dev libelf-dev liblz4-tool libncurses5 libncurses5-dev libsdl1.2-dev libssl-dev libxml2 libxml2-utils lzop pngcrush rsync schedtool squashfs-tools xsltproc zip zlib1g-dev
-#source scripts/extras.sh
+rm -rf .repo/local_manifests
+#rm -rf device/lge/
+#rm -rf kernel/lge/msm8996
+mkdir -p .repo/local_manifests
+cp scripts/roomservice.xml .repo/local_manifests
 
 
-repo init -u https://github.com/LineageOS/android.git -b lineage-21.0 --git-lfs
-#repo init -u https://github.com/xc112lg/android.git -b 14.0 --git-lfs
+#repo init -u https://github.com/LineageOS/android.git -b lineage-21.0 --git-lfs
+repo init -u https://github.com/xc112lg/android.git -b 14.0 --git-lfs
 /opt/crave/resync.sh
 #source scripts/clean.sh
