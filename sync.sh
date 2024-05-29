28dcba27cd8f887ab184d546be76f3fc3274ea0e
xc112lg <152837944+xc112lg@users.noreply.github.com>
2024-05-29 20:20:52 +0800

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -8,21 +8,21 @@ cp scripts/roomservice.xml .repo/local_manifests
 
 #repo init -u https://github.com/LineageOS/android.git -b lineage-21.0 --git-lfs
 repo init -u https://github.com/xc112lg/android.git -b 14.0 --git-lfs
-/opt/crave/resync.sh
+# /opt/crave/resync.sh
 
 
 
 source scripts/clean.sh
 # source scripts/extras.sh
 
-source build/envsetup.sh
-chmod +x scripts/generate_certs.sh
-chmod +x scripts/build_and_sign.sh
+# source build/envsetup.sh
+# chmod +x scripts/generate_certs.sh
+# chmod +x scripts/build_and_sign.sh
 
 
 
-#source scripts/generate_certs.sh
-source scripts/build_and_sign.sh
+# #source scripts/generate_certs.sh
+# source scripts/build_and_sign.sh
     # lunch lineage_h872-userdebug
     # m installclean
     # m -j$(nproc --all) bacon
