2ad32456f44cd408ee8ba52292a42f1549d3ffb9
xc112lg <152837944+xc112lg@users.noreply.github.com>
2024-05-29 19:52:45 +0800

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -10,40 +10,6 @@ cp scripts/roomservice.xml .repo/local_manifests
 repo init -u https://github.com/xc112lg/android.git -b 14.0 --git-lfs
 # /opt/crave/resync.sh
 
-main() {
-    # Run repo sync command and capture the output
-    repo sync -c -j32 --force-sync --no-clone-bundle --no-tags 2>&1 | tee /tmp/output.txt
-
-    # Check if there are any failing repositories
-    if grep -q "Failing repos:" /tmp/output.txt ; then
-        echo "Deleting failing repositories..."
-        # Extract failing repositories from the error message and echo the deletion path
-        while IFS= read -r line; do
-            # Extract repository name and path from the error message
-            repo_info=$(echo "$line" | awk -F': ' '{print $NF}')
-            repo_path=$(dirname "$repo_info")
-            repo_name=$(basename "$repo_info")
-            # Echo the deletion path
-            echo "Deleted repository: $repo_info"
-            # Save the deletion path to a text file
-            echo "Deleted repository: $repo_info" > deleted_repositories.txt
-            # Delete the repository
-            rm -rf "$repo_path/$repo_name"
-        done <<< "$(cat /tmp/output.txt | awk '/Failing repos:/ {flag=1; next} /Try/ {flag=0} flag')"
-
-        # Re-sync all repositories after deletion
-        echo "Re-syncing all repositories..."
-        repo sync -c -j32 --force-sync --no-clone-bundle --no-tags
-    else
-        echo "All repositories synchronized successfully."
-    fi
-}
-
-main $*
-
-
-
-
 
 
 # source scripts/clean.sh
@@ -55,7 +21,7 @@ chmod +x scripts/build_and_sign.sh
 
 
 
-source scripts/generate_certs.sh
+#source scripts/generate_certs.sh
 source scripts/build_and_sign.sh
     # lunch lineage_h872-userdebug
     # m installclean
