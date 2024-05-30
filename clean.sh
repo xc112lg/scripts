8b0be230a2d4e43b0a9bb918cd3d4997601896cb
xc112lg <152837944+xc112lg@users.noreply.github.com>
2024-05-30 20:43:44 +0800

    Update clean.sh

--- clean.sh
+++ clean.sh
@@ -1,5 +1,3 @@
 cd device/lge/msm8996-common
 git fetch https://github.com/LineageOS/android_device_lge_msm8996-common refs/changes/28/388128/3 && git reset --hard FETCH_HEAD
-git fetch https://github.com/xc112lg/android_device_lge_msm8996-common.git patch-3
-git cherry-pick ea02d8efb0593566ca649281bf793caf5da3fb88
 cd ../../../
