98306e324a13745d17264151fafce193807a8dbf
xc112lg <152837944+xc112lg@users.noreply.github.com>
2024-05-31 07:42:32 +0800

    Update signed.sh

--- signed.sh
+++ signed.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 cd hardware/lge
-git fetch https://github.com/LineageOS/android_hardware_lge refs/changes/41/390841/7 && git reset --hard FETCH_HEAD
-git fetch https://github.com/LineageOS/android_hardware_lge refs/changes/42/390842/11 && git reset --hard FETCH_HEAD
-git fetch https://github.com/LineageOS/android_hardware_lge refs/changes/45/390845/13 && git reset --hard FETCH_HEAD
-git fetch https://github.com/LineageOS/android_hardware_lge refs/changes/46/390846/15 && git reset --hard FETCH_HEAD
-git fetch https://github.com/LineageOS/android_hardware_lge refs/changes/47/390847/17 && git reset --hard FETCH_HEAD
-git fetch https://github.com/LineageOS/android_hardware_lge refs/changes/48/390848/18 && git reset --hard FETCH_HEAD
-git fetch https://github.com/LineageOS/android_hardware_lge refs/changes/70/391370/3 && git reset --hard FETCH_HEAD
+git fetch https://github.com/LineageOS/android_hardware_lge refs/changes/41/390841/7 && git checkout FETCH_HEAD
+git fetch https://github.com/LineageOS/android_hardware_lge refs/changes/42/390842/11 && git checkout FETCH_HEAD
+git fetch https://github.com/LineageOS/android_hardware_lge refs/changes/45/390845/13 && git checkout FETCH_HEAD
+git fetch https://github.com/LineageOS/android_hardware_lge refs/changes/46/390846/15 && git checkout FETCH_HEAD
+git fetch https://github.com/LineageOS/android_hardware_lge refs/changes/47/390847/17 && git checkout FETCH_HEAD
+git fetch https://github.com/LineageOS/android_hardware_lge refs/changes/48/390848/18 && git checkout FETCH_HEAD
+git fetch https://github.com/LineageOS/android_hardware_lge refs/changes/70/391370/3 && git checkout FETCH_HEAD
 
 cd ../..
 
