428700bae8ef5a6fdb90fdbe0587c65ba2f0ab7e
xc112lg <152837944+xc112lg@users.noreply.github.com>
2024-05-31 08:40:01 +0800

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -106,7 +106,11 @@ else
     RELEASETYPE1="$RELEASETYPE"
 fi
 
-source scripts/signed.sh
+
+rm -rf hardware/lge
+git clone https://github.com/xc112lg/android_hardware_lge hardware/lge
+
+#source scripts/signed.sh
 #source scripts/extras.sh
 source scripts/fixes.sh
 chmod +x scripts/generate_certs.sh
