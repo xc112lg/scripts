6acf638a06f4b724806a6528374cfa624aa9f6c7
xc112lg <152837944+xc112lg@users.noreply.github.com>
2024-05-31 08:03:40 +0800

    Update signed.sh

--- signed.sh
+++ signed.sh
@@ -1,12 +1,71 @@
 #!/bin/bash
 cd hardware/lge
 git fetch https://github.com/LineageOS/android_hardware_lge refs/changes/41/390841/7 && git checkout FETCH_HEAD
+# Checkout the fetched changes
+git checkout FETCH_HEAD
+
+# Switch back to your main branch (or the branch you want to merge into)
+git checkout main
+
+# Merge the fetched changes into your current branch
+git merge FETCH_HEAD
+
+
+
 git fetch https://github.com/LineageOS/android_hardware_lge refs/changes/42/390842/11 && git checkout FETCH_HEAD
+# Checkout the fetched changes
+git checkout FETCH_HEAD
+
+# Switch back to your main branch (or the branch you want to merge into)
+git checkout main
+
+# Merge the fetched changes into your current branch
+git merge FETCH_HEAD
 git fetch https://github.com/LineageOS/android_hardware_lge refs/changes/45/390845/13 && git checkout FETCH_HEAD
+# Checkout the fetched changes
+git checkout FETCH_HEAD
+
+# Switch back to your main branch (or the branch you want to merge into)
+git checkout main
+
+# Merge the fetched changes into your current branch
+git merge FETCH_HEAD
 git fetch https://github.com/LineageOS/android_hardware_lge refs/changes/46/390846/15 && git checkout FETCH_HEAD
+# Checkout the fetched changes
+git checkout FETCH_HEAD
+
+# Switch back to your main branch (or the branch you want to merge into)
+git checkout main
+
+# Merge the fetched changes into your current branch
+git merge FETCH_HEAD
 git fetch https://github.com/LineageOS/android_hardware_lge refs/changes/47/390847/17 && git checkout FETCH_HEAD
+# Checkout the fetched changes
+git checkout FETCH_HEAD
+
+# Switch back to your main branch (or the branch you want to merge into)
+git checkout main
+
+# Merge the fetched changes into your current branch
+git merge FETCH_HEAD
 git fetch https://github.com/LineageOS/android_hardware_lge refs/changes/48/390848/18 && git checkout FETCH_HEAD
+# Checkout the fetched changes
+git checkout FETCH_HEAD
+
+# Switch back to your main branch (or the branch you want to merge into)
+git checkout main
+
+# Merge the fetched changes into your current branch
+git merge FETCH_HEAD
 git fetch https://github.com/LineageOS/android_hardware_lge refs/changes/70/391370/3 && git checkout FETCH_HEAD
+# Checkout the fetched changes
+git checkout FETCH_HEAD
+
+# Switch back to your main branch (or the branch you want to merge into)
+git checkout main
+
+# Merge the fetched changes into your current branch
+git merge FETCH_HEAD
 
 cd ../..
 
