dece9a234f9158ae921ecc95e157e1b530189a15
xc112lg <152837944+xc112lg@users.noreply.github.com>
2024-06-09 14:50:19 +0800

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -8,6 +8,12 @@ rm -rf kernel/lge/msm8996/
 rm -rf vendor/lge/
 rm -rf vendor/lineage/
 repo init -u https://github.com/LineageOS/android.git -b lineage-20.0 --git-lfs
+
+rm -rf .repo/local_manifests
+
+mkdir -p .repo/local_manifests
+cp scripts/roomservice.xml .repo/local_manifests
+
 rm -rf external/chromium-webview/prebuilt/*
 rm -rf .repo/projects/external/chromium-webview/prebuilt/*.git
 rm -rf .repo/project-objects/LineageOS/android_external_chromium-webview_prebuilt_*.git
