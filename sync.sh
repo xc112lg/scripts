417444bae4d6d57a518727b7c88eeb5506bcdd14
xc112lg <152837944+xc112lg@users.noreply.github.com>
2024-06-13 08:41:32 +0800

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -103,10 +103,10 @@ rm -rf .repo/project-objects/LineageOS/android_external_chromium-webview_prebuil
 
 source build/envsetup.sh
 sed -i '/include $(LOCAL_PATH)\/vendor_prop.mk/a -include vendor/extra/product.mk' device/lge/msm8996-common/msm8996.mk
-# cd build/tools
-# git fetch https://github.com/xc112lg/android_build.git patch-1
-# git cherry-pick b7b12b875a97eee6e512c74c53a82066e237a31a
-# cd ../../
+cd build/tools
+git fetch https://github.com/xc112lg/android_build.git patch-1
+git cherry-pick b7b12b875a97eee6e512c74c53a82066e237a31a
+cd ../../
 
 cd frameworks/base
 git fetch https://github.com/xc112lg/android_frameworks_base-1.git patch-19
