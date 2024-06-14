0b17e581d334c83b3232e6822cceee3c0cb252fa
xc112lg <152837944+xc112lg@users.noreply.github.com>
2024-06-14 20:45:01 +0800

    Update fixes.sh

--- fixes.sh
+++ fixes.sh
@@ -10,11 +10,11 @@ git fetch https://github.com/LG-G6/android_device_lge_g6-common.git dev/lineage-
 git cherry-pick b3edeba5ac6500c145fec7222ffc696c9b819af0
 cd ../../../
 
-cd kernel/lge/msm8996
-# Fix LTO
-sleep 1 && git fetch https://github.com/xc112lg/msm8996_lge_kernel.git patch-1
-sleep 1 && git cherry-pick 581d1240a1ac99ebbf172ec95b8d7f4f40ca4d21
-cd ../../../
+# cd kernel/lge/msm8996
+# # Fix LTO
+# sleep 1 && git fetch https://github.com/xc112lg/msm8996_lge_kernel.git patch-1
+# sleep 1 && git cherry-pick 581d1240a1ac99ebbf172ec95b8d7f4f40ca4d21
+# cd ../../../
 
 #cd vendor/lge/msm8996-common/
 
