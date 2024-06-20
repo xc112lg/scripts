b28a0920baea62944b3d636c826dea0013621653
xc112lg <152837944+xc112lg@users.noreply.github.com>
2024-06-20 11:27:10 +0800

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -16,12 +16,12 @@ source scripts/clean.sh
 
 
 
-# source scripts/extras.sh
+source scripts/extras.sh
 
-# source build/envsetup.sh
-# lunch lineage_h872-userdebug
-# m installclean
-# m -j$(nproc --all) bacon
+source build/envsetup.sh
+lunch lineage_h872-userdebug
+m installclean
+m -j$(nproc --all) bacon
 # chmod +x scripts/generate_certs.sh
 # chmod +x scripts/build_and_sign.sh
 
