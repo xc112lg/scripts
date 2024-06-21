20538ff7d06628307e5808e7abdc976131d5a85a
xc112lg <152837944+xc112lg@users.noreply.github.com>
2024-06-21 11:33:15 +0800

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -4,17 +4,16 @@
 #rm -rf kernel/lge/msm8996
 #mkdir -p .repo/local_manifests
 #cp scripts/roomservice.xml .repo/local_manifests
-# mkdir -p cc
-# ls -1 c | xargs -I {} -P 10 -n 1 rsync -au c/{} cc/
-mkdir -p c
-cd c
-
+mkdir -p cc
+ls -1 c | xargs -I {} -P 10 -n 1 rsync -au c/{} cc/
+# mkdir -p c
+# cd c
+cd cc
 repo init -u https://github.com/xc112lg/android.git -b 14.0 --git-lfs
 /opt/crave/resync.sh
+crave ssh -- git clone https://github.com/xc112lg/scripts.git -b test
 
-
-
-#source scripts/clean.sh
+source scripts/clean.sh
 
 
 
