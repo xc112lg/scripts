a970cc885544d8632b7772e4c79dc9220cfb19ae
xc112lg <152837944+xc112lg@users.noreply.github.com>
2024-06-24 21:21:13 +0800

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -46,6 +46,7 @@ main $*
 cd device/lge/msm8996-common
 sleep 1 &&git fetch https://github.com/xc112lg/android_device_lge_msm8996-common.git patch-12
 sleep 1 &&git cherry-pick 4fa74117cd16f6c81709c5ee6022c52ccbe486a6 
+sleep 1 &&git cherry-pick 02e827da0d5dc692668016bfb2a9c282ea2de57c
 cd ../../../
 
 source build/envsetup.sh
