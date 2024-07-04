9de23cab9fedfa1704687a4bbdb838248bdb8756
xc112lg <152837944+xc112lg@users.noreply.github.com>
2024-07-04 22:03:31 +0800

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -1,59 +1,59 @@
 #!/bin/bash
+echo $(nproc --all)
+# rm -rf .repo/local_manifests device/lge/msm8996-common
+# rm -rf  ~/.android-certs/
+# mkdir -p .repo/local_manifests
+# cp scripts/roomservice.xml .repo/local_manifests
+# #rm -rf ~/.android-certs
 
-rm -rf .repo/local_manifests device/lge/msm8996-common
-rm -rf  ~/.android-certs/
-mkdir -p .repo/local_manifests
-cp scripts/roomservice.xml .repo/local_manifests
-#rm -rf ~/.android-certs
+# repo init --git-lfs
+# rm -rf external/chromium-webview/prebuilt/*
+# rm -rf .repo/projects/external/chromium-webview/prebuilt/*.git
+# rm -rf .repo/project-objects/LineageOS/android_external_chromium-webview_prebuilt_*.git
+# repo init -u https://github.com/LineageOS/android.git -b lineage-21.0 --git-lfs
 
-repo init --git-lfs
-rm -rf external/chromium-webview/prebuilt/*
-rm -rf .repo/projects/external/chromium-webview/prebuilt/*.git
-rm -rf .repo/project-objects/LineageOS/android_external_chromium-webview_prebuilt_*.git
-repo init -u https://github.com/LineageOS/android.git -b lineage-21.0 --git-lfs
+# main() {
+#     # Run repo sync command and capture the output
+#     repo sync -c -j20 --force-sync --no-clone-bundle --no-tags 2>&1 | tee /tmp/output.txt
 
-main() {
-    # Run repo sync command and capture the output
-    repo sync -c -j20 --force-sync --no-clone-bundle --no-tags 2>&1 | tee /tmp/output.txt
+#     # Check if there are any failing repositories
+#     if grep -q "Failing repos:" /tmp/output.txt ; then
+#         echo "Deleting failing repositories..."
+#         # Extract failing repositories from the error message and echo the deletion path
+#         while IFS= read -r line; do
+#             # Extract repository name and path from the error message
+#             repo_info=$(echo "$line" | awk -F': ' '{print $NF}')
+#             repo_path=$(dirname "$repo_info")
+#             repo_name=$(basename "$repo_info")
+#             # Echo the deletion path
+#             echo "Deleted repository: $repo_info"
+#             # Save the deletion path to a text file
+#             echo "Deleted repository: $repo_info" > deleted_repositories.txt
+#             # Delete the repository
+#             rm -rf "$repo_path/$repo_name"
+#         done <<< "$(cat /tmp/output.txt | awk '/Failing repos:/ {flag=1; next} /Try/ {flag=0} flag')"
 
-    # Check if there are any failing repositories
-    if grep -q "Failing repos:" /tmp/output.txt ; then
-        echo "Deleting failing repositories..."
-        # Extract failing repositories from the error message and echo the deletion path
-        while IFS= read -r line; do
-            # Extract repository name and path from the error message
-            repo_info=$(echo "$line" | awk -F': ' '{print $NF}')
-            repo_path=$(dirname "$repo_info")
-            repo_name=$(basename "$repo_info")
-            # Echo the deletion path
-            echo "Deleted repository: $repo_info"
-            # Save the deletion path to a text file
-            echo "Deleted repository: $repo_info" > deleted_repositories.txt
-            # Delete the repository
-            rm -rf "$repo_path/$repo_name"
-        done <<< "$(cat /tmp/output.txt | awk '/Failing repos:/ {flag=1; next} /Try/ {flag=0} flag')"
+#         # Re-sync all repositories after deletion
+#         echo "Re-syncing all repositories..."
+#         repo sync -c -j20 --force-sync --no-clone-bundle --no-tags
+#     else
+#         echo "All repositories synchronized successfully."
+#     fi
+# }
 
-        # Re-sync all repositories after deletion
-        echo "Re-syncing all repositories..."
-        repo sync -c -j20 --force-sync --no-clone-bundle --no-tags
-    else
-        echo "All repositories synchronized successfully."
-    fi
-}
-
-main $*
-
-
-rm -rf device/lge/msm8996-common
-git clone https://github.com/xc112lg/android_device_lge_msm8996-common.git --depth 1 -b patch-2 device/lge/msm8996-common
+# main $*
 
 
-source build/envsetup.sh
-#repopick -p 396073
-lunch lineage_h872-ap2a-eng
-m installclean
-m bacon
+# rm -rf device/lge/msm8996-common
+# git clone https://github.com/xc112lg/android_device_lge_msm8996-common.git --depth 1 -b patch-2 device/lge/msm8996-common
 
 
-# breakfast h872
-# brunch h872 eng
+# source build/envsetup.sh
+# #repopick -p 396073
+# lunch lineage_h872-ap2a-eng
+# m installclean
+# m bacon
+
+
+# # breakfast h872
+# # brunch h872 eng
