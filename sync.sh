d30251a4e7ea173d591c6fcb1dc64e76e269da88
xc112lg <152837944+xc112lg@users.noreply.github.com>
2024-07-08 09:21:32 +0800

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -7,6 +7,6 @@ rm -rf lineage_build_unified lineage_patches_unified
 repo init -u https://github.com/LineageOS/android.git -b lineage-21.0 --git-lfs
 
 git clone https://github.com/xc112lg/lineage_build_unified lineage_build_unified -b lineage-21-td
-git clone https://github.com/xc112lg/lineage_patches_unified lineage_patches_unified -b patch-1
+git clone https://github.com/xc112lg/lineage_patches_unified lineage_patches_unified -b patch-2
 
 bash lineage_build_unified/buildbot_unified.sh treble A64GN
