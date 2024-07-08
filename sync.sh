57302b68e4aae2e07a026847e6760c872cedafb0
xc112lg <152837944+xc112lg@users.noreply.github.com>
2024-07-09 04:56:56 +0800

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -3,7 +3,7 @@
 
 #!/bin/bash
 
-rm -rf lineage_build_unified lineage_patches_unified
+rm -rf lineage_build_unified lineage_patches_unified frameworks/base
 repo init -u https://github.com/LineageOS/android.git -b lineage-21.0 --git-lfs
 
 git clone https://github.com/xc112lg/lineage_build_unified lineage_build_unified -b lineage-21-td
