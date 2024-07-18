f6c6cb9b2c7e6f9591a1a6381591bdfe9e36883a
xc112lg <152837944+xc112lg@users.noreply.github.com>
2024-07-18 15:18:27 +0800

    Update sync1.sh

--- sync1.sh
+++ sync1.sh
@@ -32,7 +32,7 @@ git reset --hard
 
 cd -
 
-sed -i '0,/echo "including \$f"; \. "\$T\/\$f"/ s|echo "including \$f"; \. "\$T\/\$f"|echo "vendorsetup.sh is not allowed, skipping changes"|' build/make/envsetup.sh
+#sed -i '0,/echo "including \$f"; \. "\$T\/\$f"/ s|echo "including \$f"; \. "\$T\/\$f"|echo "vendorsetup.sh is not allowed, skipping changes"|' build/make/envsetup.sh
 
 
 #cat build/envsetup.sh
