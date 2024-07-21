b6378b3af88101ca4625ab4e18f5b3b1cbb0bd01
xc112lg <152837944+xc112lg@users.noreply.github.com>
2024-07-21 19:43:01 +0800

    Update sync2.sh

--- sync2.sh
+++ sync2.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
 
 
+USERNAME="${1}"
+MANIFEST="${2}"  
+BRANCH="${3}" 
+DEVICE="${4}"
 
-MANIFEST="${1}"  
-BRANCH="${2}" 
-DEVICE="${3}"
+rm -rf external/chromium-webview/prebuilt/*
+rm -rf .repo/projects/external/chromium-webview/prebuilt/*.git
+rm -rf .repo/project-objects/LineageOS/android_external_chromium-webview_prebuilt_*.git
 
-echo "$MANIFEST -b $BRANCH" 
-echo "$BRANCH"
-echo "$DEVICE"
+repo init -u https://github.com/accupara/los21-exp.git -b lineage-21.0 --git-lfs
+
+echo "building $DEVICE for $USERNAME"
+
+git clone --depth=1 $MANIFEST -b $BRANCH .repo/local_manifests
+
+/opt/crave/resync.sh
+
+
+
+
 
