5a2989ce7678e9df8ded144f8cd07d5d797564b1
xc112lg <152837944+xc112lg@users.noreply.github.com>
2024-07-23 13:05:31 +0800

    Update sync2.sh

--- sync2.sh
+++ sync2.sh
@@ -11,8 +11,8 @@ rm -rf external/chromium-webview/prebuilt/*
 rm -rf .repo/projects/external/chromium-webview/prebuilt/*.git
 rm -rf .repo/project-objects/LineageOS/android_external_chromium-webview_prebuilt_*.git
 
-repo init -u https://github.com/accupara/los21-exp.git -b lineage-21.0 --git-lfs
-#repo init -u https://github.com/LineageOS/android.git -b lineage-21.0 --git-lfs
+#repo init -u https://github.com/accupara/los21-exp.git -b lineage-21.0 --git-lfs
+repo init -u https://github.com/LineageOS/android.git -b lineage-21.0 --git-lfs
 
 
 source scripts/cleanmanifest.sh
