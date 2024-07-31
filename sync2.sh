a07c3835dd9bb584a6b689da7a04cb8e67cae7bd
xc112lg <152837944+xc112lg@users.noreply.github.com>
2024-08-01 07:09:11 +0800

    Update sync2.sh

--- sync2.sh
+++ sync2.sh
@@ -7,12 +7,12 @@ BRANCH="${3}"
 DEVICE="${4}"
 BUILD_TYPE="${5}"
 #rm -rf hardware vendor  kernel
-rm -rf external/chromium-webview/prebuilt/*
-rm -rf .repo/projects/external/chromium-webview/prebuilt/*.git
-rm -rf .repo/project-objects/LineageOS/android_external_chromium-webview_prebuilt_*.git
+# rm -rf external/chromium-webview/prebuilt/*
+# rm -rf .repo/projects/external/chromium-webview/prebuilt/*.git
+# rm -rf .repo/project-objects/LineageOS/android_external_chromium-webview_prebuilt_*.git
 
-#repo init -u https://github.com/accupara/los21-exp.git -b lineage-21.0 --git-lfs
-repo init -u https://github.com/LineageOS/android.git -b lineage-21.0 --git-lfs
+repo init -u https://github.com/accupara/los21-exp.git -b lineage-21.0 --git-lfs
+#repo init -u https://github.com/LineageOS/android.git -b lineage-21.0 --git-lfs
 
 
 source scripts/cleanmanifest.sh
@@ -33,7 +33,7 @@ cd -
 
 /opt/crave/resync.sh
 
-sed -i '0,/echo "including \$f"; \. "\$T\/\$f"/ s|echo "including \$f"; \. "\$T\/\$f"|echo "vendorsetup.sh is not allowed, skipping changes"|' build/make/envsetup.sh
+#sed -i '0,/echo "including \$f"; \. "\$T\/\$f"/ s|echo "including \$f"; \. "\$T\/\$f"|echo "vendorsetup.sh is not allowed, skipping changes"|' build/make/envsetup.sh
 
 . build/envsetup.sh
 brunch $DEVICE $BUILD_TYPE
