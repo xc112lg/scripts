205fc8be00889bbe2f11007d3972fba139c0422a
xc112lg <152837944+xc112lg@users.noreply.github.com>
2024-08-07 00:52:59 +0800

    Update s4.sh

--- s4.sh
+++ s4.sh
@@ -60,7 +60,7 @@ echo "Running main command..."
 
 
 
-rm -rf .repo/local_manifests prebuilts/clang/host/linux-x86
+rm -rf .repo/local_manifests device/lge/h872
 rm -rf  ~/.android-certs/
 mkdir -p .repo/local_manifests
 cp scripts/roomservice.xml .repo/local_manifests
@@ -79,8 +79,15 @@ repo init -u https://github.com/LineageOS/android.git -b lineage-21.0 --git-lfs
 # cd ..
 # sed -i '0,/echo "including \$f"; \. "\$T\/\$f"/ s|echo "including \$f"; \. "\$T\/\$f"|echo "vendorsetup.sh is not allowed, skipping changes"|' build/envsetup.sh
 
-sed -i 's/lineageos_h872_defconfig/vendor\/lge\/h872.config/g' device/lge/h872/BoardConfig.mk
-cat device/lge/h872/BoardConfig.mk
+#sed -i 's/lineageos_h872_defconfig/vendor\/lge\/h872.config/g' device/lge/h872/BoardConfig.mk
+#cat device/lge/h872/BoardConfig.mk
+
+cd kernel/lge/msm8996
+# Fix LTO
+sleep 1 && git fetch https://github.com/xc112lg/msm8996_lge_kernel.git patch-2
+sleep 1 && git cherry-pick f06b7401fe3b2162387a72c78bde0bbe7f2828dd
+cd -
+
 
 source build/envsetup.sh
 # #repopick -p 396073
