2c7cfccbfae3b53b8b22c386cfd5984dca1607f1
xc112lg <152837944+xc112lg@users.noreply.github.com>
2024-08-25 15:17:25 +0800

    Update fixes.sh

--- fixes.sh
+++ fixes.sh
@@ -18,7 +18,7 @@ sleep 1 && git cherry-pick a9b2d47299267762cf2f64cc0e79606fc3b52301
 cd -
 
 #some fixes will be push to source fter testingg
-cd device/lge/msm8996-common
+cd hardware/lge
 sleep 1 &&git fetch https://github.com/xc112lg/android_hardware_lge.git aosp14
 sleep 1 &&git cherry-pick 72583c8caff8932487b7be015ff8f4eb73a3f5a0 
 cd -
