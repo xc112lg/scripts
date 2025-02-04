8076f656a648cdeb996d6ca7778b41ba290a7aac
xc112lg <152837944+xc112lg@users.noreply.github.com>
2025-02-04 08:08:24 +0800

    ff

---
+++ changes.sh
@@ -0,0 +1,5 @@
+## Cam fix for LG G6 and delete some line cause im stupid.
+cd frameworks/base/
+git fetch https://github.com/xc112lg/android_frameworks_base-1.git patch-18
+git cherry-pick da7a9e6003df677f0707575be464e22fee9c0b5b
+cd ../../
--- sync.sh
+++ sync.sh
@@ -1,37 +1,19 @@
 #!/bin/bash
 
 
-DEVICE="${1:-all}"  
-COMMAND="${2:-build}" 
-DELZIP="${3}"
-MAKEFILE="${4}" 
-RELEASETYPE="${5}" 
-VENDOR="${6}" 
-COM1="${7}"
-COM2="${8}"
-CORE="${9:-"$(nproc --all)"}"
 
-
-## Remove existing build artifactsa
-if [ "$DELZIP" == "delzip" ]; then
-    rm -rf out/target/product/*/*.zip
-fi
-
-#git clean -fdX
-#rm -rf frameworks/base/
 rm -rf .repo/local_manifests
-#rm -rf device/lge/
-#rm -rf kernel/lge/msm8996
+
 mkdir -p .repo/local_manifests
 cp scripts/roomservice.xml .repo/local_manifests
 
-source scripts/clean.sh
+
 
 #!/bin/bash
 
 main() {
     # Run repo sync command and capture the output
-    repo sync -c -j${CORE} --force-sync --no-clone-bundle --no-tags 2>&1 | tee /tmp/output.txt
+    repo sync -c -j32 --force-sync --no-clone-bundle --no-tags 2>&1 | tee /tmp/output.txt
 
     # Check if there are any failing repositories
     if grep -q "Failing repos:" /tmp/output.txt ; then
@@ -63,61 +45,11 @@ main $*
 
 
 
-if [ "$RELEASETYPE" == "none" ]; then
-    RELEASETYPE1=""
-else 
-    RELEASETYPE1="$RELEASETYPE"
-fi
 
-
-
-source scripts/fixes.sh
+source scripts/changes.sh
 source scripts/signed.sh
-export USE_CCACHE=1
+
 source build/envsetup.sh
-
-
-# Check if command is "clean"
-if [ "$COMMAND" == "clean" ]; then
-    echo "Cleaning..."
-    m clean
-fi
-
-# Check if device is set to "all"
-if [ "$DEVICE" == "all" ]; then
-    echo "Building for all devices..."
-
-    lunch ${MAKEFILE}_us997${RELEASETYPE1}-userdebug
-    m installclean
-    ${COM1} -j$(nproc --all) ${COM2}
-    lunch ${MAKEFILE}_h870${RELEASETYPE1}-userdebug
-    m installclean
-    ${COM1} -j$(nproc --all) ${COM2}
-    lunch ${MAKEFILE}_h872${RELEASETYPE1}-userdebug
-    m installclean
-    ${COM1} -j$(nproc --all) ${COM2}
- 
-elif [ "$DEVICE" == "h872" ]; then
-    echo "Building for h872..."
-export BUILD_DEVICE="h872"
-	echo "${MAKEFILE}_h872${RELEASETYPE1}-userdebug"
-
-    lunch ${MAKEFILE}_h872${RELEASETYPE1}-eng
-# Check if command is "clean"
-if [ "$COMMAND" == "clean" ]; then
-    echo "Cleaning..."
-    m clean
-fi
-    m installclean
-    ${COM1} -j$(nproc --all) ${COM2}
-else
-    echo "Building for the specified device: $DEVICE..."
-    # Build for the specified device
-    lunch "$DEVICE"
-    ${COM1} -j16 ${COM2}
-fi
-
-
 
 
 
