f45eeb09bc562657bdab6aaef686ad4cfcab4527
xc112lg <152837944+xc112lg@users.noreply.github.com>
2025-02-04 11:03:25 +0800

    d

--- changes.sh
+++ changes.sh
@@ -1,5 +1,6 @@
 ## Cam fix for LG G6 and delete some line cause im stupid.
-# cd frameworks/base/
-# git fetch https://github.com/xc112lg/android_frameworks_base-1.git patch-18
-# git cherry-pick da7a9e6003df677f0707575be464e22fee9c0b5b
-# cd ../../
+cd frameworks/base/
+
+git fetch https://github.com/xc112lg/android_frameworks_base-1.git patch-22
+git cherry-pick 3a0afde4ba85ed085bb777babe888a0a2c80c69d
+cd ../../
