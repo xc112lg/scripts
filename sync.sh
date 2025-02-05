22fca21773d7a85a607980dd0cde32660f90d065
xc112lg <152837944+xc112lg@users.noreply.github.com>
2025-02-06 06:42:49 +0800

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -30,60 +30,12 @@ cp scripts/roomservice.xml .repo/local_manifests
 
 
 
-if [ "$RELEASETYPE" == "none" ]; then
-    RELEASETYPE1=""
-else 
-    RELEASETYPE1="$RELEASETYPE"
-fi
-
-
 
 # source scripts/fixes.sh
 # source scripts/signed.sh
 export USE_CCACHE=1
 source build/envsetup.sh
-
-
-# Check if command is "clean"
-if [ "$COMMAND" == "clean" ]; then
-    echo "Cleaning..."
-    m clean
-fi
-
-# Check if device is set to "all"
-if [ "$DEVICE" == "all" ]; then
-    echo "Building for all devices..."
-
-    lunch ${MAKEFILE}_us997${RELEASETYPE1}-userdebug
-    m installclean
-    ${COM1} -j$(nproc --all) ${COM2}
-    lunch ${MAKEFILE}_h870${RELEASETYPE1}-userdebug
-    m installclean
-    ${COM1} -j$(nproc --all) ${COM2}
-    lunch ${MAKEFILE}_h872${RELEASETYPE1}-userdebug
-    m installclean
-    ${COM1} -j$(nproc --all) ${COM2}
- 
-elif [ "$DEVICE" == "h872" ]; then
-    echo "Building for h872..."
-export BUILD_DEVICE="h872"
-	echo "${MAKEFILE}_h872${RELEASETYPE1}-userdebug"
-
-    lunch ${MAKEFILE}_h872${RELEASETYPE1}-eng
-# Check if command is "clean"
-if [ "$COMMAND" == "clean" ]; then
-    echo "Cleaning..."
-    m clean
-fi
-    m installclean
-    ${COM1} -j$(nproc --all) ${COM2}
-else
-    echo "Building for the specified device: $DEVICE..."
-    # Build for the specified device
-    lunch "$DEVICE"
-    ${COM1} -j16 ${COM2}
-fi
-
+brunch h872
 
 
 
