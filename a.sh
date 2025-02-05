3a91a3b5bb0c086de7c9c34318b2e4f50f9c0a0d
xc112lg <152837944+xc112lg@users.noreply.github.com>
2025-02-06 04:10:34 +0800

    Update a.sh

--- a.sh
+++ a.sh
@@ -1,12 +1,4 @@
-#!/bin/bash
-
-rm -rf h870/* h872/* us997/* 
-rm crdroid10.1/*.zip
-crave pull cd9/out/target/product/*/*.zip
-crave pull cd9/out/target/product/*/recovery.img
-mv h870/recovery.img h870/recoveryh870.img
-mv h872/recovery.img h872/recoveryh872.img
-mv us997/recovery.img us997/recoveryus997.img
-mv h870/* h872/* us997/* ./crdroid10.1/ 
-cd crdroid10.1
-./multi_upload.sh
+rm -rf scripts
+git clone https://github.com/xc112lg/scripts.git -b cd10
+chmod u+x scripts/sync.sh
+bash scripts/sync.sh h872
