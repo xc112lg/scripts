e5c8421b52ce275c045ac9325d110f461d0f5d90
xc112lg <152837944+xc112lg@users.noreply.github.com>
2025-02-06 10:12:27 +0800

    Update b.sh

--- b.sh
+++ b.sh
@@ -7,13 +7,13 @@ repo init --depth=1 --no-repo-verify -u https://github.com/crdroidandroid/androi
 git clone https://github.com/aosp-realm/android_build_manifest.git -b apollo-cr14 .repo/local_manifests
 /opt/crave/resync.sh
 source build/envsetup.sh
-brunch apollo
+#brunch apollo
 
 rm -rf scripts
 git clone https://github.com/xc112lg/scripts.git -b cd10
 chmod u+x scripts/sync.sh
-bash scripts/sync.sh h872
+bash scripts/sync.sh
 
 
 
-curl -s https://raw.githubusercontent.com/xc112lg/scripts/refs/heads/vayu1/a.sh | bash
+
