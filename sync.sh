c52978d195fb10c0410b1c97cb6b96913b2da70f
xc112lg <152837944+xc112lg@users.noreply.github.com>
2025-02-09 03:09:34 +0800

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -7,7 +7,7 @@ rm -rf frameworks/base/
 mkdir -p .repo/local_manifests
 cp scripts/roomservice.xml .repo/local_manifests
 
-
+repo init -u https://github.com/crdroidandroid/android.git -b 15.0 --git-lfs
 
 main() {
     # Run repo sync command and capture the output
