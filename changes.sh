5b9876599ca8921249a4580ff2e7b73d51fcd6d1
xc112lg <152837944+xc112lg@users.noreply.github.com>
2025-02-24 08:07:16 +0800

    Update changes.sh

--- changes.sh
+++ changes.sh
@@ -4,3 +4,10 @@ cd frameworks/base/
 git fetch https://github.com/xc112lg/android_frameworks_base-1.git patch-24
 git cherry-pick 06942a5f31c83cae39a877c0c1fab7c9e4daf040
 cd ../../
+
+
+cd system/core/
+
+git fetch https://github.com/xc112lg/android_system_core.git patch-2
+git cherry-pick 0af0d0611b2a58ba0e8950d396f7ef222377f29d
+cd -
