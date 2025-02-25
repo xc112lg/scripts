1bcebe7c8aefe8767d4fbbe96eb38a78f04a801f
xc112lg <152837944+xc112lg@users.noreply.github.com>
2025-02-26 07:30:13 +0800

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -53,6 +53,7 @@ export RBE_JAVA_POOL=default
 export RBE_METALAVA_POOL=default
 export RBE_LINT_POOL=default
 repo init -u https://github.com/Evolution-X/manifest -b vic --git-lfs --depth 1
+repo sync -c -j32 --force-sync --no-clone-bundle --no-tags --prune
 /opt/crave/resync.sh 
 #source scripts/changes.sh
 cd frameworks/base/
