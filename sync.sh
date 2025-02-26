4ea944873e5c9c8154f7c8fb7ddb20492107a36f
xc112lg <152837944+xc112lg@users.noreply.github.com>
2025-02-26 15:21:43 +0800

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -52,22 +52,22 @@ export RBE_LINT=1
 export RBE_JAVA_POOL=default
 export RBE_METALAVA_POOL=default
 export RBE_LINT_POOL=default
-repo init -u https://github.com/Evolution-X/manifest -b vic --git-lfs --depth 1
-repo sync -c -j32 --force-sync --no-clone-bundle --no-tags --prune
-/opt/crave/resync.sh 
-#source scripts/changes.sh
+# repo init -u https://github.com/Evolution-X/manifest -b vic --git-lfs --depth 1
+# repo sync -c -j32 --force-sync --no-clone-bundle --no-tags --prune
+# /opt/crave/resync.sh 
+# #source scripts/changes.sh
 
-git clone https://gitlab.com/crdroidandroid/android_prebuilts_clang_host_linux-x86_clang-r547379 prebuilts/clang/host/linux-x86/clang-r547379 --depth 1
-cd frameworks/base/
-git fetch https://github.com/xc112lg/android_frameworks_base.git patch-2
-git cherry-pick 3a3b3718ffcfe53127cbfa228577f02d825e1960
-cd -
-source scripts/signed.sh
-source build/envsetup.sh
-EPOCH_TIME=$(date +%s)
+# git clone https://gitlab.com/crdroidandroid/android_prebuilts_clang_host_linux-x86_clang-r547379 prebuilts/clang/host/linux-x86/clang-r547379 --depth 1
+# cd frameworks/base/
+# git fetch https://github.com/xc112lg/android_frameworks_base.git patch-2
+# git cherry-pick 3a3b3718ffcfe53127cbfa228577f02d825e1960
+# cd -
+# source scripts/signed.sh
+# source build/envsetup.sh
+# EPOCH_TIME=$(date +%s)
 
-# Export the variable
-export SOURCE_DATE_EPOCH=$EPOCH_TIME 
+# # Export the variable
+# export SOURCE_DATE_EPOCH=$EPOCH_TIME 
 
 lunch lineage_vayu-ap4a-userdebug
 m installclean
