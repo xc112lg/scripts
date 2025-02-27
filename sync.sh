2617b483dc54d6bb2b5502f0bcb63e8d7d4d1e9f
xc112lg <152837944+xc112lg@users.noreply.github.com>
2025-02-27 09:54:38 +0800

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -76,9 +76,6 @@ lunch lineage_vayu-ap4a-userdebug
 m installclean
 echo legacy 
 echo $TARGET_IS_LEGACY
-unset TARGET_IS_LEGACY
-echo $TARGET_IS_LEGACY
-export TARGET_IS_LEGACY=false
 m evolution
 
 #brunch vayu
