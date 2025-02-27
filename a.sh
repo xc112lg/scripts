515c84aa30a109cda24bc9e0e0f44124fa9663dd
xc112lg <152837944+xc112lg@users.noreply.github.com>
2025-02-27 09:36:25 +0800

    Update a.sh

--- a.sh
+++ a.sh
@@ -1,1 +1,1 @@
-export TARGET_IS_LEGACY=true;source build/envsetup.sh;lunch lineage_vayu-ap4a-userdebug;m installclean;m evolution
+export TARGET_IS_LEGACY=true;source build/envsetup.sh;lunch lineage_vayu-ap4a-userdebug;m installclean;echo $TARGET_IS_LEGACY;m evolution
