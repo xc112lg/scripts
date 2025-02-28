257580308dcb4c70c1a7e964dbf2d83ce531de7b
xc112lg <152837944+xc112lg@users.noreply.github.com>
2025-02-28 15:31:54 +0800

    Update signed.sh

--- signed.sh
+++ signed.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 rm -rf vendor/lineage-priv/keys
-if [ -f gh_token.txt ]; then
-    export GH_TOKEN=$(cat gh_token.txt)
-    git clone https://$GH_TOKEN@github.com/xc112lg/key vendor/lineage-priv/keys
-else
-    echo "gh_token.txt not found. Skipping repository clone."
-fi
+
+git clone https://$GH_TOKEN@github.com/xc112lg/key vendor/lineage-priv/keys
+
 
 
 
