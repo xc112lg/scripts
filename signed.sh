b6af6f6387709e098327c0c895f2c4b40844ff8a
xc112lg <152837944+xc112lg@users.noreply.github.com>
2025-02-28 15:33:05 +0800

    Update signed.sh

--- signed.sh
+++ signed.sh
@@ -3,6 +3,9 @@ rm -rf vendor/lineage-priv/keys
 
 git clone https://$GH_TOKEN@github.com/xc112lg/key vendor/lineage-priv/keys
 
+grep -qxF 'include vendor/lineage-priv/keys/keys.mk' device/xiaomi/sm8150-common/BoardConfigCommon.mk || echo 'include vendor/lineage-priv/keys/keys.mk' >> device/xiaomi/sm8150-common/BoardConfigCommon.mk
+
+
 
 
 
