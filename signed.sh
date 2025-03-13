68601137d2d10bea75160f32e7d12c0119fbc8d6
xc112lg <152837944+xc112lg@users.noreply.github.com>
2025-03-13 19:51:34 +0800

    Update signed.sh

--- signed.sh
+++ signed.sh
@@ -3,7 +3,7 @@ rm -rf vendor/lineage-priv/keys
 
 git clone https://$GH_TOKEN@github.com/xc112lg/key vendor/lineage-priv/keys
 
-grep -qxF 'include vendor/lineage-priv/keys/keys.mk' device/xiaomi/sm8150-common/msmnile.mk || echo 'include vendor/lineage-priv/keys/keys.mk' >> device/xiaomi/sm8150-common/msmnile.mk
+grep -qxF '-include vendor/lineage-priv/keys/keys.mk' device/xiaomi/sm8150-common/msmnile.mk || echo '-include vendor/lineage-priv/keys/keys.mk' >> device/xiaomi/sm8150-common/msmnile.mk
 
 
 
