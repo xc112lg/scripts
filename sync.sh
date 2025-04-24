6c0ec20ab068c0c7c4e2134256171daa80b136d6
xc112lg <152837944+xc112lg@users.noreply.github.com>
2025-04-25 07:57:11 +0800

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -60,7 +60,7 @@ git clone https://github.com/vayu-development-sources/local_manifests.git -b evo
 #git clone https://gitlab.com/crdroidandroid/android_prebuilts_clang_host_linux-x86_clang-r547379 prebuilts/clang/host/linux-x86/clang-r547379 --depth 1
 
 grep -q '"com.lazada.android"' frameworks/base/core/java/com/android/internal/util/evolution/PixelPropsUtils.java || \
-sed -i '/"com.google.android.gms",/a\        "com.lazada.android",\n        "com.shopee.ph",' frameworks/base/core/java/com/android/internal/util/evolution/PixelPropsUtils.java
+sed -i '/"com.google.android.gms",/a\        "com.lazada.android",\n        "com.shopee.ph",\n' frameworks/base/core/java/com/android/internal/util/evolution/PixelPropsUtils.java
 cat frameworks/base/core/java/com/android/internal/util/evolution/PixelPropsUtils.java
 
 source scripts/signed.sh
