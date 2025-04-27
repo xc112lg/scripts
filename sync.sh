dbab0b7304870bc2ab0270c2d9c2a4d810eb4739
xc112lg <152837944+xc112lg@users.noreply.github.com>
2025-04-28 06:04:26 +0800

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -13,7 +13,7 @@ export NINJA_REMOTE_NUM_JOBS=500                       # Number of parallel remo
 # --- BuildBuddy Connection Settings ---
 export RBE_service="remote.buildbuddy.io:443"        # BuildBuddy instance address (without grpcs://, add the port 443)
 export RBE_remote_headers="x-buildbuddy-api-key=NF5nEUUyU7LIy2QkkIIe"    # Your BuildBuddy API key
-export RBE_use_rpc_credentials=false                   
+export RBE_use_rpc_credentials=true                   
 export RBE_service_no_auth=false                       
 # --- Unified Downloads/Uploads (Recommended) ---
 export RBE_use_unified_downloads=true
