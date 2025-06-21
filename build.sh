574bcef7b00b0b5d554aec13f99286a71925dc7b
xc112lg <152837944+xc112lg@users.noreply.github.com>
2025-06-21 16:57:30 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ rm -rf device/xiaomi
 rm -rf kernel/xiaomi
 rm -rf vendor/xiaomi
 rm -rf hardware/xiaomi
-
+rm -rf build/soong
 # Cleanup previous changelog to make it always fresh
 rm -rf out/target/product/*/system/etc/Changelog.txt \
        out/target/product/*/obj/ETC/Changelog.txt_intermediates \
