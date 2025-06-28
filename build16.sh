fd6ef0244c2d67a8f58f954150a51016033e40cf
xc112lg <152837944+xc112lg@users.noreply.github.com>
2025-06-28 09:03:00 +0800

    Update build16.sh

--- build16.sh
+++ build16.sh
@@ -27,7 +27,7 @@ repo sync -c -j32 --force-sync --no-clone-bundle --no-tags
 grep -q '"com.lazada.android"' frameworks/base/core/java/com/android/internal/util/evolution/PixelPropsUtils.java || \
 sed -i '/"com.android.chrome",/a\        "com.lazada.android",\n        "com.shopee.ph",' frameworks/base/core/java/com/android/internal/util/evolution/PixelPropsUtils.java
 #cat frameworks/base/core/java/com/android/internal/util/evolution/PixelPropsUtils.java
-grep -q 'allow_undefined_symbols: true,' vendor/xiaomi/sm8150-common/Android.bp || echo 'allow_undefined_symbols: true,' >> vendor/xiaomi/sm8150-common/Android.bp
+grep -q 'allow_undefined_symbols: true,' vendor/xiaomi/sm8150-common/Android.bp || echo 'allow_undefined_symbols: true' >> vendor/xiaomi/sm8150-common/Android.bp
 cat vendor/xiaomi/sm8150-common/Android.bp
 
 
