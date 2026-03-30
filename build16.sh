51456268d146999027b6ff8fd709a6ae408623b4
xc112lg <152837944+xc112lg@users.noreply.github.com>
2026-03-30 16:49:48 +0800

    Update build16.sh

--- build16.sh
+++ build16.sh
@@ -90,8 +90,13 @@ sed -i '/"com.android.chrome",/a\        "com.lazada.android",\n        "com.sho
 
 # grep -q '^PRODUCT_OTA_ENFORCE_VINTF_KERNEL_REQUIREMENTS *:=' device/xiaomi/vayu/lineage_vayu.mk || echo 'PRODUCT_OTA_ENFORCE_VINTF_KERNEL_REQUIREMENTS := false' >> device/xiaomi/vayu/lineage_vayu.mk
 
-if grep -q '$(call inherit-product, vendor/xiaomi/miuicamera/MiuiCamera.mk)' device/xiaomi/blossom/device.mk; then
-    sed -i 's|$(call inherit-product, vendor/xiaomi/miuicamera/MiuiCamera.mk)|$(call inherit-product-if-exists, vendor/xiaomi/miuicamera/MiuiCamera.mk)|g' device/xiaomi/blossom/device.mk
+FILE="device/xiaomi/blossom/device.mk"
+
+if grep -q 'inherit-product-if-exists, vendor/xiaomi/miuicamera/MiuiCamera.mk' "$FILE"; then
+    echo "Already correct, no changes made."
+else
+    echo "Fixing line..."
+    sed -i 's|$(call inherit-product, vendor/xiaomi/miuicamera/MiuiCamera.mk|$(call inherit-product-if-exists, vendor/xiaomi/miuicamera/MiuiCamera.mk|g' "$FILE"
 fi
 
 # cd hardware/xiaomi
