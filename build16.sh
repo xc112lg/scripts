5f81e4740a39e4586b9ca2242912851a402aaa8e
xc112lg <152837944+xc112lg@users.noreply.github.com>
2026-03-30 21:01:33 +0800

    Update build16.sh

--- build16.sh
+++ build16.sh
@@ -14,60 +14,60 @@ rm -rf out/target/product/*/system/etc/Changelog.txt \
        out/target/product/*/gen/ETC/Changelog.txt_intermediates
 
 
-git clone https://github.com/xc112lg/rbe --depth 1
+# git clone https://github.com/xc112lg/rbe --depth 1
 
 
 
-export USE_RBE=1                                      
-export RBE_DIR="rbe"                      # Path to the extracted reclient directory (relative or absolute)
-export NINJA_REMOTE_NUM_JOBS=500                       # Number of parallel remote jobs (adjust based on your RAM, buildbuddy has 80 CPU cores in the free tier)
+# export USE_RBE=1                                      
+# export RBE_DIR="rbe"                      # Path to the extracted reclient directory (relative or absolute)
+# export NINJA_REMOTE_NUM_JOBS=500                       # Number of parallel remote jobs (adjust based on your RAM, buildbuddy has 80 CPU cores in the free tier)
 
-# --- BuildBuddy Connection Settings ---
-export RBE_service="remote.buildbuddy.io:443"        # BuildBuddy instance address (without grpcs://, add the port 443)
-export RBE_remote_headers="x-buildbuddy-api-key=NF5nEUUyU7LIy2QkkIIe"    # Your BuildBuddy API key
-export RBE_use_rpc_credentials=false                   
-export RBE_service_no_auth=true                       
+# # --- BuildBuddy Connection Settings ---
+# export RBE_service="remote.buildbuddy.io:443"        # BuildBuddy instance address (without grpcs://, add the port 443)
+# export RBE_remote_headers="x-buildbuddy-api-key=NF5nEUUyU7LIy2QkkIIe"    # Your BuildBuddy API key
+# export RBE_use_rpc_credentials=false                   
+# export RBE_service_no_auth=true                       
 
-# --- Unified Downloads/Uploads (Recommended) ---
-export RBE_use_unified_downloads=true
-export RBE_use_unified_uploads=true
+# # --- Unified Downloads/Uploads (Recommended) ---
+# export RBE_use_unified_downloads=true
+# export RBE_use_unified_uploads=true
 
-# --- Execution Strategies (remote_local_fallback is generally best) ---
-export RBE_R8_EXEC_STRATEGY=remote_local_fallback
-export RBE_D8_EXEC_STRATEGY=remote_local_fallback
-export RBE_JAVAC_EXEC_STRATEGY=remote_local_fallback
-export RBE_JAR_EXEC_STRATEGY=remote_local_fallback
-export RBE_ZIP_EXEC_STRATEGY=remote_local_fallback
-export RBE_TURBINE_EXEC_STRATEGY=remote_local_fallback
-export RBE_SIGNAPK_EXEC_STRATEGY=remote_local_fallback
-export RBE_CXX_EXEC_STRATEGY=remote_local_fallback    # Important see below.
-export RBE_CXX_LINKS_EXEC_STRATEGY=remote_local_fallback
-export RBE_ABI_LINKER_EXEC_STRATEGY=remote_local_fallback
-export RBE_ABI_DUMPER_EXEC_STRATEGY=    # Will make build slower, by a lot. Keeping this for documentation
-export RBE_CLANG_TIDY_EXEC_STRATEGY=remote_local_fallback
-export RBE_METALAVA_EXEC_STRATEGY=remote_local_fallback
-export RBE_LINT_EXEC_STRATEGY=remote_local_fallback
+# # --- Execution Strategies (remote_local_fallback is generally best) ---
+# export RBE_R8_EXEC_STRATEGY=remote_local_fallback
+# export RBE_D8_EXEC_STRATEGY=remote_local_fallback
+# export RBE_JAVAC_EXEC_STRATEGY=remote_local_fallback
+# export RBE_JAR_EXEC_STRATEGY=remote_local_fallback
+# export RBE_ZIP_EXEC_STRATEGY=remote_local_fallback
+# export RBE_TURBINE_EXEC_STRATEGY=remote_local_fallback
+# export RBE_SIGNAPK_EXEC_STRATEGY=remote_local_fallback
+# export RBE_CXX_EXEC_STRATEGY=remote_local_fallback    # Important see below.
+# export RBE_CXX_LINKS_EXEC_STRATEGY=remote_local_fallback
+# export RBE_ABI_LINKER_EXEC_STRATEGY=remote_local_fallback
+# export RBE_ABI_DUMPER_EXEC_STRATEGY=    # Will make build slower, by a lot. Keeping this for documentation
+# export RBE_CLANG_TIDY_EXEC_STRATEGY=remote_local_fallback
+# export RBE_METALAVA_EXEC_STRATEGY=remote_local_fallback
+# export RBE_LINT_EXEC_STRATEGY=remote_local_fallback
 
-# --- Enable RBE for Specific Tools ---
-export RBE_R8=1
-export RBE_D8=1
-export RBE_JAVAC=1
-export RBE_JAR=1
-export RBE_ZIP=1
-export RBE_TURBINE=1
-export RBE_SIGNAPK=1
-export RBE_CXX_LINKS=1
-export RBE_CXX=1
-export RBE_ABI_LINKER=1
-export RBE_ABI_DUMPER=    # Will make build slower, by a lot. Keeping this for documentation
-export RBE_CLANG_TIDY=1
-export RBE_METALAVA=1
-export RBE_LINT=1
+# # --- Enable RBE for Specific Tools ---
+# export RBE_R8=1
+# export RBE_D8=1
+# export RBE_JAVAC=1
+# export RBE_JAR=1
+# export RBE_ZIP=1
+# export RBE_TURBINE=1
+# export RBE_SIGNAPK=1
+# export RBE_CXX_LINKS=1
+# export RBE_CXX=1
+# export RBE_ABI_LINKER=1
+# export RBE_ABI_DUMPER=    # Will make build slower, by a lot. Keeping this for documentation
+# export RBE_CLANG_TIDY=1
+# export RBE_METALAVA=1
+# export RBE_LINT=1
 
-# --- Resource Pools ---
-export RBE_JAVA_POOL=default
-export RBE_METALAVA_POOL=default
-export RBE_LINT_POOL=default
+# # --- Resource Pools ---
+# export RBE_JAVA_POOL=default
+# export RBE_METALAVA_POOL=default
+# export RBE_LINT_POOL=default
 
 # Clone DerpFest
 repo init -u https://github.com/Evolution-X/manifest -b bq2 --depth=1 --git-lfs
@@ -89,6 +89,7 @@ sed -i '/"com.android.chrome",/a\        "com.lazada.android",\n        "com.sho
 #cat frameworks/base/core/java/com/android/internal/util/evolution/PixelPropsUtils.java
 # sed -i '/$(call inherit-product, vendor/xiaomi/blossom/blossom-vendor.mk)\n    $(call inherit-product-if-exists, vendor/xiaomi/miuicamera/MiuiCamera.mk/' device/xiaomi/blossom/device.mk
 sed -i 's|$(call inherit-product, vendor/xiaomi/miuicamera/MiuiCamera.mk)|$(call inherit-product-if-exists, vendor/xiaomi/miuicamera/MiuiCamera.mk|' device/xiaomi/blossom/device.mk
+sed -i 's|$(call inherit-product, hardware/dolby/dolby.mk)|$(call inherit-product-if-exists, hardware/dolby/dolby.mk|' device/xiaomi/blossom/device.mk
 # grep -q '^PRODUCT_OTA_ENFORCE_VINTF_KERNEL_REQUIREMENTS *:=' device/xiaomi/vayu/lineage_vayu.mk || echo 'PRODUCT_OTA_ENFORCE_VINTF_KERNEL_REQUIREMENTS := false' >> device/xiaomi/vayu/lineage_vayu.mk
 
 
