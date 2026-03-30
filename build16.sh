61377aed9857c09d6b54a3c1574eb25568d128a3
xc112lg <152837944+xc112lg@users.noreply.github.com>
2026-03-30 16:35:23 +0800

    Update build16.sh

--- build16.sh
+++ build16.sh
@@ -82,7 +82,7 @@ git clone https://github.com/xc112lg/local_manifests --depth 1 -b crblossom .rep
 #  fi
 repo sync -c -j32 --force-sync --no-clone-bundle --no-tags
 # repo sync
-/opt/crave/resync.sh
+#/opt/crave/resync.sh
 grep -q '"com.lazada.android"' frameworks/base/core/java/com/android/internal/util/evolution/PixelPropsUtils.java || \
 sed -i '/"com.android.chrome",/a\        "com.lazada.android",\n        "com.shopee.ph",' frameworks/base/core/java/com/android/internal/util/evolution/PixelPropsUtils.java
 #cat frameworks/base/core/java/com/android/internal/util/evolution/PixelPropsUtils.java
