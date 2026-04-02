18187106038f01fb3ef43e1fdb7c3ff0c415eb7e
xc112lg <152837944+xc112lg@users.noreply.github.com>
2026-04-02 12:47:44 +0800

    Update build16.sh

--- build16.sh
+++ build16.sh
@@ -93,10 +93,9 @@ sed -i 's|$(call inherit-product, hardware/dolby/dolby.mk)|$(call inherit-produc
 
 
 cd device/xiaomi/blossom
-git fetch https://github.com/xc112lg/android_device_xiaomi_blossom.git patch-2
+git fetch https://github.com/xc112lg/android_device_xiaomi_blossom.git patch-3
 sleep 5
-git cherry-pick 18f395f1c3de530500be1e6a62cdf96ade9bde6f e139eba1535a4465ea57f98d0b8b3f5528f558bb
-cd - 
+git cherry-pick 523fb262dd41a83f05b4303e53976b9ba1ce6a6d
 
 source build/envsetup.sh
 
