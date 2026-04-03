7f1a77225e42c55468a0e584e74c41068d92b9cc
xc112lg <152837944+xc112lg@users.noreply.github.com>
2026-04-03 08:45:09 +0800

    Update build16.sh

--- build16.sh
+++ build16.sh
@@ -89,13 +89,13 @@ sed -i '/"com.android.chrome",/a\        "com.lazada.android",\n        "com.sho
 #cat frameworks/base/core/java/com/android/internal/util/evolution/PixelPropsUtils.java
 # sed -i '/$(call inherit-product, vendor/xiaomi/blossom/blossom-vendor.mk)\n    $(call inherit-product-if-exists, vendor/xiaomi/miuicamera/MiuiCamera.mk/' device/xiaomi/blossom/device.mk
 
-cd device/xiaomi/blossom
-git fetch https://github.com/xc112lg/android_device_xiaomi_blossom.git patch-3
-sleep 5
-git cherry-pick 523fb262dd41a83f05b4303e53976b9ba1ce6a6d
-cd -
-sed -i 's|$(call inherit-product, vendor/xiaomi/miuicamera/MiuiCamera.mk)|$(call inherit-product-if-exists, vendor/xiaomi/miuicamera/MiuiCamera.mk)|' device/xiaomi/blossom/device.mk
-sed -i 's|$(call inherit-product, hardware/dolby/dolby.mk)|$(call inherit-product-if-exists, hardware/dolby/dolby.mk)|' device/xiaomi/blossom/device.mk
+# cd device/xiaomi/blossom
+# git fetch https://github.com/xc112lg/android_device_xiaomi_blossom.git patch-3
+# sleep 5
+# git cherry-pick 523fb262dd41a83f05b4303e53976b9ba1ce6a6d
+# cd -
+# sed -i 's|$(call inherit-product, vendor/xiaomi/miuicamera/MiuiCamera.mk)|$(call inherit-product-if-exists, vendor/xiaomi/miuicamera/MiuiCamera.mk)|' device/xiaomi/blossom/device.mk
+# sed -i 's|$(call inherit-product, hardware/dolby/dolby.mk)|$(call inherit-product-if-exists, hardware/dolby/dolby.mk)|' device/xiaomi/blossom/device.mk
 
 # sed -i '/proc \/tp_gesture/d' device/xiaomi/blossom/sepolicy/**/*
 # sed -i '/cpufreq/d' device/xiaomi/blossom/sepolicy/**/*
