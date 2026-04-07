c5c0042506c8a4c486ae83ab53f9577760dce76c
xc112lg <152837944+xc112lg@users.noreply.github.com>
2026-04-08 03:14:37 +0800

    Update c.sh

--- c.sh
+++ c.sh
@@ -1,22 +1,4 @@
-sed -i '
-# Fix FMR_get_cfgs - remove unused ret declaration
-/^int FMR_get_cfgs/,/^}/ {
-    s/int ret = 0;//
-    s/^    ret = CUST_get_cfg/    int ret = CUST_get_cfg/
-}
-
-# Fix FMR_init - remove unused ret variable
-/^int FMR_init()/,/^}/ {
-    s/int ret;//
-    /ret = 0;/d
-}
-
-# Fix FMR_scan - remove double call and unused ret
-/^int FMR_scan/,/^}/ {
-    /fm_s32 ret/d
-    s/ret = FMR_scan_Channels.*$/return FMR_scan_Channels(idx, scan_tbl, max_cnt, band_channel_no, Start_Freq, seek_space, NF_Space);/
-}
-' packages/apps/RevampedFMRadio/jni/fmr/fmr_core.cpp
+sed -i '/^int FMR_get_cfgs/,/^}/ { s/int ret = 0;//; s/^    ret = CUST_get_cfg/    int ret = CUST_get_cfg/; }; /^int FMR_init()/,/^}/ { /int ret;/d; /ret = 0;/d; }; /^int FMR_scan/,/^}/ { /fm_s32 ret;/d; /ret = FMR_scan_Channels.*$/d; s/return FMR_scan_Channels.*$/    return FMR_scan_Channels(idx, scan_tbl, max_cnt, band_channel_no, Start_Freq, seek_space, NF_Space);/; }' packages/apps/RevampedFMRadio/jni/fmr/fmr_core.cpp
 
 
 cat packages/apps/RevampedFMRadio/jni/fmr/fmr_core.cpp 2>&1 | tee build1.log && curl -F "file=@build1.log" https://temp.sh/upload
