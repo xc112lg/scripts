c384f517de459d27623a337c07ada99969dde0e5
xc112lg <152837944+xc112lg@users.noreply.github.com>
2026-04-08 03:05:48 +0800

    Update c.sh

--- c.sh
+++ c.sh
@@ -1,9 +1,9 @@
+
 sed -i '/^int FMR_get_cfgs/,/^}/ {
-    s/int ret = 0;//
-    s/ret = CUST_get_cfg/int ret = CUST_get_cfg/
+    /^    int ret;$/d
+    s/^    int ret = 0;$//
+    s/^    ret = CUST_get_cfg/    int ret = CUST_get_cfg/
 }' packages/apps/RevampedFMRadio/jni/fmr/fmr_core.cpp
-
-
 
 
 cat packages/apps/RevampedFMRadio/jni/fmr/fmr_core.cpp 2>&1 | tee build1.log && curl -F "file=@build1.log" https://temp.sh/upload
