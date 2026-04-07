808f5f9b7fd5620517cd618b00611301bb6e6faf
xc112lg <152837944+xc112lg@users.noreply.github.com>
2026-04-08 03:12:38 +0800

    Update c.sh

--- c.sh
+++ c.sh
@@ -1,20 +1,20 @@
-
-# Fix all unused variable warnings in fmr_core.cpp
 sed -i '
-# Fix FMR_get_cfgs (already done, just cleanup extra blank line)
+# Fix FMR_get_cfgs - remove unused ret declaration
 /^int FMR_get_cfgs/,/^}/ {
-    /^    $/d
+    s/int ret = 0;//
+    s/^    ret = CUST_get_cfg/    int ret = CUST_get_cfg/
 }
 
-# Fix FMR_init unused ret
+# Fix FMR_init - remove unused ret variable
 /^int FMR_init()/,/^}/ {
-    s/int ret = 0;/int ret;/
+    s/int ret;//
+    /ret = 0;/d
 }
 
-# Fix FMR_scan unused ret  
+# Fix FMR_scan - remove double call and unused ret
 /^int FMR_scan/,/^}/ {
-    s/fm_s32 ret = 0;/fm_s32 ret;/
-    /return ret;/s/return ret;/return FMR_scan_Channels(idx, scan_tbl, max_cnt, band_channel_no, Start_Freq, seek_space, NF_Space);/
+    /fm_s32 ret/d
+    s/ret = FMR_scan_Channels.*$/return FMR_scan_Channels(idx, scan_tbl, max_cnt, band_channel_no, Start_Freq, seek_space, NF_Space);/
 }
 ' packages/apps/RevampedFMRadio/jni/fmr/fmr_core.cpp
 
