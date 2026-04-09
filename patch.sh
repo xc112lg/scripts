ebbdb4b7d9335caa929fe6d8ce7dcfabee71c4ff
xc112lg <152837944+xc112lg@users.noreply.github.com>
2026-04-09 12:01:47 +0800

    Create patch.sh

---
+++ patch.sh
@@ -0,0 +1,17 @@
+# Create a patch file (save as fix-fmr-core.patch)
+cat > fix-fmr-core.patch << 'EOF'
+--- a/packages/apps/RevampedFMRadio/jni/fmr/fmr_core.cpp
++++ b/packages/apps/RevampedFMRadio/jni/fmr/fmr_core.cpp
+@@ -70,8 +70,6 @@ int FMR_get_cfgs(int idx)
+ 
+ int FMR_chk_cfg_data(int idx __unused)
+ {
+-    int ret = 0;
+-
+     //TODO Need check? how to check?
+     return 0;
+ }
+EOF
+
+# Apply it in your build script before compiling
+patch -p1 < fix-fmr-core.patch
