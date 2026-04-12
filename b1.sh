eaa91c458b1c9f0c1447dc4bb969c69cdbd3dd6f
xc112lg <152837944+xc112lg@users.noreply.github.com>
2026-04-12 14:26:48 +0800

    Update b1.sh

--- b1.sh
+++ b1.sh
@@ -55,7 +55,7 @@ repo sync -c -j32 --force-sync --no-clone-bundle --no-tags
 
 # curl -sf https://raw.githubusercontent.com/xc112lg/scripts/refs/heads/blossom-evo/fix_sepolicy.sh | bash
 curl -sf https://raw.githubusercontent.com/xc112lg/scripts/refs/heads/blossom-evo/test.sh  | bash
-
+rm -rf hardware/mediatek/interfaces/hardware/bluetooth
 source build/envsetup.sh
 lunch lineage_blossom-bp4a-userdebug
 
