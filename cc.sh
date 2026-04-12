b9aa431f56d8522797c53eac4d81ffbb1fc29ea0
xc112lg <152837944+xc112lg@users.noreply.github.com>
2026-04-13 03:25:18 +0800

    Create cc.sh

---
+++ cc.sh
@@ -0,0 +1,67 @@
+sudo apt update
+sudo apt install patchelf -y
+
+rm -rf .repo/local_manifests/
+rm -rf device/xiaomi
+rm -rf device/xiaomi/blossom-kernel
+rm -rf vendor/xiaomi
+rm -rf vendor/xiaomi/miuicamera
+rm -rf hardware/mediatek
+rm -rf device/mediatek/sepolicy_vndr
+rm -rf hardware/dolby
+rm -rf hardware/
+rm -rf packages/apps/RevampedFMRadio
+
+#rm -rf build/soong
+# Cleanup previous changelog to make it always fresh
+rm -rf out/target/product/*/system/etc/Changelog.txt \
+       out/target/product/*/obj/ETC/Changelog.txt_intermediates \
+       out/target/product/*/gen/ETC/Changelog.txt_intermediates
+
+
+repo init -u https://github.com/Evolution-X/manifest -b bq2 --depth=1 --git-lfs
+#Temp Fix Repo tool
+#cd .repo/repo;git pull -r;cd ../..;
+
+
+# Clone local_manifests repository
+#git clone https://github.com/0kaarun/Blossom_local_mainfest --depth 1 -b A16 .repo/local_manifests
+
+git clone https://github.com/xc112lg/local_manifests --depth 1 -b crblossom .repo/local_manifests
+# if [ ! 0 == 0 ]
+#  then   curl -o .repo/local_manifests https://github.com/bagaskara815/local_manifests.git
+#  fi
+repo sync -c -j32 --force-sync --no-clone-bundle --no-tags
+# repo sync
+/opt/crave/resync.sh
+# grep -q '"com.lazada.android"' frameworks/base/core/java/com/android/internal/util/evolution/PixelPropsUtils.java || \
+# sed -i '/"com.android.chrome",/a\        "com.lazada.android",\n        "com.shopee.ph",' frameworks/base/core/java/com/android/internal/util/evolution/PixelPropsUtils.java
+
+
+# cd device/xiaomi/blossom
+# git fetch https://github.com/xc112lg/device_xiaomi_blossom.git patch-3
+# sleep 5
+# git cherry-pick 27f6bcc191aaaeb66a424b591218418250cec4c6
+# cd -
+# sed -i 's/name: "android.hardware.sensors@2.0-subhal-impl-1.0"/name: "android.hardware.sensors@2.0-subhal-impl-1.0-mtk"/' hardware/mediatek/sensors/Android.bp
+# curl -sf https://raw.githubusercontent.com/xc112lg/scripts/refs/heads/blossom-evo/extras.sh | bash
+
+# tr -d '\000' < packages/apps/Settings/Evolver/res/xml/evolution_settings_miscellaneous.xml > /tmp/fixed.xml
+# mv /tmp/fixed.xml packages/apps/Settings/Evolver/res/xml/evolution_settings_miscellaneous.xml
+
+# # Verify the fix
+# echo "Line 39 should now look normal:"
+# sed -n '39p' packages/apps/Settings/Evolver/res/xml/evolution_settings_miscellaneous.xml
+
+# curl -sf https://raw.githubusercontent.com/xc112lg/scripts/refs/heads/blossom-evo/fix_sepolicy.sh | bash
+curl -sf https://raw.githubusercontent.com/xc112lg/scripts/refs/heads/blossom-evo/test.sh  | bash
+rm -rf hardware/mediatek/interfaces/hardware/bluetooth
+curl -sf https://raw.githubusercontent.com/xc112lg/scripts/refs/heads/blossom-evo/disablegms.sh  | bash
+source build/envsetup.sh
+lunch lineage_blossom-bp4a-userdebug
+export SOONG_UI_THREADS=1
+export JACK_SERVER_VM_ARGUMENTS="-Xmx2g"
+
+m -j1 evolution 2>&1 | tee build.log
+curl -F "file=@build.log" https://temp.sh/upload
+
