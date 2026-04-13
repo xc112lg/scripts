75bf48548d29c3c5004244a7100d00ba00c098d3
xc112lg <152837944+xc112lg@users.noreply.github.com>
2026-04-13 13:37:35 +0800

    Update cc.sh

--- cc.sh
+++ cc.sh
@@ -57,7 +57,7 @@ repo sync -c -j32 --force-sync --no-clone-bundle --no-tags
 # curl -sf https://raw.githubusercontent.com/xc112lg/scripts/refs/heads/blossom-evo/fix_sepolicy.sh | bash
 curl -sf https://raw.githubusercontent.com/xc112lg/scripts/refs/heads/blossom-evo/test.sh  | bash
 rm -rf hardware/mediatek/interfaces/hardware/bluetooth
-curl -sf https://raw.githubusercontent.com/xc112lg/scripts/refs/heads/blossom-evo/disablegms.sh  | bash
+curl -sf https://raw.githubusercontent.com/xc112lg/scripts/refs/heads/blossom-evo/disablegms1.sh  | bash
 source build/envsetup.sh
 lunch lineage_blossom-bp4a-userdebug
 export SOONG_JOBS=1
