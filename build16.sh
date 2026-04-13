a5e05fe61312e44ff2808fdb07cc9cfe7acfb541
xc112lg <152837944+xc112lg@users.noreply.github.com>
2026-04-13 19:55:48 +0800

    Update build16.sh

--- build16.sh
+++ build16.sh
@@ -57,8 +57,12 @@ repo sync -c -j32 --force-sync --no-clone-bundle --no-tags
 # curl -sf https://raw.githubusercontent.com/xc112lg/scripts/refs/heads/blossom-evo/fix_sepolicy.sh | bash
 curl -sf https://raw.githubusercontent.com/xc112lg/scripts/refs/heads/blossom-evo/test.sh  | bash
 rm -rf hardware/mediatek/interfaces/hardware/bluetooth
-curl -sf https://raw.githubusercontent.com/xc112lg/scripts/refs/heads/blossom-evo/disablegms1.sh  | bash
+#curl -sf https://raw.githubusercontent.com/xc112lg/scripts/refs/heads/blossom-evo/disablegms1.sh  | bash
 source build/envsetup.sh
+
+export TARGET_USES_PICO_GAPPS=true
+rm -rf hardware/interfaces/biometrics/fingerprint/2.1/default
+sed -i '\|$(call inherit-product, vendor/gapps/arm64/arm64-vendor.mk)|d' device/xiaomi/blossom/lineage_blossom.mk
 lunch lineage_blossom-bp4a-userdebug
 
 
