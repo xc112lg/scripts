58e98e802a2b87d9e599b69f0b383282b57213b6
xc112lg <152837944+xc112lg@users.noreply.github.com>
2026-04-14 03:18:28 +0800

    Update build16.sh

--- build16.sh
+++ build16.sh
@@ -54,7 +54,7 @@ repo sync -c -j32 --force-sync --no-clone-bundle --no-tags
 # echo "Line 39 should now look normal:"
 # sed -n '39p' packages/apps/Settings/Evolver/res/xml/evolution_settings_miscellaneous.xml
 
-# curl -sf https://raw.githubusercontent.com/xc112lg/scripts/refs/heads/blossom-evo/fix_sepolicy.sh | bash
+curl -sf https://raw.githubusercontent.com/xc112lg/scripts/refs/heads/blossom-evo/fix_sepolicy.sh | bash
 curl -sf https://raw.githubusercontent.com/xc112lg/scripts/refs/heads/blossom-evo/test.sh  | bash
 rm -rf hardware/mediatek/interfaces/hardware/bluetooth
 #curl -sf https://raw.githubusercontent.com/xc112lg/scripts/refs/heads/blossom-evo/disablegms1.sh  | bash
