c35bdaef440aebdb5bc21210eb43b75ed45a03d9
xc112lg <152837944+xc112lg@users.noreply.github.com>
2026-04-15 07:50:37 +0800

    Update build16.sh

--- build16.sh
+++ build16.sh
@@ -66,27 +66,28 @@ source build/envsetup.sh
 export TARGET_USES_PICO_GAPPS=true
 rm -rf hardware/interfaces/biometrics/fingerprint/2.1/default
 # Fix ThemeUtils.getInstance() syntax errors
-echo "Fixing ThemeUtils.getInstance() syntax errors..."
+# echo "Fixing ThemeUtils.getInstance() syntax errors..."
 
-FILES=(
-    "packages/apps/Settings/Evolver/src/org/evolution/settings/fragments/themes/IconShapes.java"
-    "packages/apps/Settings/Evolver/src/org/evolution/settings/fragments/themes/NavigationBarIcons.java"
-    "packages/apps/Settings/Evolver/src/org/evolution/settings/fragments/themes/Themes.java"
-)
+# FILES=(
+#     "packages/apps/Settings/Evolver/src/org/evolution/settings/fragments/themes/IconShapes.java"
+#     "packages/apps/Settings/Evolver/src/org/evolution/settings/fragments/themes/NavigationBarIcons.java"
+#     "packages/apps/Settings/Evolver/src/org/evolution/settings/fragments/themes/Themes.java"
+# )
 
-for file in "${FILES[@]}"; do
-    if [ -f "$file" ]; then
-        echo "Processing: $file"
-        sed -i 's/new ThemeUtils\.getInstance(/ThemeUtils.getInstance(/g' "$file"
-    else
-        echo "Warning: $file not found"
-    fi
-done
+# for file in "${FILES[@]}"; do
+#     if [ -f "$file" ]; then
+#         echo "Processing: $file"
+#         sed -i 's/new ThemeUtils\.getInstance(/ThemeUtils.getInstance(/g' "$file"
+#     else
+#         echo "Warning: $file not found"
+#     fi
+# done
 
-echo "Done! Re-run your build."
+# echo "Done! Re-run your build."
 
 
 sed -i '\|$(call inherit-product, vendor/gapps/arm64/arm64-vendor.mk)|d' device/xiaomi/blossom/lineage_blossom.mk
+sed -i '/# FM Radio/,+2d' device/xiaomi/blossom/device.mk
 lunch lineage_blossom-bp4a-userdebug
 
 
