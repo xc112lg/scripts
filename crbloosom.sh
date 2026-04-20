62dcf4430d12bd7ef46a6226e8291c40c475d2f0
xc112lg <152837944+xc112lg@users.noreply.github.com>
2026-04-20 13:57:50 +0800

    Update crbloosom.sh

--- crbloosom.sh
+++ crbloosom.sh
@@ -5,12 +5,10 @@ rm -rf .repo/local_manifests/
 rm -rf device/xiaomi
 rm -rf device/xiaomi/blossom-kernel
 rm -rf vendor/xiaomi
-rm -rf vendor/gms
 rm -rf vendor/xiaomi/miuicamera
 rm -rf hardware/mediatek
 rm -rf device/mediatek/sepolicy_vndr
 rm -rf hardware/dolby
-rm -rf hardware/
 rm -rf packages/apps/RevampedFMRadio
 rm -rf packages/apps/Settings/
 
