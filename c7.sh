1f6ffb74fc00e401b194297b4996439dec64a92d
xc112lg <152837944+xc112lg@users.noreply.github.com>
2026-06-09 07:29:21 +0800

    Update and rename c6.sh to c7.sh

--- c7.sh
+++ c7.sh
@@ -14,7 +14,7 @@ type patch
 
 source ~/.bashrc 
 type patch
-
+cat ~/.bashrc
 # rm -rf .repo/local_manifests/  && # Clone local_manifests repository
 
 #  git clone https://github.com/ardiandideyashidiq/local_manifest-P13001L --depth 1 -b lineage-23.2 .repo/local_manifests && 
