72a2e8c015c3f634a7d35b64f0e49e5c0f96e8f5
xc112lg <152837944+xc112lg@users.noreply.github.com>
2026-06-08 21:56:49 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,7 +6,8 @@ sudo apt install patchelf -y
 rm -rf .repo/local_manifests
 repo init -u https://github.com/Lunaris-AOSP/android -b 16.2 --depth=1 --git-lfs
 git clone https://github.com/xc112lg/local_manifests.git -b lunaris .repo/local_manifests
-/opt/crave/resync.sh
+repo sync -c -j32 --force-sync --no-clone-bundle --no-tags
+#/opt/crave/resync.sh
 . build/envsetup.sh
 export WITH_GMS=false
 lunch lineage_blossom-bp4a-eng
