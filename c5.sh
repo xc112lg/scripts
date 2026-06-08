c19a9afa5c98ffb28124ceedeac11c1f5bc22a81
xc112lg <152837944+xc112lg@users.noreply.github.com>
2026-06-09 07:50:02 +0800

    Update c5.sh

--- c5.sh
+++ c5.sh
@@ -1,6 +1,6 @@
-sed -i '/^esac$/a\
-alias patch='"'"'patch --force'"'"'\
-alias repo-init='"'"'repo init --depth=1'"'"'' ~/.bashrc
+sed -i '/alias patch/d; /alias repo-init/d' ~/.bashrc
+
+sed -i '/^esac$/a alias patch="patch --force"\nalias repo-init="repo init --depth=1"' ~/.bashrc
 echo here
 source ~/.bashrc
 alias patch
