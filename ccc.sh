8d4fa615b38b6893b2f35504557435c38e3bb791
xc112lg <152837944+xc112lg@users.noreply.github.com>
2026-06-09 05:07:22 +0800

    Create ccc.sh

---
+++ ccc.sh
@@ -0,0 +1,41 @@
+type patch
+
+source /home/admin/.bashrc
+
+
+
+rm -rf .repo/local_manifests/  && # Clone local_manifests repository
+
+ git clone https://github.com/ardiandideyashidiq/local_manifest-P13001L --depth 1 -b lineage-23.2 .repo/local_manifests && 
+
+# Sync the repositories
+
+if [ -f /usr/bin/resync ]
+
+ then
+
+  /usr/bin/resync # For compatibility with Omansh's Docker image 
+
+else
+
+  /opt/crave/resync.sh
+
+fi && 
+
+# Set up build environment
+
+export BUILD_USERNAME=ardiandideyashidiq 
+
+ export BUILD_HOSTNAME=crave 
+
+ source build/envsetup.sh && 
+
+echo Repository: ardiandideyashidiq/crave_aosp_builder
+
+ echo Run ID: 26355419836
+
+ 
+
+# Build the ROM
+
+breakfast P13001L user && make installclean && mka bacon
