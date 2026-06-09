22f586558bdfd85eacb4705831ccddd5107e3d82
xc112lg <152837944+xc112lg@users.noreply.github.com>
2026-06-10 03:17:18 +0800

    Update sep.sh

--- sep.sh
+++ sep.sh
@@ -5,6 +5,7 @@ rm -rf TMP_PATCHES
 sudo apt update
 sudo apt install patchelf -y
 rm -rf .repo/local_manifests
+repo init -u https://github.com/Lunaris-AOSP/android -b 16.2 --depth=1 --git-lfs
 git clone https://github.com/xc112lg/local_manifests.git -b lunaris .repo/local_manifests
 repo sync -c -j32 --force-sync --no-clone-bundle --no-tags
 /opt/crave/resync.sh
