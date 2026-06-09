3a2c63e1f1c06807893acd06a8bff44f8881d60c
xc112lg <152837944+xc112lg@users.noreply.github.com>
2026-06-10 02:44:25 +0800

    Create sep.sh

---
+++ sep.sh
@@ -0,0 +1,15 @@
+type patch
+rm -rf .repo/local_manifests/
+rm -rf device/xiaomi
+rm -rf TMP_PATCHES
+sudo apt update
+sudo apt install patchelf -y
+rm -rf .repo/local_manifests
+git clone https://github.com/xc112lg/local_manifests.git -b lunaris .repo/local_manifests
+repo sync -c -j32 --force-sync --no-clone-bundle --no-tags
+/opt/crave/resync.sh
+. build/envsetup.sh
+export WITH_GMS=false
+lunch lineage_blossom-bp4a-eng
+m installclean
+make sepolicy
