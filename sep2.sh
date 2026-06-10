3c8ecd91339bf7e35cd819c20762aa030f7623da
xc112lg <152837944+xc112lg@users.noreply.github.com>
2026-06-10 08:51:25 +0800

    Update and rename sep1.sh to sep2.sh

--- sep2.sh
+++ sep2.sh
@@ -1,9 +1,13 @@
 type patch
 rm -rf .repo/local_manifests/
-rm -rf device/xiaomi
-rm -rf TMP_PATCHES
+#rm -rf device/xiaomi
+#rm -rf TMP_PATCHES
 sudo apt update
 sudo apt install patchelf -y
+sudo apt install ccache -y
+mkdir tmp
+export CCACHE_DIR=tmp
+export USE_CCACHE=1
 rm -rf .repo/local_manifests
 repo init -u https://github.com/Lunaris-AOSP/android -b 16.2 --depth=1 --git-lfs
 git clone https://github.com/xc112lg/local_manifests.git -b lunaris .repo/local_manifests
@@ -11,6 +15,9 @@ repo sync -c -j32 --force-sync --no-clone-bundle --no-tags
 /opt/crave/resync.sh
 . build/envsetup.sh
 export WITH_GMS=false
+# Add memory-saving flags
+export USE_NINJA=false  # Use Make instead of Ninja (more memory efficient)
+export ENABLE_CPUSTATS=false
 lunch lineage_blossom-bp4a-eng
 m installclean
-make sepolicy
+make sepolicy -j1
