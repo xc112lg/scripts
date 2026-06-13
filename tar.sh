3ae2953f27de6fd28e41781ab71445d3b0217231
xc112lg <152837944+xc112lg@users.noreply.github.com>
2026-06-14 04:51:23 +0800

    Update tar.sh

--- tar.sh
+++ tar.sh
@@ -5,11 +5,11 @@ rm -rf blossom_lunaris
 git clone https://$GH_TOKEN@github.com//xc112lg/blossom_lunaris
 echo "Created: recovery.tar"
 #cd -
-rm -rf crdroid10.x/*.img crdroid10.x/*.zip crdroid10.x/*.tar
-cp out/target/product/*/recovery.img crdroid10.x
-    cp out/target/product/*/*.zip crdroid10.x/
+rm -rf blossom_lunaris/*.img blossom_lunaris/*.zip blossom_lunaris/*.tar
+cp out/target/product/*/recovery.img blossom_lunaris
+    cp out/target/product/*/*.zip blossom_lunaris/
 
-    cp out/target/product/*/*.tar crdroid10.x
+    cp out/target/product/*/*.tar blossom_lunaris
 cd blossom_lunaris
 chmod +x multi_upload.sh
 ./multi_upload.sh
