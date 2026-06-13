14c9eb8a94765f7fba431e3d9764b6bab76e935b
xc112lg <152837944+xc112lg@users.noreply.github.com>
2026-06-14 04:46:29 +0800

    Update tar.sh

--- tar.sh
+++ tar.sh
@@ -1,7 +1,7 @@
 
 if ls out/target/product/*/*.zip >/dev/null 2>&1; then
 export GH_TOKEN=$(cat gh_token.txt)
-rm -rf crdroid10.x
+rm -rf blossom_lunaris
 git clone https://$GH_TOKEN@github.com//xc112lg/blossom_lunaris
 echo "Created: recovery.tar"
 #cd -
@@ -10,7 +10,7 @@ cp out/target/product/*/recovery.img crdroid10.x
     cp out/target/product/*/*.zip crdroid10.x/
 
     cp out/target/product/*/*.tar crdroid10.x
-cd crdroid10.x
+cd blossom_lunaris
 chmod +x multi_upload.sh
 ./multi_upload.sh
 else
