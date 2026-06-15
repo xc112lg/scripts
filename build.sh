d5e9aa853408f41b1a1326cfdd40991162c83b41
xc112lg <152837944+xc112lg@users.noreply.github.com>
2026-06-15 16:17:06 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -12,7 +12,7 @@ repo sync -c -j32 --force-sync --no-clone-bundle --no-tags
 . build/envsetup.sh
 export WITH_GMS=false
 sed -i '/<item>com.android.nfc<\/item>/d' frameworks/base/core/res/res/values/policy_exempt_apps.xml
-cat frameworks/base/core/res/res/values/policy_exempt_apps.xml
+
 lunch lineage_blossom-bp4a-eng
 m installclean
 m bacon
