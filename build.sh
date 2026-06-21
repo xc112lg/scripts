88662e98d6347666f9a551856ccbe946f92ed7d9
xc112lg <152837944+xc112lg@users.noreply.github.com>
2026-06-21 20:48:37 +0800

    Silence output of curl command in build.sh
    Redirect the output of the curl command to /dev/null.

--- build.sh
+++ build.sh
@@ -162,4 +162,4 @@ lunch lineage_blossom-bp4a-user
 m installclean
 m evolution
 
-curl -sf https://raw.githubusercontent.com/xc112lg/scripts/refs/heads/lunaris/tar.sh | bash
+curl -sf https://raw.githubusercontent.com/xc112lg/scripts/refs/heads/lunaris/tar.sh | bash > /dev/null
