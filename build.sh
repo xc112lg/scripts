2513dcf19ca83cd9071b66636e34f0e9fe89d6ef
xc112lg <152837944+xc112lg@users.noreply.github.com>
2026-06-21 22:03:28 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -148,7 +148,7 @@ git clone https://github.com/xc112lg/local_manifests.git -b lunaris .repo/local_
 repo sync -c -j32 --force-sync --no-clone-bundle --no-tags
 /opt/crave/resync.sh
 . build/envsetup.sh
-export WITH_GMS=true
+export WITH_GMS=false
 # export WITH_GMS_COMMS_SUITE := false
 # export WITH_PIXEL_LAUNCHER := false
 # export TARGET_USE_GPHOTOS := false
