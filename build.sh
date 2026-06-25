70931263777408922b08cea5ac8d027a5c939d0f
xc112lg <152837944+xc112lg@users.noreply.github.com>
2026-06-25 20:35:55 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -23,6 +23,8 @@ export WITH_GMS=false
 # export TARGET_USE_GPHOTOS := false
 # export TARGET_USE_WALLPAPERS := false
 export TARGET_USES_PICO_GAPPS=true
+export TARGET_INCLUDE_VIA=true
+
 sed -i 's|-include vendor/lineage-priv/keys/keys.mk|-include vendor/evolution-priv/keys/keys.mk|' device/xiaomi/blossom/lineage_blossom.mk
 sed -i '\|vendor/extras/prebuilt/product/fonts,\$(TARGET_COPY_OUT_PRODUCT)/fonts|d' vendor/extras/evolution.mk
 #sed -i '/<item>com.android.nfc<\/item>/d' frameworks/base/core/res/res/values/policy_exempt_apps.xml
