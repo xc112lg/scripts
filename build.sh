631fc120b2cb2986899fa3072affd6d56309b437
xc112lg <152837944+xc112lg@users.noreply.github.com>
2026-06-26 16:43:51 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -29,19 +29,13 @@ sed -i 's|-include vendor/lineage-priv/keys/keys.mk|-include vendor/evolution-pr
 sed -i '\|vendor/extras/prebuilt/product/fonts,\$(TARGET_COPY_OUT_PRODUCT)/fonts|d' vendor/extras/evolution.mk
 #sed -i '/<item>com.android.nfc<\/item>/d' frameworks/base/core/res/res/values/policy_exempt_apps.xml
 #cat frameworks/base/core/res/res/values/policy_exempt_apps.xml
-echo "USE_RBE=$USE_RBE"
-echo "RBE_service=$RBE_service"
-echo "RBE_instance=$RBE_instance"
-echo "RBE_remote_headers=$RBE_remote_headers"
+
 export RBE_LOG=DEBUG
 export RBE_VERBOSE=1
-echo $RBE_service
-echo $RBE_remote_headers
-echo $RBE_instance
-echo $RBE_instance_name
-echo $USE_RBE
+
 lunch lineage_blossom-bp4a-eng
 m installclean
+m clean #once
 m evolution
 
 curl -sf https://raw.githubusercontent.com/xc112lg/scripts/refs/heads/blossom/upevo.sh | bash >/dev/null 2>&1
