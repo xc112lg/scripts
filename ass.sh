f97db48683000d58db0be1099d491bc59486c901
xc112lg <152837944+xc112lg@users.noreply.github.com>
2026-06-28 18:14:59 +0800

    Update and rename as.sh to ass.sh

--- ass.sh
+++ ass.sh
@@ -11,9 +11,9 @@ git clone https://github.com/xc112lg/rbe1 >/dev/null 2>&1
 export CLANG_TARGET_ARM32="--target=arm-linux-android"
 source build/envsetup.sh
 #make clean
-
-source <(curl -sf https://raw.githubusercontent.com/xc112lg/scripts/refs/heads/lunaris/rbe1.sh)
 source <(curl -sf https://raw.githubusercontent.com/xc112lg/scripts/refs/heads/lunaris/test-reproxy.sh)
+source <(curl -sf https://raw.githubusercontent.com/xc112lg/scripts/refs/heads/lunaris/rbe1.sh)
+
 #export WITH_GMS=false
 #rm -rf hardware/interfaces/biometrics/fingerprint/2.1/default
 
