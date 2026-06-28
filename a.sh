4ccc1997cca8ee8822d8ba614e983daa04c6a04b
xc112lg <152837944+xc112lg@users.noreply.github.com>
2026-06-28 14:06:11 +0800

    Update a.sh

--- a.sh
+++ a.sh
@@ -2,7 +2,7 @@
 rm -rf .repo/local_manifests 
 git clone https://github.com/LG-G6/scripts.git -b lineage-21 
 mkdir .repo/local_manifests 
-source <(curl -sf https://raw.githubusercontent.com/xc112lg/scripts/refs/heads/lunaris/rbe.sh) >/dev/null 2>&1
+source <(curl -sf https://raw.githubusercontent.com/xc112lg/scripts/refs/heads/lunaris/rbe1.sh) >/dev/null 2>&1
 cp scripts/roomservice.xml .repo/local_manifests/ 
 repo sync -c -j32 --force-sync --no-clone-bundle --no-tags
 /opt/crave/resync.sh
