b39c77093f55324b649542b38db05d3c7c02f784
xc112lg <152837944+xc112lg@users.noreply.github.com>
2026-07-01 07:20:06 +0800

    s

--- upevo1.sh
+++ upevo1.sh
@@ -11,13 +11,13 @@ fi
 
 if ls out/target/product/*/*.zip >/dev/null 2>&1; then
 
-rm -rf blossom_lineage
-git clone https://$GH_TOKEN@github.com//xc112lg/blossom_lineage
+rm -rf blossom_evolution
+git clone https://$GH_TOKEN@github.com//xc112lg/blossom_evolution
 
 #cd -
 #rm -rf blossom_lunaris/*.img blossom_lunaris/*.zip blossom_lunaris/*.tar
 #cp out/target/product/*/recovery.img blossom_lunaris
-cp out/target/product/*/*.zip blossom_lineage/
+cp out/target/product/*/*.zip blossom_evolution/
 # echo "test" > blossom_lunaris/dummy.txt
 
 # Create the zip
@@ -25,8 +25,8 @@ cp out/target/product/*/*.zip blossom_lineage/
 
 # Check size
 # ls -lh blossom_lunaris/test.zip
-cp out/target/product/*/*.tar blossom_lineage
-cd blossom_lineage
+cp out/target/product/*/*.tar blossom_evolution
+cd blossom_evolution
 chmod +x multi_upload3.sh
 ./multi_upload3.sh > /dev/null
 else
