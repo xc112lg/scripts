bfc95e360d2ea7bccc89de8c33902b3722277c0e
xc112lg <152837944+xc112lg@users.noreply.github.com>
2026-07-01 09:12:18 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,10 +3,11 @@
 # Recommendations based on your current setup and performance best practices
 git clone https://github.com/xc112lg/rbe1 >/dev/null 2>&1
 
-source <(curl -sf https://raw.githubusercontent.com/xc112lg/scripts/refs/heads/lunaris/rbe2.sh)  >/dev/null 2>&1
+
 rm -rf .repo/local_manifests/
 rm -rf device/xiaomi
 rm -rf kernel/xiaomi/blossom
+rm -rf 
 rm -rf TMP_PATCHES
 #rm -rf frameworks/base
 sudo apt update >/dev/null 2>&1
@@ -16,6 +17,7 @@ repo init -u https://github.com/LineageOS/android.git -b lineage-23.2 --git-lfs 
 git clone https://github.com/xc112lg/local_manifests.git -b lunaris .repo/local_manifests
 repo sync -c -j32 --force-sync --no-clone-bundle --no-tags
 /opt/crave/resync.sh
+source <(curl -sf https://raw.githubusercontent.com/xc112lg/scripts/refs/heads/lunaris/rbe2.sh)  >/dev/null 2>&1
 . build/envsetup.sh
 #export WITH_GMS=true
 export WITH_GMS=false
@@ -27,7 +29,7 @@ export TARGET_USES_PICO_GAPPS=true
 export TARGET_INCLUDE_VIA=true
 export TARGET_INCLUDE_REVAMPED=true
 sed -i '$a -include vendor/evolution-priv/keys/keys.mk' device/xiaomi/blossom/lineage_blossom.mk
-sed -i '\|vendor/extras/prebuilt/product/fonts,\$(TARGET_COPY_OUT_PRODUCT)/fonts|d' vendor/extras/evolution.mk
+#sed -i '\|vendor/extras/prebuilt/product/fonts,\$(TARGET_COPY_OUT_PRODUCT)/fonts|d' vendor/extras/evolution.mk
 #sed -i '/<item>com.android.nfc<\/item>/d' frameworks/base/core/res/res/values/policy_exempt_apps.xml
 #cat frameworks/base/core/res/res/values/policy_exempt_apps.xml
 
