0d2b82baccb136a4c4fd1a3314080b8408836bf4
xc112lg <152837944+xc112lg@users.noreply.github.com>
2026-07-02 12:50:48 +0800

    s

--- rbe4.sh
+++ rbe4.sh
@@ -35,7 +35,7 @@ export RBE_remote_cache_header="x-buildbuddy-api-key=D2SvmJdB1v8oM6KaNg6J"
 export RBE_remote_headers="x-buildbuddy-api-key=D2SvmJdB1v8oM6KaNg6J"
 
 export RBE_use_rpc_credentials=true
-export RBE_service_no_auth=true
+export RBE_service_no_auth=false
 
 # ============================================
 # RECLIENT BINARY DISCOVERY
