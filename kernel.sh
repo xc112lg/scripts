a41e6d1bdcdd26efe8f88077baf7760529244654
xc112lg <152837944+xc112lg@users.noreply.github.com>
2026-07-05 09:35:34 +0800

    Update kernel.sh to apply patches and download scripts

--- kernel.sh
+++ kernel.sh
@@ -12,28 +12,7 @@ rm -rf sashimi_kernel_xiaomi_blossom
 rm -rf android_device_xiaomi_blossom-kernel
 git clone https://github.com/zyexro/sashimi_kernel_xiaomi_blossom -b sashimi --depth 1
 cd sashimi_kernel_xiaomi_blossom 
-sed -i.backup '/^extern unsigned int gCapturePriLayerEnable;/i\
-/* Forward declaration and struct definition for dprec_logger_event */\
-struct dprec_logger {\
-\	unsigned long long period_frame;\
-\	unsigned long long period_total;\
-\	unsigned long long period_max_frame;\
-\	unsigned long long period_min_frame;\
-\	unsigned long long ts_start;\
-\	unsigned long long ts_trigger;\
-\	unsigned long long count;\
-};\
-\
-struct dprec_logger_event {\
-\	int8_t name[24];\
-\	unsigned int mmp;\
-\	uint32_t level;\
-\	struct dprec_logger logger;\
-};\
-' drivers/misc/mediatek/video/mt6765/dispsys/display_recorder.h
-
-grep -n "struct dprec_logger_event" \
-  drivers/misc/mediatek/video/mt6765/dispsys/display_recorder.h
+curl -L https://github.com/xc112lg/sashimi_kernel_xiaomi_blossom/commit/f8717ce1b4ec2c1195deccbe2365369379be538f.patch | git am
 
 
 curl -L https://github.com/xc112lg/sashimi_kernel_xiaomi_blossom/commit/a571172d38b2f706f3b21a8a0e3543c42d4bd2a6.patch | git am
