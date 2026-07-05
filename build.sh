a126de78a04029df05f1647d0865a64c5b8e83c2
xc112lg <152837944+xc112lg@users.noreply.github.com>
2026-07-05 11:11:54 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ rm -rf .repo/local_manifests/
 rm -rf device/xiaomi
 rm -rf kernel/xiaomi/blossom
 rm -rf vendor/lineage
-rm -rf build
+#rm -rf build
 rm -rf TMP_PATCHES
 #rm -rf frameworks/base
 sudo apt update >/dev/null 2>&1
