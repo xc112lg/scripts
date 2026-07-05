b06ed3c3eadeda3c2b340b3a5474bd34ad61c12b
xc112lg <152837944+xc112lg@users.noreply.github.com>
2026-07-05 09:20:57 +0800

    Update kernel.sh

--- kernel.sh
+++ kernel.sh
@@ -12,6 +12,25 @@ rm -rf sashimi_kernel_xiaomi_blossom
 rm -rf android_device_xiaomi_blossom-kernel
 git clone https://github.com/zyexro/sashimi_kernel_xiaomi_blossom -b sashimi --depth 1
 cd sashimi_kernel_xiaomi_blossom 
+sed -i '/^extern unsigned int gCapturePriLayerEnable;/i\
+/* Forward declaration and struct definition for dprec_logger_event */\
+struct dprec_logger {\
+\	unsigned long long period_frame;\
+\	unsigned long long period_total;\
+\	unsigned long long period_max_frame;\
+\	unsigned long long period_min_frame;\
+\	unsigned long long ts_start;\
+\	unsigned long long ts_trigger;\
+\	unsigned long long count;\
+};\
+\
+struct dprec_logger_event {\
+\	int8_t name[24];\
+\	unsigned int mmp;\
+\	uint32_t level;\
+\	struct dprec_logger logger;\
+};\
+' drivers/misc/mediatek/video/mt6765/dispsys/display_recorder.h
 curl -L https://github.com/xc112lg/sashimi_kernel_xiaomi_blossom/commit/a571172d38b2f706f3b21a8a0e3543c42d4bd2a6.patch | git am
 wget -O buildneutron1.sh https://raw.githubusercontent.com/xc112lg/extremeNiigo/refs/heads/yoka_rb1/buildneutron1.sh
 chmod +x buildneutron1.sh
