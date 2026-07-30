5b3d0205fc6cad913efef70c2eba2ae1c9c38069
xc112lg <152837944+xc112lg@users.noreply.github.com>
2026-07-31 04:09:44 +0800

    Update and rename rr1.sh to rr.sh

--- rr.sh
+++ rr.sh
@@ -100,3 +100,7 @@ echo "No authentication errors detected."
 
 echo
 echo "========== SUCCESS =========="
+
+
+
+prebuilts/remoteexecution-client/buildbuddyfix/reproxy -server_address=unix:///tmp/reproxy.sock -service=xc112lg.buildbuddy.io:443 -cas_service=xc112lg.buildbuddy.io:443 -remote_headers="x-buildbuddy-api-key=D2SvmJdB1v8oM6KaNg6J"
