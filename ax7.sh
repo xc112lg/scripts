4c165ea123d068185d87cd6c1a3738af06de84a6
xc112lg <152837944+xc112lg@users.noreply.github.com>
2026-07-31 06:39:46 +0800

    Update ax7.sh

--- ax7.sh
+++ ax7.sh
@@ -25,7 +25,7 @@ fi
 export RBE_service="xc112lg1.buildbuddy.io:443"        # BuildBuddy instance address (without grpcs://, add the port 443)
 export RBE_remote_headers="x-buildbuddy-api-key=7iMbWyZKZx0eFB9MLKUd"    # Your BuildBuddy API key
 export RBE_use_rpc_credentials=false
-export RBE_service_no_auth=true
+export RBE_service_no_auth=false
 
 # --- Enable RBE and General Settings ---
 export USE_RBE=1
